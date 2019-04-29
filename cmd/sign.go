284e474389f0421a35789f1109311c42237868da
Jakub Nowakowski <jakub.nowakowski@akena.co>
2019-04-29 17:54:33 +0200

    Invoke signing from command line
    Handling of command line execution. To run signing execute:
    `./keep-tecdsa sign HASH` where HASH is a string to sign.

---
+++ cmd/sign.go
@@ -0,0 +1,48 @@
+package cmd
+
+import (
+	"fmt"
+
+	"github.com/keep-network/keep-tecdsa/pkg/sign"
+	"github.com/urfave/cli"
+)
+
+// SignCommand contains the definition of the sign command-line subcommand.
+var SignCommand cli.Command
+
+const signDescription = `The sign command creates a local signer and calculates
+   a signature over provided argument.`
+
+func init() {
+	SignCommand = cli.Command{
+		Name:        "sign",
+		Usage:       "Calculates a signature",
+		Description: signDescription,
+		Action:      Sign,
+	}
+}
+
+// Sign sets up a set of local virtual nodes and launches the beacon on
+// them, simulating some relay entries and requests.
+func Sign(c *cli.Context) error {
+	arg := c.Args().First()
+
+	signer, err := sign.NewSigner()
+	if err != nil {
+		return fmt.Errorf("signer creation failed [%v]", err)
+	}
+
+	fmt.Printf("--- Generated Public Key:\nX: %x\nY: %x\n",
+		signer.PublicKey().X,
+		signer.PublicKey().Y,
+	)
+
+	signature, err := signer.CalculateSignature([]byte(arg))
+	if err != nil {
+		return fmt.Errorf("signature calculation failed [%v]", err)
+	}
+
+	fmt.Printf("--- Signature:\nR: %x\nS: %x\n", signature.R, signature.S)
+
+	return nil
+}
