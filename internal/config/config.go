a54984643abe80bb6ebb48421b22d0e9ddef6548
Jakub Nowakowski <jakub.nowakowski@akena.co>
2019-05-01 15:54:42 +0200

    Reading config from toml file

---
+++ internal/config/config.go
@@ -0,0 +1,23 @@
+package config
+
+import (
+	"fmt"
+
+	"github.com/BurntSushi/toml"
+	"github.com/keep-network/keep-tecdsa/pkg/chain/electrum"
+)
+
+// Config is the top level config structure.
+type Config struct {
+	Electrum electrum.Config
+}
+
+// ReadConfig reads in the configuration file in .toml format.
+func ReadConfig(filePath string) (*Config, error) {
+	config := &Config{}
+	if _, err := toml.DecodeFile(filePath, config); err != nil {
+		return nil, fmt.Errorf("unable to decode .toml file [%s] error [%s]", filePath, err)
+	}
+
+	return config, nil
+}
