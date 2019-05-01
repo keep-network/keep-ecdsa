d403d5e07095e6ccb6e0b6e06266d95b64142faa
Jakub Nowakowski <jakub.nowakowski@akena.co>
2019-05-01 15:51:31 +0200

    Chain interface
    Chain interface to interact with a chain. Function to Broadcast a
    transaction.

---
+++ pkg/chain/chain.go
@@ -0,0 +1,10 @@
+// Package chain contains interface for interaction with a blockchain.
+package chain
+
+// Interface is an interface that provides ability to interact with a blockchain.
+type Interface interface {
+	// BroadcastTransaction submits a transaction to a chain. It requires raw
+	// transaction to be provided in a format specific to a chain. It returns
+	// an unique identifier of the transaction.
+	BroadcastTransaction(rawTx string) (string, error)
+}
