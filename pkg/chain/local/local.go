7a6d01898a6830ce57476f3acde4e388e91e3642
Jakub Nowakowski <jakub.nowakowski@akena.co>
2019-05-08 17:48:44 +0200

    Updated local chain stub of PublishTransaction
    Perform validations and register transaction in a map.

--- pkg/chain/local/local.go
+++ pkg/chain/local/local.go
@@ -3,26 +3,130 @@
 package local
 
 import (
-	"crypto/sha256"
 	"encoding/hex"
 	"fmt"
 
+	"github.com/btcsuite/btcd/txscript"
+	"github.com/btcsuite/btcd/wire"
 	"github.com/keep-network/keep-tecdsa/pkg/chain"
+	"github.com/keep-network/keep-tecdsa/pkg/utils"
 )
 
-type localChain struct{}
+const initialTransaction = "0100000001f24d19b6980927dbe47c30fd13b1cc12e56a11cc019efed67a1b4d3937b74bab010000006a47304402201711a033c1b829719716c81419294214a7fce0f0f1f9f51b6821ca3a5beebbdd022059b7bdd0bf1fe08aa4b4654360732d2a1f97c602b2e198a41e7bc53d81376c9a0121028896955d043b5a43957b21901f2cce9f0bfb484531b03ad6cd3153e45e73ee2effffffff022823000000000000160014d849b1e1cede2ac7d7188cf8700e97d6975c91c4b2f9fd00000000001976a914d849b1e1cede2ac7d7188cf8700e97d6975c91c488ac00000000"
 
-// PublishTransaction is a stub implementation which calculates a SHA-256 hash
-// for given string and returns it encoded to a hexadecimal string.
+type localChain struct {
+	transactions map[string]*wire.MsgTx
+}
+
+// PublishTransaction performs validation on a transaction encoded to hexadecimal
+// bitcoin format and stores it on local chain transactions map. It returns
+// transaction hash as an unique identifier of the transaction.
 func (l *localChain) PublishTransaction(rawTx string) (string, error) {
 	if rawTx == "" {
 		return "", fmt.Errorf("empty transaction provided")
 	}
-	hash := sha256.Sum256([]byte(rawTx))
-	return hex.EncodeToString(hash[:]), nil
+
+	rawTxBytes, err := hex.DecodeString(rawTx)
+	if err != nil {
+		return "", fmt.Errorf("cannot decode string [%s]", err)
+	}
+	msgTx, err := utils.DeserializeTransaction(rawTxBytes)
+	if err != nil {
+		return "", fmt.Errorf("cannot deserialize transaction [%s]", err)
+	}
+
+	// Check for duplicate.
+	txHash := msgTx.TxHash().String()
+	if _, ok := l.transactions[txHash]; ok {
+		return "", fmt.Errorf(
+			"transaction already registered on chain for hash [%s]",
+			txHash,
+		)
+	}
+
+	// Validate transaction according to bitcoin rules.
+	previousTxHash := msgTx.TxIn[0].PreviousOutPoint.Hash.String()
+	previousTx, ok := l.transactions[previousTxHash]
+	if !ok {
+		return "", fmt.Errorf(
+			"previous transaction not found for hash [%s]",
+			previousTxHash,
+		)
+	}
+
+	previousOutputIndex := msgTx.TxIn[0].PreviousOutPoint.Index
+	previousOutputScript := hex.EncodeToString(previousTx.TxOut[previousOutputIndex].PkScript)
+	previousOutputAmout := previousTx.TxOut[previousOutputIndex].Value
+
+	if err := ValidateTransaction(
+		previousOutputScript,
+		previousOutputAmout,
+		msgTx,
+	); err != nil {
+		return "", err
+	}
+
+	// Register transaction on chain.
+	l.transactions[txHash] = msgTx
+
+	return txHash, nil
 }
 
 // Connect returns a stub implementation of the chain interface.
 func Connect() chain.Interface {
-	return &localChain{}
+	initialTx := initialTx()
+	return &localChain{
+		transactions: map[string]*wire.MsgTx{
+			initialTx.TxHash().String(): initialTx,
+		},
+	}
+}
+
+// ValidateTransaction verifies if a transaction fulfills all the requirements
+// specified by bitcoin's StandardVerifyFlags.
+func ValidateTransaction(
+	previousOutputScript string,
+	previousOutputAmout int64,
+	transaction *wire.MsgTx) error {
+	if len(transaction.TxIn) != 1 {
+		return fmt.Errorf("only transactions with one input are supported")
+	}
+	inputIndex := 0
+
+	subscript, err := hex.DecodeString(previousOutputScript)
+	if err != nil {
+		return fmt.Errorf("source subscript decoding failed [%s]", err)
+	}
+
+	validationEngine, err := txscript.NewEngine(
+		subscript,
+		transaction,
+		inputIndex,
+		txscript.StandardVerifyFlags,
+		nil,
+		nil,
+		previousOutputAmout,
+	)
+	if err != nil {
+		return fmt.Errorf(
+			"cannot create validation engine [%s]",
+			err,
+		)
+	}
+
+	if err := validationEngine.Execute(); err != nil {
+		return fmt.Errorf(
+			"transaction validation failed [%s]",
+			err,
+		)
+	}
+
+	return nil
+}
+
+func initialTx() *wire.MsgTx {
+	txString := initialTransaction
+	txBytes, _ := hex.DecodeString(txString)
+	tx, _ := utils.DeserializeTransaction(txBytes)
+	return tx
 }
--- pkg/chain/local/local_test.go
+++ pkg/chain/local/local_test.go
@@ -1,9 +1,19 @@
 package local
 
 import (
+	"encoding/hex"
 	"fmt"
 	"reflect"
 	"testing"
+
+	"github.com/btcsuite/btcd/chaincfg/chainhash"
+	"github.com/btcsuite/btcd/wire"
+	"github.com/keep-network/keep-tecdsa/pkg/utils"
+)
+
+const (
+	validTxRaw  = "0100000000010199dffb02e8f3e3f8053eecf6110a95f77ba658690dfc3a447b7e52cf34ca135e0000000000ffffffff02581b000000000000160014d849b1e1cede2ac7d7188cf8700e97d6975c91c4e8030000000000001976a914d849b1e1cede2ac7d7188cf8700e97d6975c91c488ac02483045022100ecadce07f5c9d84b4fa1b2728806135acd81ad9398c9673eeb4e161d42364b92022076849daa2108ed2a135d16eb9e103c5819db014ea2bad5c92f4aeecf47bf9ac80121028896955d043b5a43957b21901f2cce9f0bfb484531b03ad6cd3153e45e73ee2e00000000"
+	validTxHash = "ec367c260ead9e3c91583175f35382e22b66df6d59fd0aac175bb36519b664f7"
 )
 
 func TestPublishTransaction(t *testing.T) {
@@ -15,13 +25,28 @@ func TestPublishTransaction(t *testing.T) {
 		expectedError  error
 	}{
 		"successful transaction publication": {
-			rawTx:          "0123456789ABCDEF",
-			expectedResult: "2125b2c332b1113aae9bfc5e9f7e3b4c91d828cb942c2df1eeb02502eccae9e9",
+			rawTx:          validTxRaw,
+			expectedResult: validTxHash,
+		},
+		"previous transaction not found for hash": {
+			rawTx:          txInvalidMissingPreviousHash(),
+			expectedResult: "",
+			expectedError:  fmt.Errorf("previous transaction not found for hash [b7243a2bb8721011c226d15d9a3495032f218bf93e2de23acf2d752b255dc8c5]"),
+		},
+		"transaction already registered": {
+			rawTx:          initialTransaction,
+			expectedResult: "",
+			expectedError:  fmt.Errorf("transaction already registered on chain for hash [5e13ca34cf527e7b443afc0d6958a67bf7950a11f6ec3e05f8e3f3e802fbdf99]"),
 		},
 		"failed transaction publication": {
 			rawTx:          "",
 			expectedResult: "",
 			expectedError:  fmt.Errorf("empty transaction provided"),
+		},
+		"transaction validation failed - unsigned transaction": {
+			rawTx:          txUnsigned(),
+			expectedResult: "",
+			expectedError:  fmt.Errorf("transaction validation failed [should have exactly two items in witness, instead have 0]"),
 		},
 	}
 
@@ -46,4 +71,49 @@ func TestPublishTransaction(t *testing.T) {
 			}
 		})
 	}
+}
+
+func TestValidateTransaction(t *testing.T) {
+	previousOutputScript := "0014d849b1e1cede2ac7d7188cf8700e97d6975c91c4"
+	previousOutputAmout := int64(9000)
+	transaction, _ := hex.DecodeString(validTxRaw)
+
+	msgTx, err := utils.DeserializeTransaction(transaction)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if err := ValidateTransaction(
+		previousOutputScript,
+		previousOutputAmout,
+		msgTx,
+	); err != nil {
+		t.Errorf("transaction validation failed [%s]", err)
+	}
+}
+
+func txInvalidMissingPreviousHash() string {
+	msgTx := wire.NewMsgTx(wire.TxVersion)
+
+	hash := chainhash.DoubleHashH([]byte("invalid"))
+	outPoint := wire.NewOutPoint(&hash, 0)
+	txIn := wire.NewTxIn(outPoint, nil, nil)
+	msgTx.AddTxIn(txIn)
+
+	serializedTx, _ := utils.SerializeTransaction(msgTx)
+
+	return hex.EncodeToString(serializedTx)
+}
+
+func txUnsigned() string {
+	msgTx := wire.NewMsgTx(wire.TxVersion)
+
+	previousHash, _ := chainhash.NewHashFromStr("5e13ca34cf527e7b443afc0d6958a67bf7950a11f6ec3e05f8e3f3e802fbdf99")
+	outPoint := wire.NewOutPoint(previousHash, 0)
+	txIn := wire.NewTxIn(outPoint, nil, nil)
+	msgTx.AddTxIn(txIn)
+
+	serializedTx, _ := utils.SerializeTransaction(msgTx)
+
+	return hex.EncodeToString(serializedTx)
 }
