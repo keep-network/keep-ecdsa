73671b49d3dd5d4f303d679710fde4af7976f2c8
Jakub Nowakowski <jakub.nowakowski@akena.co>
2019-05-08 17:37:39 +0200

    Convert public key to WP2PKH address
    Address is calculated according to BIP-173. First calculate witness
    program as ripemd160(sha256(compressedPublicKey)) and pass it to btcutil
    function to calculate the address.

---
+++ pkg/btc/address.go
@@ -0,0 +1,33 @@
+package btc
+
+import (
+	"github.com/btcsuite/btcd/btcec"
+	"github.com/btcsuite/btcutil"
+
+	"github.com/btcsuite/btcd/chaincfg"
+	"github.com/keep-network/keep-tecdsa/pkg/sign"
+)
+
+// PublicKeyToWitnessPubKeyHashAddress convert public key to bitcoin Witness
+// Public Key Hash Address. It calculates the address according to [BIP-173].
+// First a public key is serialized to compressed format. Next witness program
+// is calculates as RIPEMD-160 hash over SHA-256 hash of compressed public key.
+// Finally bitcoin address is created for a specific network.
+//
+// [BIP-173]: https://github.com/bitcoin/bips/blob/master/bip-0173.mediawiki
+func PublicKeyToWitnessPubKeyHashAddress(
+	publicKey *sign.PublicKey,
+	netParams *chaincfg.Params,
+) (string, error) {
+	compressedPublicKey := (*btcec.PublicKey)(publicKey).SerializeCompressed()
+
+	// Hash ripemd160(sha256(compressedPublicKey)).
+	witnessProgram := btcutil.Hash160(compressedPublicKey)
+
+	address, err := btcutil.NewAddressWitnessPubKeyHash(witnessProgram, netParams)
+	if err != nil {
+		return "", err
+	}
+
+	return address.EncodeAddress(), nil
+}
