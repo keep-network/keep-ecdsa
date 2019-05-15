a0239a772061ad5586705c85fd02ef7a80e61a93
Jakub Nowakowski <jakub.nowakowski@akena.co>
2019-05-15 11:22:24 +0200

    KeepTECDSAGroup -> KeepRegistry

--- solidity/contracts/KeepRegistry.sol
+++ solidity/contracts/KeepRegistry.sol
@@ -1,24 +1,24 @@
 pragma solidity ^0.5.4;
 
-/// @title Keep TECDSA Group
-/// @notice Contract handling TECDSA groups.
+/// @title Keep Registry
+/// @notice Contract handling Keeps.
 /// @dev TODO: This is a stub contract - needs to be implemented.
-contract KeepTECDSAGroup {
+contract KeepRegistry {
     uint256 internal requestIDSeq = 1;
     uint256 internal groupIDSeq = 1;
 
-    event GroupRequested(uint256 requestID, uint256 groupID, uint32 groupSize, uint32 dishonestThreshold); 
+    event BondedECDSAKeepRequested(uint256 requestID, uint256 groupID, uint32 groupSize, uint32 dishonestThreshold); 
  
-    /// @notice Request a new TECDSA Group generation.
+    /// @notice Request a new bonded ECDSA Keep.
     /// @dev TODO: This is a stub function - needs to be implemented.
     /// @param _groupSize Number of members in the group.
     /// @param _dishonestThreshold Maximum number of dishonest group members.
     /// @return Unique request identifier.
-    function requestGroup(uint32 _groupSize, uint32 _dishonestThreshold) public payable returns (uint256 _requestID) {
+    function requestBondedECDSAKeep(uint32 _groupSize, uint32 _dishonestThreshold) public payable returns (uint256 _requestID) {
         _requestID = requestIDSeq++;
         uint256 _groupID = groupIDSeq++;
 
-        emit GroupRequested(_requestID, _groupID, _groupSize, _dishonestThreshold);
+        emit BondedECDSAKeepRequested(_requestID, _groupID, _groupSize, _dishonestThreshold);
 
         return _requestID;
     }
