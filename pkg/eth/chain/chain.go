856b755018eb83d004b83680ce3991f6371aec5e
Jakub Nowakowski <jakub.nowakowski@akena.co>
2019-05-17 15:20:36 +0200

    OnECDSAKeepRequested function in interface
    Function which registers callback for new ECDSA keep request
    notification.

--- pkg/eth/chain/chain.go
+++ pkg/eth/chain/chain.go
@@ -8,7 +8,13 @@ import (
 // Interface is an interface that provides ability to interact with ethereum
 // contracts.
 type Interface interface {
-	OnGroupRequested(
+	OnGroupRequested( // TODO: Remove it, it's replaced by OnECDSAKeepRequested
 		func(request *event.GroupRequested),
+	) (subscription.EventSubscription, error)
+
+	// OnECDSAKeepRequested is a callback that is invoked when an on-chain
+	// notification of a new ECDSA keep request is seen.
+	OnECDSAKeepRequested(
+		func(request *event.ECDSAKeepRequested),
 	) (subscription.EventSubscription, error)
 }
--- pkg/eth/event/event.go
+++ pkg/eth/event/event.go
@@ -1,11 +1,27 @@
+// Package event reflects structures of events emitted on an ethereum blockchain.
 package event
 
-import "math/big"
+import (
+	"math/big"
+
+	"github.com/ethereum/go-ethereum/common"
+)
+
+// MemberID is unique keep member identifier.
+// TODO: Consider changing to different type e.g. address.
+type MemberID = big.Int
 
 // GroupRequested is an event emitted on new group creation request.
-type GroupRequested struct {
+type GroupRequested struct { // TODO: Remove, it's replaced by ECDSAKeepRequested
 	RequestID          *big.Int
 	GroupID            *big.Int // currently single Signer ID
 	GroupSize          uint32   // n
 	DishonestThreshold uint32   // m
+}
+
+// ECDSAKeepRequested is an event emitted on a new keep creation request.
+type ECDSAKeepRequested struct {
+	KeepAddress        common.Address // keep contract address
+	MemberIDs          []*MemberID    // keep members IDs
+	DishonestThreshold *big.Int       // maximum number of dishonest members `m`
 }
