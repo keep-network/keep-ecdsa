6f60a07c35188f21c89fb7a360d827eff51e13d0
Jakub Nowakowski <jakub.nowakowski@akena.co>
2019-05-17 15:24:53 +0200

    Implement OnECDSAKeepRequested for ethereum
    Implements callback registration for OnECDSAKeepRequested on ethereum.

--- pkg/chain/eth/ethereum/ethereum.go
+++ pkg/chain/eth/ethereum/ethereum.go
@@ -30,3 +30,24 @@ func (ec *EthereumChain) OnGroupRequested(
 		},
 	)
 }
+
+// OnECDSAKeepRequested is a callback that is invoked when an on-chain
+// notification of a new ECDSA keep request is seen.
+func (ec *EthereumChain) OnECDSAKeepRequested(
+	handle func(groupRequested *event.ECDSAKeepRequested),
+) (subscription.EventSubscription, error) {
+	return ec.watchECDSAKeepRequested(
+		func(
+			chainEvent *abi.ECDSAKeepFactoryECDSAKeepRequested,
+		) {
+			handle(&event.ECDSAKeepRequested{
+				KeepAddress:        chainEvent.KeepAddress,
+				MemberIDs:          chainEvent.KeepMembers,
+				DishonestThreshold: chainEvent.DishonestThreshold,
+			})
+		},
+		func(err error) error {
+			return fmt.Errorf("keep requested callback failed: [%s]", err)
+		},
+	)
+}
