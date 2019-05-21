7c78aa0ddac79aaf1c8b22a0abf7937f08ce3003
Jakub Nowakowski <jakub.nowakowski@akena.co>
2019-05-21 16:25:37 +0200

    Remove MemberID

--- pkg/chain/eth/event.go
+++ pkg/chain/eth/event.go
@@ -1,14 +1,8 @@
 package eth
 
 import (
-	"math/big"
-
 	"github.com/ethereum/go-ethereum/common"
 )
-
-// MemberID is unique keep member identifier.
-// TODO: Consider changing to different type e.g. address.
-type MemberID = big.Int
 
 // ECDSAKeepCreatedEvent is an event emitted on a new keep creation.
 type ECDSAKeepCreatedEvent struct {
