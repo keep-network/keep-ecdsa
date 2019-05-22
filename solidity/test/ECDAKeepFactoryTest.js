ee07d825803ed751153f0277756058115365cdee
Jakub Nowakowski <jakub.nowakowski@akena.co>
2019-05-22 08:57:39 +0200

    Unit test for ECDSAKeepFactory

--- solidity/test/ECDAKeepFactoryTest.js
+++ solidity/test/ECDAKeepFactoryTest.js
@@ -1,27 +1,44 @@
 var ECDSAKeepFactory = artifacts.require('ECDSAKeepFactory');
 
 contract("ECDSAKeepFactory test", async accounts => {
-    it("check event", async () => {
+
+    it("ECDSAKeepCreated event emission", async () => {
         let blockNumber = await web3.eth.getBlock("latest").number
 
         let keepFactory = await ECDSAKeepFactory.deployed();
 
-        console.log('Call createNewKeep');
-        let res = await keepFactory.createNewKeep.call(
+        let expectedKeepAddress = await keepFactory.createNewKeep.call(
             10, // uint256 _groupSize,
             5, // uint256 _honestThreshold,
             "0xbc4862697a1099074168d54A555c4A60169c18BD" // address _owner
-        );
+        ).catch((err) => {
+            console.log(`ecdsa keep creation for failed: ${err}`);
+        });
+
+        await keepFactory.createNewKeep(
+            10, // uint256 _groupSize,
+            5, // uint256 _honestThreshold,
+            "0xbc4862697a1099074168d54A555c4A60169c18BD" // address _owner
+        ).catch((err) => {
+            console.log(`ecdsa keep creation failed: ${err}`);
+        });
 
         let eventList = await keepFactory.getPastEvents('ECDSAKeepCreated', {
             fromBlock: blockNumber,
             toBlock: 'latest'
         })
 
-        // Just for debug print list of events
-        console.log(eventList)
+        assert.isTrue(
+            web3.utils.isAddress(expectedKeepAddress),
+            `keep address ${expectedKeepAddress} is not a valid address`,
+        );
 
         assert.equal(eventList.length, 1, "incorrect number of emitted events")
-        assert.equal(eventList[0].returnValues.keepAddress, res, "incorrect keep address in emitted event")
+
+        assert.equal(
+            eventList[0].returnValues.keepAddress,
+            expectedKeepAddress,
+            "incorrect keep address in emitted event",
+        )
     });
 });
