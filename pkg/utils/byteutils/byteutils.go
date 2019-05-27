f7534e41b0b2ea3057caec7e581fc06f7992cfa1
Jakub Nowakowski <jakub.nowakowski@akena.co>
2019-05-27 16:13:01 +0200

    Utils function to append zeros to bytes slice
    Function to append zeros to a bytes slice to make it 32 bytes long. It's
    copied from keep-core, we should consider extracting it from both to
    some utils project.

---
+++ pkg/utils/byteutils/byteutils.go
@@ -0,0 +1,25 @@
+// Package byteutils provides helper utilities for working with bytes
+package byteutils
+
+import (
+	"fmt"
+)
+
+// LeftPadTo32Bytes appends zeros to bytes slice to make it exactly 32 bytes long.
+// TODO: This is copied from keep-core. Consider extracting utils to a separate repo.
+func LeftPadTo32Bytes(bytes []byte) ([]byte, error) {
+	expectedByteLen := 32
+	if len(bytes) > expectedByteLen {
+		return nil, fmt.Errorf(
+			"cannot pad %v byte array to %v bytes", len(bytes), expectedByteLen,
+		)
+	}
+
+	result := make([]byte, 0)
+	if len(bytes) < expectedByteLen {
+		result = make([]byte, expectedByteLen-len(bytes))
+	}
+	result = append(result, bytes...)
+
+	return result, nil
+}
