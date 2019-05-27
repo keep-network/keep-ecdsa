0bc26ef2e97361db177bf74c2190536e398f5316
Jakub Nowakowski <jakub.nowakowski@akena.co>
2019-05-27 11:39:41 +0200

    Update ethereum chain interface
    Ethereum chain interface has to cover Keep public key submission. Main
    Handle interface gives an ability to get ECDSA Keep interface deployed
    under a specific address.

--- pkg/chain/eth/chain.go
+++ pkg/chain/eth/chain.go
@@ -3,15 +3,30 @@
 package eth
 
 import (
+	"github.com/ethereum/go-ethereum/common"
 	"github.com/keep-network/keep-core/pkg/subscription"
 )
 
-// Interface is an interface that provides ability to interact with ethereum
-// contracts.
-type Interface interface {
+// Handle is an interface that provides ability to interact with the blockchain.
+type Handle interface {
+	// ECDSAKeepFactory returns an interface for ECDSA Keep Factory contract.
+	ECDSAKeepFactory() (ECDSAKeepFactory, error)
+	// ECDSAKeep returns an interface for ECDSA Keep contract of given address.
+	ECDSAKeep(address *common.Address) (ECDSAKeep, error)
+}
+
+// ECDSAKeepFactory is an interface that provides ability to interact with
+// ECDSA Keep Factory contract deployed on the blockchain.
+type ECDSAKeepFactory interface {
 	// OnECDSAKeepCreated is a callback that is invoked when an on-chain
 	// notification of a new ECDSA keep creation is seen.
 	OnECDSAKeepCreated(
 		func(request *ECDSAKeepCreatedEvent),
 	) (subscription.EventSubscription, error)
+}
+
+// ECDSAKeep is an interface that provides ability to interact with ECDSA Keep
+// contract deployed on the blockchain.
+type ECDSAKeep interface {
+	SubmitKeepPublicKey(publicKey [64]byte) error // TODO: Add promise *async.KeepPublicKeySubmissionPromise
 }
