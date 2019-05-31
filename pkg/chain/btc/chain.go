d464d1a0ea6d442f2d1ae0b1579be08ef4406053
Jakub Nowakowski <jakub.nowakowski@akena.co>
2019-05-31 15:04:05 +0200

    Get current target added to btc chain api
    Added function to bitcoin interface to get difficulty target bits from
    the chain.

--- pkg/chain/btc/chain.go
+++ pkg/chain/btc/chain.go
@@ -7,4 +7,8 @@ type Interface interface {
 	// transaction to be provided in a format specific to a chain. It returns
 	// an unique identifier of the transaction.
 	PublishTransaction(rawTx string) (string, error)
+
+	// GetCurrentTargetBits gets difficulty target bits from latest block published
+	// to the blockchain.
+	GetCurrentTargetBits() (int, error)
 }
