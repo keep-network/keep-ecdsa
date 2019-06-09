46e8a6a0ffc5448d4ec810d595081a85842cc899
liamzebedee <liamzebedee@yahoo.com.au>
2019-06-09 17:08:56 +0200

    Move ECDSAKeep tests into one file

--- solidity/test/ECDSAKeepTest.js
+++ solidity/test/ECDSAKeepTest.js
@@ -1,6 +1,49 @@
-var ECDSAKeep = artifacts.require('ECDSAKeep');
+const ECDSAKeepFactory = artifacts.require('./ECDSAKeepFactory.sol');
+const ECDSAKeep = artifacts.require('./ECDSAKeep.sol');
 
-contract("ECDSAKeep test", async accounts => {
+const truffleAssert = require('truffle-assertions');
+
+contract('ECDSAKeep', function(accounts) {
+  describe("#constructor", async function() {
+    it('succeeds', async () => {
+        let owner = accounts[0]
+        let members = [ owner ];
+        let threshold = 1;
+
+        let instance = await ECDSAKeep.new(
+            owner,
+            members,
+            threshold
+        );
+
+        expect(instance.address).to.be.not.empty;
+    })
+  });
+
+  describe('#sign', async function () {
+    let instance;
+
+    before(async () => {
+        let owner = accounts[0]
+        let members = [ owner ];
+        let threshold = 1;
+
+        instance = await ECDSAKeep.new(
+            owner,
+            members,
+            threshold
+        );
+    });
+
+    it('emits event', async () => {
+        let res = await instance.sign('0x00')
+        truffleAssert.eventEmitted(res, 'SignatureRequested', (ev) => {
+            return ev.digest == '0x00'
+        });
+    })
+  })
+
+  describe("public key", () => {
     let expectedPublicKey = web3.utils.hexToBytes("0x67656e657261746564207075626c6963206b6579")
     let owner = "0xbc4862697a1099074168d54A555c4A60169c18BD";
     let members = ["0x774700a36A96037936B8666dCFdd3Fb6687b08cb"];
@@ -33,4 +76,6 @@ contract("ECDSAKeep test", async accounts => {
             "incorrect public key"
         )
     });
+  })
+
 });
