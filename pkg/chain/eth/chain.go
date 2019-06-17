9b2d8f986aceaae9dea791c4aaf16ab455557cef
liamzebedee <liamzebedee@yahoo.com.au>
2019-06-17 13:48:35 +0200

    Reference 'keep' in OnSignatureRequested doc

--- pkg/chain/eth/chain.go
+++ pkg/chain/eth/chain.go
@@ -20,7 +20,7 @@ type Interface interface {
 	) (subscription.EventSubscription, error)
 
 	// OnSignatureRequested is a callback that is invoked when an on-chain
-	// notification of a new signing request is seen.
+	// notification of a new signing request for a given keep is seen.
 	OnSignatureRequested(
 		keepAddress common.Address,
 		callback func(request *SignatureRequestedEvent),
