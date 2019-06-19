323c53338e6797e19115451679756a38589a19c7
liamzebedee <liamzebedee@yahoo.com.au>
2019-06-19 14:16:32 +0200

    Implemented requestSignature, passing test

--- pkg/chain/eth/local/ecdsa_keep.go
+++ pkg/chain/eth/local/ecdsa_keep.go
@@ -1,11 +1,32 @@
 package local
 
 import (
-	"github.com/ethereum/go-ethereum/common"
-	// "github.com/keep-network/keep-tecdsa/pkg/chain/eth"
+	"github.com/keep-network/keep-tecdsa/pkg/chain/eth"
+
+	"fmt"
 )
 
-func (c *localChain) requestSignature(keepAddress common.Address, digest []byte) {
+func (c *localChain) requestSignature(address eth.KeepAddress, digest []byte) error {
 	c.handlerMutex.Lock()
 	defer c.handlerMutex.Unlock()
+
+	keep, ok := c.keeps[address]
+	if !ok {
+		return fmt.Errorf(
+			"keep not found for address [%s]",
+			address.String(),
+		)
+	}
+
+	signatureRequestedEvent := &eth.SignatureRequestedEvent{
+		Digest: digest,
+	}
+
+	for _, handler := range keep.signatureRequestedHandlers {
+		go func(handler func(event *eth.SignatureRequestedEvent), signatureRequestedEvent *eth.SignatureRequestedEvent) {
+			handler(signatureRequestedEvent)
+		}(handler, signatureRequestedEvent)
+	}
+
+	return nil
 }
