d1bf9afab38844ce6491977cc87f349b2b94d582
liamzebedee <liamzebedee@yahoo.com.au>
2019-07-09 18:16:07 +0200

    Added TestRequestSignature

--- pkg/chain/eth/local/ecdsa_keep_test.go
+++ pkg/chain/eth/local/ecdsa_keep_test.go
@@ -1,7 +1,10 @@
 package local
 
 import (
+	"bytes"
+	"context"
 	"testing"
+	"time"
 
 	"github.com/keep-network/keep-tecdsa/pkg/chain/eth"
 )
@@ -27,6 +30,38 @@ func TestRequestSignatureNonexistentKeep(t *testing.T) {
 // 	// handler := func(event *eth.SignatureRequestedEvent)
 // }
 
-// func TestRequestSignature(t *testing.T) {
+func TestRequestSignature(t *testing.T) {
+	ctx, cancel := context.WithTimeout(context.Background(), 1*time.Second)
+	defer cancel()
 
-// }
+	chain := initializeLocalChain()
+	keepAddress := eth.KeepAddress([20]byte{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1})
+	digest := []byte{1}
+
+	eventEmitted := make(chan *eth.SignatureRequestedEvent)
+
+	handler := func(event *eth.SignatureRequestedEvent) {
+		eventEmitted <- event
+	}
+
+	chain.createKeep(keepAddress)
+	chain.keeps[keepAddress].signatureRequestedHandlers[0] = handler
+
+	err := chain.requestSignature(keepAddress, digest)
+	if err != nil {
+		t.Fatalf("unexpected error: %s", err.Error())
+	}
+
+	select {
+	case event := <-eventEmitted:
+		if !bytes.Equal(event.Digest, digest) {
+			t.Errorf(
+				"unexpected digest from signature request\nexpected: %x\nactual:   %x\n",
+				digest,
+				event.Digest,
+			)
+		}
+	case <-ctx.Done():
+		t.Fatal(ctx.Err())
+	}
+}
