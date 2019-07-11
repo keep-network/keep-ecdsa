0e4843f3a5266eb6c6f3407956f2d00dfd04ce69
liamzebedee <liamzebedee@yahoo.com.au>
2019-07-11 17:53:21 +0200

    Add TestCreateKeepDuplicate

--- pkg/chain/eth/local/ecdsa_keep_factory_test.go
+++ pkg/chain/eth/local/ecdsa_keep_factory_test.go
@@ -2,10 +2,33 @@ package local
 
 import (
 	"bytes"
+	"fmt"
+	"reflect"
 	"testing"
 
 	"github.com/keep-network/keep-tecdsa/pkg/chain/eth"
 )
+
+func TestCreateKeepDuplicate(t *testing.T) {
+	chain := initializeLocalChain()
+
+	keepAddress := eth.KeepAddress([20]byte{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1})
+	expectedError := fmt.Errorf("keep already exists for address [0x0000000000000000000000000000000000000001]")
+
+	err := chain.createKeep(keepAddress)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	err = chain.createKeep(keepAddress)
+	if !reflect.DeepEqual(err, expectedError) {
+		t.Fatalf(
+			"unexpected error\nexpected: [%v]\nactual:   [%v]",
+			expectedError,
+			err.Error(),
+		)
+	}
+}
 
 func TestCreateKeep(t *testing.T) {
 	chain := initializeLocalChain()
