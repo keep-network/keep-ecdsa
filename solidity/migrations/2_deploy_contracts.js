182d5fc89c5dd7bc351d01e665612db89435935f
Jakub Nowakowski <jakub.nowakowski@akena.co>
2019-07-16 13:17:26 +0200

    Update truffle contract deployment script

--- solidity/migrations/2_deploy_contracts.js
+++ solidity/migrations/2_deploy_contracts.js
@@ -1,7 +1,19 @@
 const ECDSAKeepFactory = artifacts.require("./ECDSAKeepFactory.sol");
+const ECDSAKeepVendor = artifacts.require("./ECDSAKeepVendor.sol");
 const KeepRegistry = artifacts.require("./KeepRegistry.sol");
 
 module.exports = async function (deployer) {
-    await deployer.deploy(ECDSAKeepFactory);
-    await deployer.deploy(KeepRegistry, ECDSAKeepFactory.address);
-};
+    await deployer.deploy(ECDSAKeepFactory)
+    let ecdsaKeepFactory = await ECDSAKeepFactory.deployed()
+
+    await deployer.deploy(ECDSAKeepVendor)
+        .then((instance) => {
+            instance.registerFactory(ecdsaKeepFactory.address)
+        })
+    let ecdsaKeepVendor = await ECDSAKeepVendor.deployed()
+
+    await deployer.deploy(KeepRegistry)
+        .then((instance) => {
+            instance.setKeepTypeVendor('ECDSAKeep', ecdsaKeepVendor.address)
+        })
+}
