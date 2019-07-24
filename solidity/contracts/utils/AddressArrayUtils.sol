80a23decaad4dc9beeb4dad6379add2335367e29
Jakub Nowakowski <jakub.nowakowski@akena.co>
2019-07-24 15:08:37 +0200

    Deleted removeAddress from array utils

--- solidity/contracts/utils/AddressArrayUtils.sol
+++ solidity/contracts/utils/AddressArrayUtils.sol
@@ -13,22 +13,4 @@ library AddressArrayUtils {
         }
         return false;
     }
-
-    function removeAddress(address[] storage self, address _addressToRemove)
-        internal
-        returns (address[] storage)
-    {
-        for (uint i = 0; i < self.length; i++) {
-            // If address is found in array.
-            if (_addressToRemove == self[i]) {
-                // Delete element at index and shift array.
-                for (uint j = i; j < self.length-1; j++) {
-                    self[j] = self[j+1];
-                }
-                self.length--;
-                i--;
-            }
-        }
-        return self;
-    }
 }
