0d5ae9f9c8e5d36d5812875049eeda1e9abffa7b
Jakub Nowakowski <jakub.nowakowski@akena.co>
2019-08-29 13:47:26 +0200

    Wait for event instead of sleep
    We are now waiting for a signature submission event instead of just
    sleeping 1 second.

--- solidity/test/integration/keep_signing.js
+++ solidity/test/integration/keep_signing.js
@@ -1,6 +1,8 @@
 const ECDSAKeepFactory = artifacts.require('./ECDSAKeepFactory.sol');
 const ECDSAKeep = artifacts.require('./ECDSAKeep.sol');
 
+const waitForEvent = require('../helpers/waitForEvent')
+
 // ECDSAKeep signing.
 // Creates a new keep, requests signature for a digest and gets the signature
 // submitted to the chain.
@@ -8,12 +10,10 @@ module.exports = async function () {
     let accounts = await web3.eth.getAccounts();
     let factory = await ECDSAKeepFactory.deployed();
 
-    // Create new keep
+    // Create new keep.
     let groupSize = 1;
     let honestThreshold = 1;
     let owner = accounts[1];
-
-    const startBlockNumber = await web3.eth.getBlock('latest').number
 
     let openKeepTx = await factory.openKeep(
         groupSize,
@@ -30,32 +30,34 @@ module.exports = async function () {
 
     const keep = await ECDSAKeep.at(keepAddress)
 
-    // Sign digest
+    // Register event listener to wait for an event emitted after signature is
+    // submitted by an off-chain keep client.
+    const eventPromise = waitForEvent(keep.SignatureSubmitted())
+        .catch(err => {
+            console.error(`Event watching failed: [${err}]`)
+            process.exit(1)
+        })
+
+    // Sign digest.
     const digest = '0xca071ca92644f1f2c4ae1bf71b6032e5eff4f78f3aa632b27cbc5f84104a32da'
     console.log('Sign digest:', digest)
 
-    let signTx = await keep.sign(digest, { from: owner })
+    await keep.sign(digest, { from: owner })
         .catch(err => {
             console.error(`failed signing: [${err}]`)
             process.exit(1)
         })
 
-    // Give off-chain client some time to calculate and submit a signature.
-    await sleep(1000);
+    const receivedSignatureEvent = await eventPromise
 
-    // Get signature submitted by off-chain keep client
-    eventList = await keep.getPastEvents('SignatureSubmitted', {
-        fromBlock: startBlockNumber,
-        toBlock: 'latest',
-    })
-
-    if (eventList[0].returnValues.digest != digest) {
-        console.error(`unexpected digest: ${eventList[0].returnValues.digest}`)
+    if (receivedSignatureEvent.returnValues.digest != digest) {
+        console.error(`unexpected digest: ${receivedSignatureEvent.returnValues.digest}`)
         process.exit(1)
     }
+
     // TODO: Validate signature.
 
-    console.log(`Received signature:\nR: ${eventList[0].returnValues.r}\nS: ${eventList[0].returnValues.s}`)
+    console.log(`Received signature:\nR: ${receivedSignatureEvent.returnValues.r}\nS: ${receivedSignatureEvent.returnValues.s}`)
 
     process.exit(0)
 }
