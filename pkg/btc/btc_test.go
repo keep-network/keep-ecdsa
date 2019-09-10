0f92ed7c7c6b4a31cfc65a210b96dc1138b678fe
Jakub Nowakowski <jakub.nowakowski@akena.co>
2019-09-10 16:56:35 +0200

    Signer initialization update for btc test
    We need to update signer initialziation to use ethereum's secp256k1
    curve implementation which is required by signing process.

--- pkg/btc/btc_test.go
+++ pkg/btc/btc_test.go
@@ -8,6 +8,7 @@ import (
 	"testing"
 
 	"github.com/btcsuite/btcutil"
+	"github.com/ethereum/go-ethereum/crypto/secp256k1"
 	"github.com/keep-network/keep-tecdsa/internal/testdata"
 	"github.com/keep-network/keep-tecdsa/pkg/chain/btc/local"
 	"github.com/keep-network/keep-tecdsa/pkg/sign"
@@ -17,7 +18,7 @@ func TestSignAndPublishTransaction(t *testing.T) {
 	witnessSignatureHash, _ := hex.DecodeString(testdata.ValidTx.WitnessSignatureHash)
 	transactionPreimage, _ := hex.DecodeString(testdata.ValidTx.UnsignedRaw)
 
-	signer, err := signer()
+	signer, err := newTestSigner()
 	if err != nil {
 		t.Fatal(err)
 	}
@@ -29,12 +30,20 @@ func TestSignAndPublishTransaction(t *testing.T) {
 	}
 }
 
-func signer() (*sign.Signer, error) {
+func newTestSigner() (*sign.Signer, error) {
+	curve := secp256k1.S256()
+
 	wif, err := btcutil.DecodeWIF("923CjseKgQf7Xz185dmYUJer9i8jsb9Cd18Rtec4DFKeiBZg3wi")
 	if err != nil {
 		return nil, fmt.Errorf("cannot decode WIF [%s]", err)
 	}
-	privateKey := (*ecdsa.PrivateKey)(wif.PrivKey)
+
+	k := wif.PrivKey.D
+
+	privateKey := new(ecdsa.PrivateKey)
+	privateKey.PublicKey.Curve = curve
+	privateKey.D = k
+	privateKey.PublicKey.X, privateKey.PublicKey.Y = curve.ScalarBaseMult(k.Bytes())
 
 	return sign.NewSigner(privateKey), nil
 }
