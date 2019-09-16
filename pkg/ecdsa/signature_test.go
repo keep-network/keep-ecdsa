ec2a41f6f19000c062782fe3b500d5c127bf54ad
Jakub Nowakowski <jakub.nowakowski@akena.co>
2019-09-16 12:58:52 +0200

    Improve tests

--- pkg/ecdsa/signature_test.go
+++ pkg/ecdsa/signature_test.go
@@ -18,98 +18,70 @@ func TestCalculateSignature(t *testing.T) {
 	signer := newTestSigner()
 	hash, _ := hex.DecodeString("54a6483b8aca55c9df2a35baf71d9965ddfd623468d81d51229bd5eb7d1e1c1b")
 
-	for i := 0; i < 100; i++ {
-		signature, err := signer.CalculateSignature(crand.Reader, hash)
-		if err != nil {
-			t.Fatal(err)
-		}
-
-		serializedSignature, err := serializeSignature(signature)
-		if err != nil {
-			t.Fatal(err)
-		}
-
-		publicKey, err := crypto.SigToPub(hash, serializedSignature)
-		if err != nil {
-			t.Fatal(err)
-		}
-
-		if signer.PublicKey().X.Cmp(publicKey.X) != 0 ||
-			signer.PublicKey().Y.Cmp(publicKey.Y) != 0 {
-			t.Fatalf(
-				"unexpected public key:\nexpected: [%x]\nactual:   [%x]\n",
-				signer.PublicKey(),
-				publicKey,
-			)
-		}
-	}
-
-func TestFindRecoveryID(t *testing.T) {
-	signer := newTestSigner()
-	sigR, _ := new(big.Int).SetString("9b32c3623b6a16e87b4d3a56cd67c666c9897751e24a51518136185403b1cba2", 16)
-	sigS, _ := new(big.Int).SetString("90838891021e1c7d0d1336613f24ecab703dee5ff1b6c8881bccc2c011606a35", 16)
-	hash, _ := hex.DecodeString("54a6483b8aca55c9df2a35baf71d9965ddfd623468d81d51229bd5eb7d1e1c1b")
-
-	expectedRecoveryID := 1
-
-	recoveryID, err := signer.findRecoveryID(sigR, sigS, hash)
+	signature, err := signer.CalculateSignature(crand.Reader, hash)
 	if err != nil {
 		t.Fatal(err)
 	}
 
-	if recoveryID != expectedRecoveryID {
+	serializedSignature, err := serializeSignature(signature)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	publicKey, err := crypto.SigToPub(hash, serializedSignature)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if signer.PublicKey().X.Cmp(publicKey.X) != 0 ||
+		signer.PublicKey().Y.Cmp(publicKey.Y) != 0 {
 		t.Fatalf(
-			"unexpected recovery ID:\nexpected: [%d]\nactual:   [%d]\n",
-			expectedRecoveryID,
-			recoveryID,
+			"unexpected public key:\nexpected: [%x]\nactual:   [%x]\n",
+			signer.PublicKey(),
+			publicKey,
 		)
 	}
 }
 
-func TestRecoverPublicKeyFromSignature(t *testing.T) {
+func TestFindRecoveryID(t *testing.T) {
 	signer := newTestSigner()
-	sigR, _ := new(big.Int).SetString("9b32c3623b6a16e87b4d3a56cd67c666c9897751e24a51518136185403b1cba2", 16)
-	sigS, _ := new(big.Int).SetString("90838891021e1c7d0d1336613f24ecab703dee5ff1b6c8881bccc2c011606a35", 16)
 	hash, _ := hex.DecodeString("54a6483b8aca55c9df2a35baf71d9965ddfd623468d81d51229bd5eb7d1e1c1b")
 
-	x0, _ := new(big.Int).SetString("657282135ed640b0f5a280874c7e7ade110b5c3db362e0552e6b7fff2cc84593", 16)
-	y0, _ := new(big.Int).SetString("28850039b734db7629c31567d7fc5677536b7fc504e967dc11f3f2289d3d4051", 16)
-	expectedPublicKey0 := &PublicKey{signer.Curve(), x0, y0}
-
-	x1, _ := new(big.Int).SetString("2f01e5e15cca351daff3843fb70f3c2f0a1bdd05e5af888a67784ef3e10a2a01", 16)
-	y1, _ := new(big.Int).SetString("5c4da8a741539949293d082a132d13b4c2e213d6ba5b7617b5da2cb76cbde904", 16)
-	expectedPublicKey1 := &PublicKey{signer.Curve(), x1, y1}
-
-	iteration := int64(0)
-
-	publicKey, err := recoverPublicKeyFromSignature(signer.Curve(), sigR, sigS, hash, iteration)
-	if err != nil {
-		t.Fatal(err)
+	var tests = map[string]struct {
+		sigR               string
+		sigS               string
+		expectedRecoveryID int
+	}{
+		"recovers recovery ID 0": {
+			sigR:               "c896280f80f74055910baa4e4e6cfd52d73e1f7deba19e0dcbb01218dc49a729",
+			sigS:               "3628a6016d1afb333f6fe7ea65a3e255219b23734d2be50088318ca0e2b2e119",
+			expectedRecoveryID: 0,
+		},
+		"recovers recovery ID 1": {
+			sigR:               "9b32c3623b6a16e87b4d3a56cd67c666c9897751e24a51518136185403b1cba2",
+			sigS:               "90838891021e1c7d0d1336613f24ecab703dee5ff1b6c8881bccc2c011606a35",
+			expectedRecoveryID: 1,
+		},
 	}
 
-	if expectedPublicKey0.X.Cmp(publicKey.X) != 0 ||
-		expectedPublicKey0.Y.Cmp(publicKey.Y) != 0 {
-		t.Fatalf(
-			"unexpected public key for iteration 0\nexpected: [%x]\nactual:   [%x]\n",
-			expectedPublicKey0,
-			publicKey,
-		)
-	}
+	for testName, test := range tests {
+		t.Run(testName, func(t *testing.T) {
+			sigR, _ := new(big.Int).SetString(test.sigR, 16)
+			sigS, _ := new(big.Int).SetString(test.sigS, 16)
 
-	iteration = 1
+			recoveryID, err := signer.findRecoveryID(sigR, sigS, hash)
+			if err != nil {
+				t.Fatal(err)
+			}
 
-	publicKey, err = recoverPublicKeyFromSignature(signer.Curve(), sigR, sigS, hash, iteration)
-	if err != nil {
-		t.Fatal(err)
-	}
-
-	if expectedPublicKey1.X.Cmp(publicKey.X) != 0 ||
-		expectedPublicKey1.Y.Cmp(publicKey.Y) != 0 {
-		t.Fatalf(
-			"unexpected public key for iteration 1\nexpected: [%x]\nactual:   [%x]\n",
-			expectedPublicKey0,
-			publicKey,
-		)
+			if recoveryID != test.expectedRecoveryID {
+				t.Fatalf(
+					"unexpected recovery ID:\nexpected: [%d]\nactual:   [%d]\n",
+					test.expectedRecoveryID,
+					recoveryID,
+				)
+			}
+		})
 	}
 }
 
