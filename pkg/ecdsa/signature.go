8fb232e71d970f6c2aa03f3b3e361cf9713db134
Jakub Nowakowski <jakub.nowakowski@akena.co>
2019-09-16 11:50:18 +0200

    Improve Signature documentation

--- pkg/ecdsa/signature.go
+++ pkg/ecdsa/signature.go
@@ -10,8 +10,12 @@ import (
 	"github.com/ethereum/go-ethereum/crypto/secp256k1"
 )
 
-// Signature holds a signature in a form of two big.Int r and s values and a
+// Signature holds a signature in a form of two big.Int `r` and `s` values and a
 // recovery ID value in {0, 1, 2, 3}.
+//
+// The signature is chain-agnostic. Some chains (e.g. Ethereum and BTC) requires
+// `v` to start from 27. Please consult the documentation about what the
+// particular chain expects.
 type Signature struct {
 	R          *big.Int
 	S          *big.Int
