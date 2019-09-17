52690f9e40d56d4eb89da16c8d3fd1fd9d258234
Jakub Nowakowski <jakub.nowakowski@akena.co>
2019-09-17 14:53:07 +0200

    Make recoverPublicKeyFromSignature internal function

--- pkg/ecdsa/ecdsa.go
+++ pkg/ecdsa/ecdsa.go
@@ -57,7 +57,91 @@ func (s *Signer) CalculateSignature(rand io.Reader, hash []byte) (*Signature, er
 // recovery to get a missing `y` coordinate for the `R` point. The curve we use
 // for signing has up to 4 possible points for the given `x` coordinate. We use
 // recovery ID to point which of them is the one we used for signing.
+//
+// Internal function recoverPublicKeyFromSignature recovers a public key from the
+// signature's R and S values for the given message hash. Based on the algorithm
+// described in section 4.1.6 of [SEC 1]. It handles the inner loop of the
+// algorithm from point 1.6 based on the `iteration` parameter. It decides to
+// select `R` or `-R` value based on oddness of the y cooridnate. This is
+// consistent with solution implemented by btcd.
 func (s *Signer) findRecoveryID(sigR, sigS *big.Int, hash []byte) (int, error) {
+	recoverPublicKeyFromSignature := func(iteration int) (*PublicKey, error) {
+		curve := s.Curve()
+		j := iteration / 2
+
+		// 1.1 Calculate x coordinate of the R point.
+		// x = r + (j * n)
+		Rx := new(big.Int).Add(
+			sigR,
+			new(big.Int).Mul(
+				big.NewInt(int64(j)),
+				curve.Params().N,
+			),
+		)
+
+		if Rx.Cmp(curve.Params().P) != -1 {
+			return nil, fmt.Errorf("calculated Rx is larger than curve P")
+		}
+
+		// 1.3 Estimate y coordinate of the R point. For each x cooridnate there are
+		// two possible points on the elliptic curve - `R` and `-R`.
+
+		// `calculateY` always returns `y` coordinate of `R` point. We calculate `-R`
+		// point later if it is required.
+		Ry := calculateY(curve, Rx)
+		if Ry == nil {
+			return nil, fmt.Errorf("failed to calculate y")
+		}
+
+		// We check a `negativeR` flag to check which point we should use in our
+		// calculation.
+		// We compare it with oddness of the y coordinate to match btcec solution.
+		oddIteration := iteration%2 == 1
+		if oddIteration != isOdd(Ry) {
+			Ry = new(big.Int).Mod(
+				new(big.Int).Neg(Ry),
+				curve.Params().P,
+			)
+		}
+
+		// Validate found point.
+		if !curve.IsOnCurve(Rx, Ry) {
+			return nil, fmt.Errorf("point is not on curve")
+		}
+
+		// 1.5 Calculate `e` from message using the same algorithm as ecdsa signature
+		// calculation.
+		e := hashToInt(curve, hash)
+
+		// 1.6.1 Calculate a candidate public key.
+		// Q = (r^-1) * ( (s * R) - (e * G))
+		rInverse := new(big.Int).ModInverse(sigR, curve.Params().N) // (r^-1)
+
+		sRx, sRy := curve.ScalarMult(Rx, Ry, sigS.Bytes()) // (s * R)
+
+		// - (e * G)
+		minusE := new(big.Int).Mod(
+			new(big.Int).Neg(e),
+			curve.Params().N)
+		minuseGx, minuseGy := curve.ScalarBaseMult(minusE.Bytes())
+
+		// (s * R) - (e * G)
+		addedX, addedY := curve.Add(
+			sRx, sRy,
+			minuseGx, minuseGy,
+		)
+
+		Qx, Qy := curve.ScalarMult( // (r^-1) * ( (s * R) - (e * G))
+			addedX, addedY,
+			rInverse.Bytes(),
+		)
+
+		// We don't perform validation mentioned in point 1.6.2, because we expect
+		// it to be performed inside of the loop calling this function.
+
+		return &PublicKey{Curve: curve, X: Qx, Y: Qy}, nil
+	}
+
 	// `h` is a co-factor of the elliptic curve, for the curve we are using (scp256k1)
 	// the co-factor is equal `1`
 	h := 1
@@ -66,14 +150,16 @@ func (s *Signer) findRecoveryID(sigR, sigS *big.Int, hash []byte) (int, error) {
 	// signature there are 4 public keys against which the signature is valid.
 	// Here we check which recovery ID will correspond with signer's public key.
 	for i := 0; i < 2*(h+1); i++ {
-		publicKey, err := recoverPublicKeyFromSignature(
-			s.Curve(),
-			sigR,
-			sigS,
-			hash,
-			int64(i),
-		)
+		publicKey, err := recoverPublicKeyFromSignature(i)
 		if err != nil {
+			// If key recovery is executed in appropriate order we we don't expect
+			// the error to happen.
+			// TODO: Change to logger.Warnf after logging is merged to master.
+			fmt.Printf(
+				"failed to recover public key in iteration [%d]: [%v]",
+				i,
+				err,
+			)
 			continue
 		}
 
@@ -86,97 +172,6 @@ func (s *Signer) findRecoveryID(sigR, sigS *big.Int, hash []byte) (int, error) {
 	}
 
 	return -1, fmt.Errorf("failed to find recovery ID")
-}
-
-// recoverPublicKeyFromSignature recovers a public key from the signature's R and
-// S values for the given message hash. Based on the algorithm described in section
-// 4.1.6 of [SEC 1].
-//
-// It handles the inner loop of the algorithm from point 1.6 based on the `iteration`
-// parameter. It decides to select `R` or `-R` value based on oddness of the
-// y cooridnate. This is consistent with solution implemented in btcd.
-//
-// It requires the elliptic curve to be a short-form Weierstrass curve where
-// `a = 0` (`y² = x³ + b`).
-func recoverPublicKeyFromSignature(
-	curve *secp256k1.BitCurve,
-	sigR, sigS *big.Int, // signature's `r` and `s` values
-	hash []byte, // hash of the signed message
-	iteration int64,
-) (*PublicKey, error) {
-	j := iteration / 2
-
-	// 1.1 Calculate x coordinate of the R point.
-	// x = r + (j * n)
-	Rx := new(big.Int).Add(
-		sigR,
-		new(big.Int).Mul(
-			big.NewInt(j),
-			curve.Params().N,
-		),
-	)
-
-	if Rx.Cmp(curve.Params().P) != -1 {
-		return nil, fmt.Errorf("calculated Rx is larger than curve P")
-	}
-
-	// 1.3 Estimate y coordinate of the R point. For each x cooridnate there are
-	// two possible points on the elliptic curve - `R` and `-R`.
-
-	// `calculateY` always returns `y` coordinate of `R` point. We calculate `-R`
-	// point later if it is required.
-	Ry := calculateY(curve, Rx)
-	if Ry == nil {
-		return nil, fmt.Errorf("failed to calculate y")
-	}
-
-	// We check a `negativeR` flag to check which point we should use in our
-	// calculation.
-	// We compare it with oddness of the y coordinate to match btcec solution.
-	oddIteration := iteration%2 == 1
-	if oddIteration != isOdd(Ry) {
-		Ry = new(big.Int).Mod(
-			new(big.Int).Neg(Ry),
-			curve.Params().P,
-		)
-	}
-
-	// Validate found point.
-	if !curve.IsOnCurve(Rx, Ry) {
-		return nil, fmt.Errorf("point is not on curve")
-	}
-
-	// 1.5 Calculate `e` from message using the same algorithm as ecdsa signature
-	// calculation.
-	e := hashToInt(curve, hash)
-
-	// 1.6.1 Calculate a candidate public key.
-	// Q = (r^-1) * ( (s * R) - (e * G))
-	rInverse := new(big.Int).ModInverse(sigR, curve.Params().N) // (r^-1)
-
-	sRx, sRy := curve.ScalarMult(Rx, Ry, sigS.Bytes()) // (s * R)
-
-	// - (e * G)
-	minusE := new(big.Int).Mod(
-		new(big.Int).Neg(e),
-		curve.Params().N)
-	minuseGx, minuseGy := curve.ScalarBaseMult(minusE.Bytes())
-
-	// (s * R) - (e * G)
-	addedX, addedY := curve.Add(
-		sRx, sRy,
-		minuseGx, minuseGy,
-	)
-
-	Qx, Qy := curve.ScalarMult( // (r^-1) * ( (s * R) - (e * G))
-		addedX, addedY,
-		rInverse.Bytes(),
-	)
-
-	// We don't perform validation mentioned in point 1.6.2, because we expect
-	// it to be performed inside of the loop calling this function.
-
-	return &PublicKey{Curve: curve, X: Qx, Y: Qy}, nil
 }
 
 // calculateY calculates `y` coordinate for `x` curve point coordinate using the provided
