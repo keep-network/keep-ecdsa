2ae98f49596676a4a7e855e972c71b00ce653b81
Jakub Nowakowski <jakub.nowakowski@akena.co>
2019-09-20 15:55:27 +0200

    Read ethereum account key from a file
    Replaced ethereum account private key provided in config file with
    configuration of key file path and reading the file.

--- internal/config/config.go
+++ internal/config/config.go
@@ -9,6 +9,8 @@ import (
 	"github.com/keep-network/keep-tecdsa/pkg/chain/eth/ethereum"
 )
 
+const passwordEnvVariable = "KEEP_ETHEREUM_PASSWORD"
+
 // Config is the top level config structure.
 type Config struct {
 	Electrum    electrum.Config
@@ -16,12 +18,15 @@ type Config struct {
 	Ethereum    ethereum.Config
 }
 
-// ReadConfig reads in the configuration file in .toml format.
+// ReadConfig reads in the configuration file in .toml format. Ethereum key file
+// password is expected to be provided as environment variable.
 func ReadConfig(filePath string) (*Config, error) {
 	config := &Config{}
 	if _, err := toml.DecodeFile(filePath, config); err != nil {
 		return nil, fmt.Errorf("failed to decode file [%s]: [%v]", filePath, err)
 	}
+
+	config.Ethereum.Account.KeyFilePassword = os.Getenv(passwordEnvVariable)
 
 	return config, nil
 }
