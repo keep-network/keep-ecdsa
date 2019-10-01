8d59dcacffc1f897672254f1c21b816acfac02b2
Jakub Nowakowski <jakub.nowakowski@akena.co>
2019-10-01 14:26:06 +0200

    Unit tests for candidates registration

--- solidity/test/ECDSAKeepFactoryTest.js
+++ solidity/test/ECDSAKeepFactoryTest.js
@@ -1,40 +1,71 @@
 var ECDSAKeepFactory = artifacts.require('ECDSAKeepFactory');
 
 contract("ECDSAKeepFactory", async accounts => {
+    let keepFactory
 
-    it("emits ECDSAKeepCreated event upon keep creation", async () => {
-        let blockNumber = await web3.eth.getBlockNumber()
+    before(async () => {
+        keepFactory = await ECDSAKeepFactory.new()
+    })
 
-        let keepFactory = await ECDSAKeepFactory.deployed();
+    describe("openKeep", async () => {
+        it("reverts if no member candidates are registered", async () => {
+            keepFactory = await ECDSAKeepFactory.new()
 
-        let keepAddress = await keepFactory.openKeep.call(
-            10, // _groupSize
-            5, // _honestThreshold
-            "0xbc4862697a1099074168d54A555c4A60169c18BD" // _owner
-        )
+            try {
+                await keepFactory.openKeep.call(
+                    10, // _groupSize
+                    5, // _honestThreshold
+                    "0xbc4862697a1099074168d54A555c4A60169c18BD" // _owner
+                )
 
-        await keepFactory.openKeep(
-            10, // _groupSize
-            5, // _honestThreshold
-            "0xbc4862697a1099074168d54A555c4A60169c18BD" // _owner
-        )
-
-        let eventList = await keepFactory.getPastEvents('ECDSAKeepCreated', {
-            fromBlock: blockNumber,
-            toBlock: 'latest'
+                assert(false, 'Test call did not error as expected')
+            } catch (e) {
+                assert.include(e.message, "candidates list is empty")
+            }
         })
 
-        assert.isTrue(
-            web3.utils.isAddress(keepAddress),
-            `keep address ${keepAddress} is not a valid address`,
-        );
+        it("emits ECDSAKeepCreated event upon keep creation", async () => {
+            const member = accounts[1]
 
-        assert.equal(eventList.length, 1, "incorrect number of emitted events")
+            await keepFactory.registerCandidate({ from: member })
 
-        assert.equal(
-            eventList[0].returnValues.keepAddress,
-            keepAddress,
-            "incorrect keep address in emitted event",
-        )
-    });
-});
+            let blockNumber = await web3.eth.getBlockNumber()
+
+            let keepAddress = await keepFactory.openKeep.call(
+                10, // _groupSize
+                5, // _honestThreshold
+                "0xbc4862697a1099074168d54A555c4A60169c18BD" // _owner
+            )
+
+            await keepFactory.openKeep(
+                10, // _groupSize
+                5, // _honestThreshold
+                "0xbc4862697a1099074168d54A555c4A60169c18BD" // _owner
+            )
+
+            let eventList = await keepFactory.getPastEvents('ECDSAKeepCreated', {
+                fromBlock: blockNumber,
+                toBlock: 'latest'
+            })
+
+            assert.isTrue(
+                web3.utils.isAddress(keepAddress),
+                `keep address ${keepAddress} is not a valid address`,
+            );
+
+            assert.equal(eventList.length, 1, "incorrect number of emitted events")
+
+            assert.equal(
+                eventList[0].returnValues.keepAddress,
+                keepAddress,
+                "incorrect keep address in emitted event",
+            )
+
+            assert.equal(
+                eventList[0].returnValues.members,
+                member,
+                "incorrect member in emitted event",
+            )
+        })
+    })
+})
