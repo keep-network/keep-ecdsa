9ab90ebc70bf67eaa84a1eaf25f212093b297853
Jakub Nowakowski <jakub.nowakowski@akena.co>
2019-10-01 14:36:10 +0200

    Function to check if address is on members list

--- pkg/chain/eth/event.go
+++ pkg/chain/eth/event.go
@@ -10,6 +10,16 @@ type ECDSAKeepCreatedEvent struct {
 	Members     []common.Address // keep members addresses
 }
 
+// ContainsMember checks if list of members contains given address.
+func (e *ECDSAKeepCreatedEvent) ContainsMember(address common.Address) bool {
+	for _, member := range e.Members {
+		if member == address {
+			return true
+		}
+	}
+	return false
+}
+
 // SignatureRequestedEvent is an event emitted when a user requests
 // a digest to be signed.
 type SignatureRequestedEvent struct {
