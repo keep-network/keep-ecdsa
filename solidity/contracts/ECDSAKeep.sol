fd3916c53cb199a8058fd0678ccb7f3ffd216fde
Nicholas Evans <nicholas.evans@thesis.co>
2019-10-04 14:34:33 -0500

    add SafeMath import to ECDSAKeep

--- solidity/contracts/ECDSAKeep.sol
+++ solidity/contracts/ECDSAKeep.sol
@@ -1,6 +1,8 @@
 pragma solidity ^0.5.4;
 
 import "openzeppelin-solidity/contracts/ownership/Ownable.sol";
+import "openzeppelin-solidity/contracts/math/SafeMath.sol";
+import "openzeppelin-solidity/contracts/token/ERC20/IERC20.sol";
 import "./api/IECDSAKeep.sol";
 import "./utils/AddressArrayUtils.sol";
 
