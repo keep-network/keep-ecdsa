b1227f029f66ca368a70ca29b5c1c1bd307187e2
Nicholas Evans <nicholas.evans@thesis.co>
2019-10-30 05:14:18 -0500

    Add newly implemented functions to API

--- solidity/contracts/api/IECDSAKeep.sol
+++ solidity/contracts/api/IECDSAKeep.sol
@@ -12,4 +12,18 @@ contract IECDSAKeep {
     /// @notice Calculates a signature over provided digest by the keep.
     /// @param _digest Digest to be signed.
     function sign(bytes32 _digest) external;
+
+    /// @notice Distributes ETH evenly across all keep members
+    /// @dev Only the value passed to this function will be distributed
+    function distributeETHToMembers() external payable;
+
+    /// @notice Distributes ERC20 token evenly across all keep members
+    /// @dev This works with any ERC20 token that implements a transferFrom
+    /// function similar to the interface imported here from
+    /// openZeppelin. This function only has authority over pre-approved
+    /// token amount. We don't explicitly check for allowance, SafeMath
+    /// subtraction overflow is enough protection.
+    /// @param _tokenAddress Address of the ERC20 token to distribute
+    /// @param _value Amount of ERC20 token to distribute
+    function distributeERC20ToMembers(address _tokenAddress, uint256 _value) external;
 }
