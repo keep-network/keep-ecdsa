65519d4a4d7b0a32db95b607c44db4b39a5738a4
Jakub Nowakowski <jakub.nowakowski@keep.network>
2019-11-29 12:20:23 +0100

    Initial threshold signer struct
    Added initial signer struct which holds results of key generation. The
    results should be persisted to the local storage.
    Exposed function to obtain a group public key which is a result of key
    generation and should be the same for all threshold signers in a signing
    group.

---
+++ pkg/ecdsa/tss/signer.go
@@ -0,0 +1,30 @@
+package tss
+
+import (
+	"github.com/keep-network/keep-tecdsa/pkg/ecdsa"
+
+	"github.com/binance-chain/tss-lib/ecdsa/keygen"
+	tssLib "github.com/binance-chain/tss-lib/tss"
+)
+
+// Signer is threshold signer.
+type Signer struct {
+	tssParty tssLib.Party
+	// keygenData contains output of key generation stage. This data should be
+	// persisted to local storage.
+	keygenData keygen.LocalPartySaveData
+}
+
+// PublicKey returns Signer's public key as a pair of X and Y coordinates.
+func (s *Signer) PublicKey() *ecdsa.PublicKey {
+	pkX, pkY := s.keygenData.ECDSAPub.X(), s.keygenData.ECDSAPub.Y()
+
+	curve := tssLib.EC()
+	publicKey := ecdsa.PublicKey{
+		Curve: curve,
+		X:     pkX,
+		Y:     pkY,
+	}
+
+	return (*ecdsa.PublicKey)(&publicKey)
+}
