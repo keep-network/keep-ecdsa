c6dee58b41ac92efd358eae2878cc83d8edd6e7d
Jakub Nowakowski <jakub.nowakowski@keep.network>
2019-12-02 18:39:50 +0100

    Made net package unrelated to tss.Message
    net package implements own Message struct which should contain tss
    specific message as a payload.
    We have just one function for sending both unicast and broadcast
    messages and will determine the type by `IsBroadcast` parameter.

--- pkg/net/net.go
+++ pkg/net/net.go
@@ -1,26 +1,19 @@
 package net
 
-import (
-	"github.com/binance-chain/tss-lib/tss"
-)
-
 // NetworkChannel represents a channel for members to exchange messages.
 // TODO: Wrap `tss.Message` type with internal message type.
 // TODO: Reuse keep-core?
 type NetworkChannel interface {
 	// TODO: Send function has to support broadcast and unicast or we can provide
 	// separate functions for point-to-point and broadcast communication.
-	// ```
-	// destinations := message.GetTo()
-	// if destinations == nil {
-	//   // broadcast
-	// } else {
-	//   for _, dest := range destinations {
-	//    // unicast
-	//   }
-	// }
-	// ```
-	Send(message tss.Message) error
+	Send(message Message) error
 	// Receive registers handler function to be executed on message receive.
-	Receive(handler func(message tss.Message) error)
+	Receive(handler func(message Message) error)
+}
+
+type Message struct {
+	From        []byte // unique sender key
+	To          []byte // unique destination key, required for unicast
+	IsBroadcast bool
+	Payload     []byte
 }
