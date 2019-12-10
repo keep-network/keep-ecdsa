26f80de236628c1ce45d5e4840acb18ec2bb486c
Jakub Nowakowski <jakub.nowakowski@keep.network>
2019-12-10 17:03:22 +0100

    Updated start command to initialize network
    Updated start command to read key and initialize local network
    connection.

--- cmd/start.go
+++ cmd/start.go
@@ -3,14 +3,20 @@ package cmd
 import (
 	"context"
 	"fmt"
+	"math/big"
 
 	"github.com/ipfs/go-log"
+	"github.com/urfave/cli"
+
 	"github.com/keep-network/keep-common/pkg/chain/ethereum/ethutil"
 	"github.com/keep-network/keep-common/pkg/persistence"
+	"github.com/keep-network/keep-core/pkg/net/key"
+	"github.com/keep-network/keep-core/pkg/operator"
+
 	"github.com/keep-network/keep-tecdsa/internal/config"
 	"github.com/keep-network/keep-tecdsa/pkg/chain/eth/ethereum"
 	"github.com/keep-network/keep-tecdsa/pkg/client"
-	"github.com/urfave/cli"
+	"github.com/keep-network/keep-tecdsa/pkg/net/local"
 )
 
 var logger = log.Logger("keep-cmd")
@@ -52,6 +58,29 @@ func Start(c *cli.Context) error {
 		return fmt.Errorf("failed to connect to ethereum node: [%v]", err)
 	}
 
+	operatorPrivateKey, operatorPublicKey := operator.EthereumKeyToOperatorKey(ethereumKey)
+
+	_, networkPublicKey := key.OperatorKeyToNetworkKey(
+		operatorPrivateKey, operatorPublicKey,
+	)
+
+	transportID := new(big.Int).SetBytes(
+		[]byte(ethereumChain.Address().String()),
+	).String()
+
+	netErrChan := make(chan error)
+	go func() {
+		for {
+			logger.Errorf("network error ocurred: [%v]", <-netErrChan)
+		}
+	}()
+
+	networkProvider := local.LocalProvider(
+		transportID,
+		networkPublicKey,
+		netErrChan,
+	)
+
 	persistence := persistence.NewEncryptedPersistence(
 		persistence.NewDiskHandle(config.Storage.DataDir),
 		config.Ethereum.Account.KeyFilePassword,
@@ -61,6 +90,7 @@ func Start(c *cli.Context) error {
 
 	client.Initialize(
 		ethereumChain,
+		networkProvider,
 		persistence,
 	)
 
