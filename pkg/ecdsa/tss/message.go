298b90c9dd4915b9b3e9018907983397d151c099
Jakub Nowakowski <jakub.nowakowski@keep.network>
2019-12-11 10:20:44 +0100

    Replaced the way we get message type string

--- pkg/ecdsa/tss/message.go
+++ pkg/ecdsa/tss/message.go
@@ -1,8 +1,9 @@
 package tss
 
-import "encoding/json"
-
-const TSSmessageType = "TSSMessage"
+import (
+	"encoding/json"
+	"fmt"
+)
 
 // TSSMessage is a network message used to transport messages in TSS protocol
 // execution.
@@ -15,24 +16,24 @@ type TSSMessage struct {
 // Type returns a string type of the `TSSMessage` so that it conforms to
 // `net.Message` interface.
 func (m *TSSMessage) Type() string {
-	return TSSmessageType
+	return fmt.Sprintf("%T", m)
 }
 
 // Marshal converts this message to a byte array suitable for network communication.
-func (pm *TSSMessage) Marshal() ([]byte, error) {
-	return json.Marshal(pm)
+func (m *TSSMessage) Marshal() ([]byte, error) {
+	return json.Marshal(m)
 }
 
 // Unmarshal converts a byte array produced by Marshal to a message.
-func (pm *TSSMessage) Unmarshal(bytes []byte) error {
+func (m *TSSMessage) Unmarshal(bytes []byte) error {
 	var message TSSMessage
 	if err := json.Unmarshal(bytes, &message); err != nil {
 		return err
 	}
 
-	pm.Payload = message.Payload
-	pm.SenderID = message.SenderID
-	pm.IsBroadcast = message.IsBroadcast
+	m.Payload = message.Payload
+	m.SenderID = message.SenderID
+	m.IsBroadcast = message.IsBroadcast
 
 	return nil
 }
