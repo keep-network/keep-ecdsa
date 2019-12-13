110427803ebd00433a64db523688636f144d9859
Jakub Nowakowski <jakub.nowakowski@keep.network>
2019-12-13 11:30:27 +0100

    Start local command to exec start command in for loop

--- cmd/startlocal.go
+++ cmd/startlocal.go
@@ -4,14 +4,6 @@ import (
 	"context"
 	"fmt"
 
-	"github.com/keep-network/keep-common/pkg/chain/ethereum/ethutil"
-	"github.com/keep-network/keep-common/pkg/persistence"
-	"github.com/keep-network/keep-core/pkg/net/key"
-	"github.com/keep-network/keep-core/pkg/operator"
-	"github.com/keep-network/keep-tecdsa/internal/config"
-	"github.com/keep-network/keep-tecdsa/pkg/chain/eth/ethereum"
-	"github.com/keep-network/keep-tecdsa/pkg/client"
-	"github.com/keep-network/keep-tecdsa/pkg/net/local"
 	"github.com/urfave/cli"
 )
 
@@ -35,73 +27,29 @@ func init() {
 func StartLocal(c *cli.Context) error {
 	groupSize := 3
 
-	ctx := context.Background()
-	var err error
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+
+	errChan := make(chan error, groupSize)
 
 	for index := 1; index <= groupSize; index++ {
-		configPath := fmt.Sprintf("./configs/config_%d.toml", index)
+		go func(i int) {
+			configPath := fmt.Sprintf("./configs/config_%d.toml", i)
 
-		config, err := config.ReadConfig(configPath)
-		if err != nil {
-			return fmt.Errorf("failed while reading config file: [%v]", err)
-		}
+			c.GlobalSet("config", configPath)
 
-		ethereumKey, err := ethutil.DecryptKeyFile(
-			config.Ethereum.Account.KeyFile,
-			config.Ethereum.Account.KeyFilePassword,
-		)
-		if err != nil {
-			return fmt.Errorf(
-				"failed to read key file [%s]: [%v]", config.Ethereum.Account.KeyFile, err,
-			)
-		}
-
-		ethereumChain, err := ethereum.Connect(ethereumKey.PrivateKey, &config.Ethereum)
-		if err != nil {
-			return fmt.Errorf("failed to connect to ethereum node: [%v]", err)
-		}
-
-		operatorPrivateKey, operatorPublicKey := operator.EthereumKeyToOperatorKey(ethereumKey)
-
-		_, networkPublicKey := key.OperatorKeyToNetworkKey(
-			operatorPrivateKey, operatorPublicKey,
-		)
-
-		netErrChan := make(chan error)
-		go func() {
-			for {
-				logger.Errorf("network error ocurred: [%v]", <-netErrChan)
+			err := Start(c)
+			if err != nil {
+				errChan <- err
+				cancel()
 			}
-		}()
 
-		networkProvider := local.LocalProvider(
-			networkPublicKey,
-			netErrChan,
-		)
-
-		persistence := persistence.NewEncryptedPersistence(
-			persistence.NewDiskHandle(config.Storage.DataDir),
-			config.Ethereum.Account.KeyFilePassword,
-		)
-
-		client.Initialize(
-			ethereumChain,
-			networkProvider,
-			persistence,
-		)
-
-		logger.Infof("client %d started", index)
-
+			logger.Infof("client %d started", i)
+		}(index)
 	}
 
 	select {
 	case <-ctx.Done():
-		if err != nil {
-			return err
-		}
-
-		return fmt.Errorf("unexpected context cancellation")
+		return <-errChan
 	}
-
-	return nil
 }
