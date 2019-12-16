b68e92b2005715878c57a89c80436145c0937842
Jakub Nowakowski <jakub.nowakowski@keep.network>
2019-12-16 11:54:07 +0100

    Simplified pool to use channels

--- pkg/node/params_pool.go
+++ pkg/node/params_pool.go
@@ -1,8 +1,6 @@
 package node
 
 import (
-	"sync"
-
 	"github.com/binance-chain/tss-lib/ecdsa/keygen"
 	"github.com/keep-network/keep-tecdsa/pkg/ecdsa/tss"
 )
@@ -11,23 +9,16 @@ import (
 // up to the pool size. When an entry is pulled from the pool it will generate
 // new entry.
 type tssPreParamsPool struct {
-	pumpFuncMutex *sync.Mutex // lock concurrent executions of pumping function
-
-	paramsMutex *sync.Cond
-	params      []*keygen.LocalPreParams
-
-	new func() (*keygen.LocalPreParams, error)
-
-	poolSize int
+	pool chan *keygen.LocalPreParams
+	new  func() (*keygen.LocalPreParams, error)
 }
 
 // InitializeTSSPreParamsPool generates TSS pre-parameters and stores them in a pool.
 func (n *Node) InitializeTSSPreParamsPool() {
+	poolSize := 5
+
 	n.tssParamsPool = &tssPreParamsPool{
-		pumpFuncMutex: &sync.Mutex{},
-		paramsMutex:   sync.NewCond(&sync.Mutex{}),
-		params:        []*keygen.LocalPreParams{},
-		poolSize:      2,
+		pool: make(chan *keygen.LocalPreParams, poolSize),
 		new: func() (*keygen.LocalPreParams, error) {
 			return tss.GenerateTSSPreParams()
 		},
@@ -36,26 +27,15 @@ func (n *Node) InitializeTSSPreParamsPool() {
 	go n.tssParamsPool.pumpPool()
 }
 
-func (n *tssPreParamsPool) pumpPool() {
-	n.pumpFuncMutex.Lock()
-	defer n.pumpFuncMutex.Unlock()
-
+func (t *tssPreParamsPool) pumpPool() {
 	for {
-		if len(n.params) >= n.poolSize {
-			logger.Debugf("tss pre parameters pool is pumped")
-			return
-		}
-
-		params, err := n.new()
+		params, err := t.new()
 		if err != nil {
 			logger.Warningf("failed to generate tss pre parameters: [%v]", err)
 			continue
 		}
 
-		n.paramsMutex.L.Lock()
-		n.params = append(n.params, params)
-		n.paramsMutex.Signal()
-		n.paramsMutex.L.Unlock()
+		t.pool <- params
 
 		logger.Debugf("generated new tss pre parameters")
 	}
@@ -63,18 +43,6 @@ func (n *tssPreParamsPool) pumpPool() {
 
 // get returns TSS pre parameters from the pool. It pumps the pool after getting
 // and entry. If the pool is empty it will wait for a new entry to be generated.
-func (n *tssPreParamsPool) get() *keygen.LocalPreParams {
-	n.paramsMutex.L.Lock()
-	defer n.paramsMutex.L.Unlock()
-
-	for len(n.params) == 0 {
-		n.paramsMutex.Wait()
-	}
-
-	params := n.params[0]
-	n.params = n.params[1:len(n.params)]
-
-	go n.pumpPool()
-
-	return params
+func (t *tssPreParamsPool) get() *keygen.LocalPreParams {
+	return <-t.pool
 }
