055ca6b08fdacdabc88d349c37ae03d90026fcb0
Jakub Nowakowski <jakub.nowakowski@keep.network>
2019-12-23 09:54:57 +0100

    Added unit tests for threshold key marshaling

--- pkg/ecdsa/tss/marshaling_test.go
+++ pkg/ecdsa/tss/marshaling_test.go
@@ -32,7 +32,7 @@ func TestSignerMarshalling(t *testing.T) {
 			groupMemberIDs:     groupMembersIDs,
 			dishonestThreshold: dishonestThreshold,
 		},
-		keygenData: testData[signerIndex],
+		thresholdKey: ThresholdKey(testData[signerIndex]),
 	}
 
 	unmarshaled := &ThresholdSigner{}
@@ -44,6 +44,38 @@ func TestSignerMarshalling(t *testing.T) {
 		t.Fatalf(
 			"unexpected content of unmarshaled signer\nexpected: [%+v]\nactual:   [%+v]\n",
 			signer,
+			unmarshaled,
+		)
+	}
+}
+
+func TestThresholdKeyMarshalling(t *testing.T) {
+	testData, err := testdata.LoadKeygenTestFixtures(1)
+	if err != nil {
+		t.Fatalf("failed to load test data: [%v]", err)
+	}
+
+	key := ThresholdKey(testData[0])
+
+	unmarshaled := &ThresholdKey{}
+
+	marshaled, err := key.Marshal()
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	err = unmarshaled.Unmarshal(marshaled)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if err := pbutils.RoundTrip(&key, unmarshaled); err != nil {
+		t.Fatal(err)
+	}
+	if !reflect.DeepEqual(&key, unmarshaled) {
+		t.Fatalf(
+			"unexpected content of unmarshaled signer\nexpected: [%+v]\nactual:   [%+v]\n",
+			&key,
 			unmarshaled,
 		)
 	}
