b2440dd733607c351472edae82bbd1e9a7ec0153
Jakub Nowakowski <jakub.nowakowski@keep.network>
2019-12-30 15:35:29 +0100

    Added function to check deposit available for bonding
    Function returns deposit of the operator which is available to create
    bonds.

--- solidity/contracts/KeepBond.sol
+++ solidity/contracts/KeepBond.sol
@@ -6,6 +6,13 @@ contract KeepBond {
    // Unassigned ether values deposited by operators.
    mapping(address => uint) internal pot;
 
+   /// @notice Returns value of ether available for bonding for the operator.
+   /// @param operator Address of the operator.
+   /// @return Value of deposited ether available for bonding.
+   function availableForBonding(address operator) public view returns (uint256) {
+      return pot[operator];
+   }
+
    /// @notice Add ether to sender's value available for bonding.
    function deposit() external payable {
       pot[msg.sender] += msg.value;
