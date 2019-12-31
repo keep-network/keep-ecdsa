1a2c168d542927a161ae57266b65012da8932b4f
Jakub Nowakowski <jakub.nowakowski@keep.network>
2019-12-31 14:56:16 +0100

    Rename account -> operator

--- solidity/test/KeepBondingTest.js
+++ solidity/test/KeepBondingTest.js
@@ -17,37 +17,37 @@ contract('KeepBonding', (accounts) => {
 
     describe('deposit', async () => {
         it('registers unbonded value', async () => {
-            const account = accounts[1]
+            const operator = accounts[1]
             const value = new BN(100)
 
-            const expectedUnbonded = (await keepBonding.availableBondingValue(account)).add(value)
+            const expectedUnbonded = (await keepBonding.availableBondingValue(operator)).add(value)
 
-            await keepBonding.deposit(account, { value: value })
+            await keepBonding.deposit(operator, { value: value })
 
-            const unbonded = await keepBonding.availableBondingValue(account)
+            const unbonded = await keepBonding.availableBondingValue(operator)
 
             expect(unbonded).to.eq.BN(expectedUnbonded, 'invalid unbonded value')
         })
     })
 
     describe('withdraw', async () => {
-        const account = accounts[1]
+        const operator = accounts[1]
         const destination = accounts[2]
 
         before(async () => {
             const value = new BN(100)
-            await keepBonding.deposit(account, { value: value })
+            await keepBonding.deposit(operator, { value: value })
         })
 
         it('transfers unbonded value', async () => {
-            const value = (await keepBonding.availableBondingValue(account))
+            const value = (await keepBonding.availableBondingValue(operator))
 
-            const expectedUnbonded = (await keepBonding.availableBondingValue(account)).sub(value)
+            const expectedUnbonded = (await keepBonding.availableBondingValue(operator)).sub(value)
             const expectedDestinationBalance = web3.utils.toBN(await web3.eth.getBalance(destination)).add(value)
 
-            await keepBonding.withdraw(value, destination, { from: account })
+            await keepBonding.withdraw(value, destination, { from: operator })
 
-            const unbonded = await keepBonding.availableBondingValue(account)
+            const unbonded = await keepBonding.availableBondingValue(operator)
             expect(unbonded).to.eq.BN(expectedUnbonded, 'invalid unbonded value')
 
             const destinationBalance = await web3.eth.getBalance(destination)
@@ -55,34 +55,33 @@ contract('KeepBonding', (accounts) => {
         })
 
         it('fails if insufficient unbonded value', async () => {
-            const value = (await keepBonding.availableBondingValue(account)).add(new BN(1))
+            const value = (await keepBonding.availableBondingValue(operator)).add(new BN(1))
 
             await expectRevert(
-                keepBonding.withdraw(value, destination, { from: account }),
+                keepBonding.withdraw(value, destination, { from: operator }),
                 "Insufficient unbonded value"
             )
         })
     })
 
     describe('availableBondingValue', async () => {
-        const account = accounts[1]
+        const operator = accounts[1]
         const value = new BN(100)
 
         before(async () => {
-            await keepBonding.deposit(account, { value: value })
+            await keepBonding.deposit(operator, { value: value })
         })
 
         it('returns zero for not deposited operator', async () => {
-            const operator = "0x0000000000000000000000000000000000000001"
+            const unbondedOperator = "0x0000000000000000000000000000000000000001"
             const expectedUnbonded = 0
 
-            const unbondedValue = await keepBonding.availableBondingValue(operator)
+            const unbondedValue = await keepBonding.availableBondingValue(unbondedOperator)
 
             expect(unbondedValue).to.eq.BN(expectedUnbonded, 'invalid unbonded value')
         })
 
         it('returns value of operators deposit', async () => {
-            const operator = account
             const expectedUnbonded = value
 
             const unbonded = await keepBonding.availableBondingValue(operator)
