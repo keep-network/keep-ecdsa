2fe294d10901f58758280160b4ae69f1e99e4bf4
Jakub Nowakowski <jakub.nowakowski@keep.network>
2020-01-20 11:12:57 +0100

    Bumped up solc version for contracts compiler
    We use @keep-network/sortition-pools package which contracts' pragma
    require solidity compiler of ^0.5.10. With current `truffle.js`
    configuration contracts fail compilation. Here we update a version of
    the solidity compiler to the latest 0.5.x version available.

--- solidity/truffle.js
+++ solidity/truffle.js
@@ -48,7 +48,7 @@ module.exports = {
   // Configure your compilers
   compilers: {
     solc: {
-      version: "0.5.4",    // Fetch exact version from solc-bin (default: truffle's version)
+      version: "0.5.15",    // Fetch exact version from solc-bin (default: truffle's version)
     }
   }
 }
