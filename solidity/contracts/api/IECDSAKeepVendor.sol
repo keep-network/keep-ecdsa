b54ecd0db1fba9974bc111fd2a6cae840f6a21e8
Jakub Nowakowski <jakub.nowakowski@keep.network>
2020-01-21 20:31:06 +0100

    Removed openKeep from ECDSA Keep Vendor
    We removed openKeep function from ECDSA Keep Vendor contract as we want
    the function to be called directly on factory contract. The factory will
    want to verify if the caller of the keep opening function is eligible
    application and doing it through vendor contract prevents it of doing
    so.
    We expect that external application will call vendor contract to get a
    recommended factory address and then the application will call the
    given factory to open a new keep.

--- solidity/contracts/api/IECDSAKeepVendor.sol
+++ solidity/contracts/api/IECDSAKeepVendor.sol
@@ -2,16 +2,9 @@ pragma solidity ^0.5.4;
 
 /// @title ECDSA Keep Vendor
 /// @notice Contract reflecting an ECDSA keep vendor.
-contract IECDSAKeepVendor {
-    /// @notice Open a new ECDSA keep.
-    /// @dev Calls a recommended ECDSA keep factory to open a new keep.
-    /// @param _groupSize Number of members in the keep.
-    /// @param _honestThreshold Minimum number of honest keep members.
-    /// @param _owner Address of the keep owner.
-    /// @return Opened keep address.
-    function openKeep(
-        uint256 _groupSize,
-        uint256 _honestThreshold,
-        address _owner
-    ) external payable returns (address keepAddress);
+contract IECDSAKeepVendor { // TODO: Rename to IBondedECDSAKeepVendor
+    /// @notice Select a recommended ECDSA keep factory from all registered
+    /// ECDSA keep factories.
+    /// @return Selected ECDSA keep factory address.
+    function selectFactory() public view returns (address payable);
 }
