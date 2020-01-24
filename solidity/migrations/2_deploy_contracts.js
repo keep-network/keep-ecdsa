e3f821016ffb85ceb8bcf861b0d4fe9b9409afe1
Jakub Nowakowski <jakub.nowakowski@keep.network>
2020-01-25 00:08:38 +0100

    Updated contracts deployment with reading external artifacts

--- solidity/migrations/2_deploy_contracts.js
+++ solidity/migrations/2_deploy_contracts.js
@@ -3,27 +3,24 @@ const ECDSAKeepFactory = artifacts.require("./ECDSAKeepFactory.sol");
 const ECDSAKeepVendor = artifacts.require("./ECDSAKeepVendor.sol");
 const KeepRegistry = artifacts.require("./KeepRegistry.sol");
 
-const SortitionPoolFactoryStub = artifacts.require("./SortitionPoolFactoryStub");
-
-let { SortitionPoolFactoryAddress } = require('./externals')
+const { SortitionPoolFactory } = require('./external')
 
 module.exports = async function (deployer) {
     await deployer.deploy(KeepBonding)
 
-    // TODO: Temporarily we use stub of sortition pool factory until deployment
-    // process is implemented and address of SortitionPoolFactory is provided in
-    // `externals.js` file. Later we should use the stub for executing unit tests
-    // in the CI process.
-    // if (process.env.TEST = true) {
-    SortitionPoolFactoryAddress = (await deployer.deploy(SortitionPoolFactoryStub)).address
-    // }
+    if (process.env.TEST == true) {
+        const SortitionPoolFactoryStub = artifacts.require("./SortitionPoolFactoryStub");
+        SortitionPoolFactory = (await deployer.deploy(SortitionPoolFactoryStub))
+    } else {
+        SortitionPoolFactory.setProvider(web3.eth.currentProvider)
+        await SortitionPoolFactory.deployed()
+    }
 
-    await deployer.deploy(ECDSAKeepFactory, SortitionPoolFactoryAddress)
-    const ecdsaKeepFactory = await ECDSAKeepFactory.deployed()
+    await deployer.deploy(ECDSAKeepFactory, SortitionPoolFactory.address)
 
     const ecdsaKeepVendor = await deployer.deploy(ECDSAKeepVendor)
-    await ecdsaKeepVendor.registerFactory(ecdsaKeepFactory.address)
+    await ecdsaKeepVendor.registerFactory(ECDSAKeepFactory.address)
 
     const keepRegistry = await deployer.deploy(KeepRegistry)
-    await keepRegistry.setVendor('ECDSAKeep', ecdsaKeepVendor.address)
+    await keepRegistry.setVendor('ECDSAKeep', ECDSAKeepVendor.address)
 }
