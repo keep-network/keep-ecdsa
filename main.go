dd1587634f62885da30d7072bed8fc587fb701cb
Jakub Nowakowski <jakub.nowakowski@keep.network>
2020-01-24 22:56:49 +0100

    Removed btc transaction publishing and signing cli commands
    Removed CLI commands and related code for pbulishing btc transaction to
    the chain and signing messages on cli request as we no longer need and
    use this functionalities.

--- main.go
+++ main.go
@@ -15,13 +15,11 @@ import (
 var logger = log.Logger("keep-main")
 
 const (
-	defaultConfigPath   = "./configs/config.toml"
-	defaultBroadcastAPI = "blockcypher"
+	defaultConfigPath = "./configs/config.toml"
 )
 
 var (
-	configPath   string
-	broadcastAPI string
+	configPath string
 )
 
 func main() {
@@ -47,17 +45,9 @@ func main() {
 			Destination: &configPath,
 			Usage:       "full path to the configuration file",
 		},
-		cli.StringFlag{
-			Name:        "broadcast-api",
-			Value:       defaultBroadcastAPI,
-			Destination: &broadcastAPI,
-			Usage:       "external service used to communicate with the blockchain",
-		},
 	}
 	app.Commands = []cli.Command{
-		cmd.SignCommand,
 		cmd.StartCommand,
-		cmd.PublishCommand,
 	}
 
 	err = app.Run(os.Args)
