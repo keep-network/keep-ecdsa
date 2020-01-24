b812b18810589480abbe5b0c01f4141b9e1d481e
Jakub Nowakowski <jakub.nowakowski@keep.network>
2020-01-25 00:05:30 +0100

    Read contracts artifacts from json file
    It is expected that artifacts are placed in `artifacts` directory.

---
+++ solidity/migrations/external/index.js
@@ -0,0 +1,10 @@
+// Reads external contracts artifacts provided as `json` files. It is expected
+// that artifacts are placed in `artifacts` directory.
+
+const TruffleContract = require('@truffle/contract')
+
+const SortitionPoolFactory = TruffleContract(require("./artifacts/SortitionPoolFactory.json"))
+
+module.exports = {
+  SortitionPoolFactory,
+}
