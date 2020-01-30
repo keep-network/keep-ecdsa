6d07e2fbaa7cbd50d393dabae37119b96d603c3f
Jakub Nowakowski <jakub.nowakowski@keep.network>
2020-01-30 17:29:27 +0100

    Added isOperatorRegistered to SortitionPoolStub

--- solidity/test/contracts/SortitionPoolStub.sol
+++ solidity/test/contracts/SortitionPoolStub.sol
@@ -1,8 +1,16 @@
 pragma solidity ^0.5.10;
 
 contract SortitionPoolStub {
-
     address payable[] operators;
+
+    function isOperatorRegistered(address operator) public view returns (bool) {
+        for (uint256 i = 0; i < operators.length; i++) {
+            if (operators[i] == operator) {
+                return true;
+            }
+        }
+        return false;
+    }
 
     function insertOperator(address payable operator, uint256 weight) public {
         weight;
