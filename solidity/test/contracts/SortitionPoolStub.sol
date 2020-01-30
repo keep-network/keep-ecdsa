e5efbac49c6521135321681be229c72512a5a99b
Jakub Nowakowski <jakub.nowakowski@keep.network>
2020-01-30 17:27:01 +0100

    Removed selectGroup from SortitionPoolStub
    Removed the function as we no longer use it in tests

--- solidity/test/contracts/SortitionPoolStub.sol
+++ solidity/test/contracts/SortitionPoolStub.sol
@@ -4,20 +4,7 @@ contract SortitionPoolStub {
 
     address payable[] operators;
 
-    function selectGroup(uint256 groupSize, bytes32 seed) public view returns (address payable[] memory members)  {
-        seed;
-
-        members = new address payable[](groupSize);
-
-        uint nextIndex = 0;
-        for (uint i = 0; i < groupSize; i++) {
-            members[i] = operators[nextIndex];
-            nextIndex++;
-            nextIndex = nextIndex % operators.length;
-        }
-    }
-
-    function insertOperator(address payable operator, uint weight) public {
+    function insertOperator(address payable operator, uint256 weight) public {
         weight;
 
         operators.push(operator);
