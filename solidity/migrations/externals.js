dce4da186d5517565229c7f4149c52c328b80ad1
Jakub Nowakowski <jakub.nowakowski@keep.network>
2020-02-03 15:13:02 +0100

    Modified way we fetch external contracts addresses
    We used to fetch contracts artifacts and get an address from them. Here
    we leave it to truffle and jus fetch the artifacts and let truffle
    read artifact to get contract address.

--- solidity/migrations/externals.js
+++
@@ -1,6 +0,0 @@
-// Configuration for addresses of externally deployed smart contracts
-const TokenStakingAddress = '0xAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA'
-
-module.exports = {
-  TokenStakingAddress,
-}
--- solidity/scripts/circleci-provision-external-contracts.sh
+++ solidity/scripts/circleci-provision-external-contracts.sh
@@ -1,19 +1,17 @@
 #!/bin/bash
 set -ex
 
-# TokenStakingAddress: Migration from keep-network/keep-core
+# Migration from keep-network/keep-core
 # TOKEN_STAKING_CONTRACT_DATA is set in the CircleCI job config
 # ETH_NETWORK_ID is set in the CircleCI context for each deployed environment
-TOKEN_STAKING_ADDRESS=""
 
-function fetch_token_staking_address() {
-  gsutil -q cp gs://${CONTRACT_DATA_BUCKET}/keep-core/${TOKEN_STAKING_CONTRACT_DATA} ./
-  TOKEN_STAKING_ADDRESS=$(cat ./${TOKEN_STAKING_CONTRACT_DATA} | jq ".networks[\"${ETH_NETWORK_ID}\"].address" | tr -d '"')
+function fetch_token_staking_contract() {
+  gsutil -q cp gs://${CONTRACT_DATA_BUCKET}/keep-core/${TOKEN_STAKING_CONTRACT_DATA} ../build/contracts/
 }
 
-function set_token_staking_address() {
-  sed -i -e "/TokenStakingAddress/s/0x[a-fA-F0-9]\{0,40\}/${TOKEN_STAKING_ADDRESS}/" ./solidity/migrations/externals.js
+function fetch_registry_contract() {
+  gsutil -q cp gs://${CONTRACT_DATA_BUCKET}/keep-core/${REGISTRY_CONTRACT_DATA} ../build/contracts/
 }
 
-fetch_token_staking_address
-set_token_staking_address
+fetch_token_staking_contract
+fetch_registry_contract
