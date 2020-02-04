a0505782fd22eb8e6f4d7427e97f558ac2915cd8
Jakub Nowakowski <jakub.nowakowski@keep.network>
2020-02-04 16:12:54 +0100

    Updated docs in circleci-provision-external-contracts

--- solidity/scripts/circleci-provision-external-contracts.sh
+++ solidity/scripts/circleci-provision-external-contracts.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 set -ex
 
-# Migration from keep-network/keep-core
-# TOKEN_STAKING_CONTRACT_DATA is set in the CircleCI job config
-# ETH_NETWORK_ID is set in the CircleCI context for each deployed environment
+# Fetch contacts migrated from keep-network/keep-core project.
+# TOKEN_STAKING_CONTRACT_DATA and REGISTRY_CONTRACT_DATA are set in the CircleCI
+# job config to the filenames of respective contracts' artifacts.
 
 function fetch_token_staking_contract() {
   gsutil -q cp gs://${CONTRACT_DATA_BUCKET}/keep-core/${TOKEN_STAKING_CONTRACT_DATA} ./solidity/build/contracts/${TOKEN_STAKING_CONTRACT_DATA}
