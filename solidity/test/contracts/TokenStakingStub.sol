0df1e3ef7e07660c5a495aa848d9c0d39a7c9a11
Jakub Nowakowski <jakub.nowakowski@keep.network>
2020-02-05 01:12:25 +0100

    Updated TokenStakingStub for tests

--- solidity/test/contracts/TokenStakingStub.sol
+++ solidity/test/contracts/TokenStakingStub.sol
@@ -5,19 +5,31 @@ import "../../contracts/external/ITokenStaking.sol";
 /// @title Token Staking Stub
 /// @dev This contract is for testing purposes only.
 contract TokenStakingStub is ITokenStaking {
-    uint256 balance = 1;
+    mapping(bytes32 => uint256) balance;
 
     /// @dev Sets balance variable value.
-    function setBalance(uint256 _balance) public {
-        balance = _balance;
+    function setBalance(
+        address _operator,
+        address _operatorContract,
+        uint256 _balance
+    ) public {
+        bytes32 mapKey = keccak256(
+            abi.encodePacked(_operator, _operatorContract)
+        );
+
+        balance[mapKey] = _balance;
     }
 
     /// @dev Returns balance variable value.
-    function eligibleStake(address _address, address)
+    function eligibleStake(address _operator, address _operatorContract)
         public
         view
         returns (uint256)
     {
-        return balance;
+        bytes32 mapKey = keccak256(
+            abi.encodePacked(_operator, _operatorContract)
+        );
+
+        return balance[mapKey];
     }
 }
