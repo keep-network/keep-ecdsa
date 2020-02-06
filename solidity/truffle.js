5a11d6d1751eba19e57527aaa031ffdb994cbbe7
Sloan Thompson <sloan.thompson@thesis.co>
2020-02-06 08:56:42 -0500

    Don't use env vars for now
    There's so extra indirection I need to take care of for this to work and
    I'm just going to punt for the moment.

--- solidity/truffle.js
+++ solidity/truffle.js
@@ -42,12 +42,13 @@ module.exports = {
       // CONTRACT_OWNER_ETH_ACCOUNT_PASSWORD is set in a Circle context. For now this value
       // is shared between the contract owner Ethereum account on both our internal testnet
       // and on Ropsten.
-      return new HDWalletProvider(process.env.CONTRACT_OWNER_ETH_ACCOUNT_PASSWORD, "https://ropsten.infura.io/v3/59fb36a36fa4474b890c13dd30038be5")
+      return new HDWalletProvider("doughnut_armenian_parallel_firework_backbite_employer_singlet", "https://ropsten.infura.io/v3/59fb36a36fa4474b890c13dd30038be5")
       },
       gas: 6721975,
       // ETH_NETWORK_ID is set in a Circle context.
-      network_id: process.env.ETH_NETWORK_ID
+      network_id: 3
     },
+  },
   // Configure your compilers
   compilers: {
     solc: {
