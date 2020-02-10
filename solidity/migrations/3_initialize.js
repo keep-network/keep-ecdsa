817311682493ab2bd9186efd5cbf5c329e273293
Jakub Nowakowski <jakub.nowakowski@keep.network>
2020-02-10 14:15:30 +0100

    Added Registry Stub for tests
    We added a stub of the contract which is expected to be copied from
    keep-core. We use the stub for deployment to run tests.

--- solidity/migrations/3_initialize.js
+++ solidity/migrations/3_initialize.js
@@ -1,9 +1,16 @@
-const ECDSAKeepFactory = artifacts.require("./ECDSAKeepFactory.sol");
-const Registry = artifacts.require("./Registry.sol");
+const ECDSAKeepFactory = artifacts.require("ECDSAKeepFactory");
 
 module.exports = async function (deployer) {
     await ECDSAKeepFactory.deployed()
-    const registry = await Registry.deployed();
+
+    let registry
+    if (process.env.TEST) {
+        RegistryStub = artifacts.require("RegistryStub")
+        registry = await RegistryStub.new()
+    } else {
+        Registry = artifacts.require("Registry")
+        registry = await Registry.deployed()
+    }
 
     await registry.approveOperatorContract(ECDSAKeepFactory.address)
     console.log(`approved operator contract [${ECDSAKeepFactory.address}] in registry`)
---
+++ solidity/test/contracts/RegistryStub.sol
@@ -0,0 +1,7 @@
+pragma solidity ^0.5.4;
+
+/// @title Registry Stub
+/// @dev This contract is for testing purposes only.
+contract RegistryStub {
+    function approveOperatorContract(address operatorContract) public {}
+}
