0c256e1a01f927f06ccdf30071a94e69f12f0a7f
Jakub Nowakowski <jakub.nowakowski@keep.network>
2020-02-11 00:51:19 +0100

    Added ECDSAKeepFactoryExposed for testing
    The contract extends ECDSAKeepFactory with no changes to functions
    implementation, it just adds some public functions to be able to call
    internal functions from the original factory contract.

---
+++ solidity/test/contracts/ECDSAKeepFactoryExposed.sol
@@ -0,0 +1,35 @@
+pragma solidity ^0.5.4;
+
+import "../../contracts/ECDSAKeepFactory.sol";
+
+/// @title ECDSA Keep Factory Stub
+/// @dev This contract is for testing purposes only.
+contract ECDSAKeepFactoryExposed is ECDSAKeepFactory {
+    constructor(
+        address sortitionPoolFactory,
+        address tokenStaking,
+        address keepBonding,
+        address randomBeacon
+    )
+        public
+        ECDSAKeepFactory(
+            sortitionPoolFactory,
+            tokenStaking,
+            keepBonding,
+            randomBeacon
+        )
+    {}
+
+    // @dev Returns address of registered signer pool.
+    function getSignerPool(address application) public view returns (address) {
+        return candidatesPools[application];
+    }
+
+    function initialGroupSelectionSeed(uint256 _groupSelectionSeed) public {
+        groupSelectionSeed = _groupSelectionSeed;
+    }
+
+    function getGroupSelectionSeed() public view returns (uint256) {
+        return groupSelectionSeed;
+    }
+}
