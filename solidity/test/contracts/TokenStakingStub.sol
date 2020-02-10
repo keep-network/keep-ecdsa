78467589d68d7392ed55c6a6243c0a81cf077d14
Jakub Nowakowski <jakub.nowakowski@keep.network>
2020-02-10 12:40:34 +0100

    Removed default balance in TokenStakingStub
    We will need to set the balance at the begining of tests.

--- solidity/test/contracts/TokenStakingStub.sol
+++ solidity/test/contracts/TokenStakingStub.sol
@@ -5,7 +5,7 @@ import "@keep-network/sortition-pools/contracts/api/IStaking.sol";
 /// @title Token Staking Stub
 /// @dev This contract is for testing purposes only.
 contract TokenStakingStub is IStaking {
-    uint256 balance = 1;
+    uint256 balance;
 
     /// @dev Sets balance variable value.
     function setBalance(uint256 _balance) public {
