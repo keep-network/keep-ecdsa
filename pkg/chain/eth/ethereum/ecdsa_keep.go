e37cc3b0c27f8fb7701ee6a3b32bebfd6e500f45
Lukasz Zimnoch <lukasz.zimnoch@keep.network>
2020-02-12 17:44:05 +0100

    Automatic chain events resubscriptions

--- pkg/chain/eth/ethereum/ecdsa_keep.go
+++ pkg/chain/eth/ethereum/ecdsa_keep.go
@@ -3,12 +3,77 @@ package ethereum
 import (
 	"fmt"
 	"sync"
+	"time"
 
 	"github.com/keep-network/keep-core/pkg/subscription"
 	"github.com/keep-network/keep-tecdsa/pkg/chain/eth/gen/abi"
 )
 
 func (ec *EthereumChain) watchSignatureRequested(
+	keepContract *abi.ECDSAKeep,
+	success func(event *abi.ECDSAKeepSignatureRequested),
+	fail func(err error) error,
+) (subscription.EventSubscription, error) {
+	errorChan := make(chan error)
+	unsubscribeChan := make(chan struct{})
+
+	// Delay which must be preserved before a new resubscription attempt.
+	// There is no sense to resubscribe immediately after the fail of current
+	// subscription because the publisher must have some time to recover.
+	retryDelay := 5 * time.Second
+
+	watch := func() {
+		failCallback := func(err error) error {
+			fail(err)
+			errorChan <- err // trigger resubscription signal
+			return err
+		}
+
+		subscription, err := ec.subscribeSignatureRequested(
+			keepContract,
+			success,
+			failCallback,
+		)
+		if err != nil {
+			errorChan <- err // trigger resubscription signal
+			return
+		}
+
+		// wait for unsubscription signal
+		<-unsubscribeChan
+		subscription.Unsubscribe()
+	}
+
+	// trigger the resubscriber goroutine
+	go func() {
+		go watch() // trigger first subscription
+
+		for {
+			select {
+			case <-errorChan:
+				logger.Warning(
+					"subscription to event SignatureRequested terminated with error; " +
+						"resubscription attempt will be performed after the retry delay",
+				)
+				time.Sleep(retryDelay)
+				go watch()
+			case <-unsubscribeChan:
+				// shutdown the resubscriber goroutine on unsubscribe signal
+				return
+			}
+		}
+	}()
+
+	// closing the unsubscribeChan will trigger a unsubscribe signal and
+	// run unsubscription for all subscription instances
+	unsubscribeCallback := func() {
+		close(unsubscribeChan)
+	}
+
+	return subscription.NewEventSubscription(unsubscribeCallback), nil
+}
+
+func (ec *EthereumChain) subscribeSignatureRequested(
 	keepContract *abi.ECDSAKeep,
 	success func(event *abi.ECDSAKeepSignatureRequested),
 	fail func(err error) error,
--- pkg/chain/eth/ethereum/ecdsa_keep_factory.go
+++ pkg/chain/eth/ethereum/ecdsa_keep_factory.go
@@ -3,12 +3,75 @@ package ethereum
 import (
 	"fmt"
 	"sync"
+	"time"
 
 	"github.com/keep-network/keep-core/pkg/subscription"
 	"github.com/keep-network/keep-tecdsa/pkg/chain/eth/gen/abi"
 )
 
 func (ec *EthereumChain) watchECDSAKeepCreated(
+	success func(event *abi.ECDSAKeepFactoryECDSAKeepCreated),
+	fail func(err error) error,
+) (subscription.EventSubscription, error) {
+	errorChan := make(chan error)
+	unsubscribeChan := make(chan struct{})
+
+	// Delay which must be preserved before a new resubscription attempt.
+	// There is no sense to resubscribe immediately after the fail of current
+	// subscription because the publisher must have some time to recover.
+	retryDelay := 5 * time.Second
+
+	watch := func() {
+		failCallback := func(err error) error {
+			fail(err)
+			errorChan <- err // trigger resubscription signal
+			return err
+		}
+
+		subscription, err := ec.subscribeECDSAKeepCreated(
+			success,
+			failCallback,
+		)
+		if err != nil {
+			errorChan <- err // trigger resubscription signal
+			return
+		}
+
+		// wait for unsubscription signal
+		<-unsubscribeChan
+		subscription.Unsubscribe()
+	}
+
+	// trigger the resubscriber goroutine
+	go func() {
+		go watch() // trigger first subscription
+
+		for {
+			select {
+			case <-errorChan:
+				logger.Warning(
+					"subscription to event ECDSAKeepCreated terminated with error; " +
+						"resubscription attempt will be performed after the retry delay",
+				)
+				time.Sleep(retryDelay)
+				go watch()
+			case <-unsubscribeChan:
+				// shutdown the resubscriber goroutine on unsubscribe signal
+				return
+			}
+		}
+	}()
+
+	// closing the unsubscribeChan will trigger a unsubscribe signal and
+	// run unsubscription for all subscription instances
+	unsubscribeCallback := func() {
+		close(unsubscribeChan)
+	}
+
+	return subscription.NewEventSubscription(unsubscribeCallback), nil
+}
+
+func (ec *EthereumChain) subscribeECDSAKeepCreated(
 	success func(event *abi.ECDSAKeepFactoryECDSAKeepCreated),
 	fail func(err error) error,
 ) (subscription.EventSubscription, error) {
