0f95d875a70c4b32079e33f3d43e0fc74173d681
Jakub Nowakowski <jakub.nowakowski@keep.network>
2020-02-12 17:51:50 +0100

    Load Registry contract from published by keep-core
    We load the contract to truffle object to be able to interact with it.

--- solidity/migrations/3_initialize.js
+++ solidity/migrations/3_initialize.js
@@ -1,7 +1,9 @@
-const ECDSAKeepFactory = artifacts.require("ECDSAKeepFactory");
+const ECDSAKeepFactory = artifacts.require("ECDSAKeepFactory")
+const truffleContract = require("@truffle/contract")
 
 let { RegistryAddress } = require('./externals')
-module.exports = async function (deployer) {
+
+module.exports = async function (deployer, network, accounts) {
     await ECDSAKeepFactory.deployed()
 
     let registry
@@ -9,10 +11,18 @@ module.exports = async function (deployer) {
         RegistryStub = artifacts.require("RegistryStub")
         registry = await RegistryStub.new()
     } else {
-        Registry = artifacts.require("Registry")
+        // Read compiled contract artifact from 
+        const registryJSON = require('@keep-network/keep-core/build/truffle/Registry.json')
+        const Registry = truffleContract(registryJSON)
+        Registry.setProvider(web3.currentProvider);
+
         registry = await Registry.at(RegistryAddress)
     }
 
-    await registry.approveOperatorContract(ECDSAKeepFactory.address)
+    await registry.approveOperatorContract(
+        ECDSAKeepFactory.address,
+        { from: accounts[0] }
+    )
+
     console.log(`approved operator contract [${ECDSAKeepFactory.address}] in registry`)
-};
+}
