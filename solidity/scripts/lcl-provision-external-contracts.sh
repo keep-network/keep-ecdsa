57c425fa4d6ffd304e63649e7e8c793f6a4aae1c
Jakub Nowakowski <jakub.nowakowski@keep.network>
2020-02-12 17:58:16 +0100

    Replaced local copy external artifacts with addreses fetch
    We no longer require artifacts of external contracts. We decided to
    fetch addresses from external systems and define them in a config file.
    Here we replace the script to obtain the required contracts addresses
    from deployed keep-core.

---
+++ solidity/scripts/lcl-provision-external-contracts.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+set -ex
+
+# Fetch addresses of contacts migrated from keep-network/keep-core project.
+# The `keep-core` contracts have to be migrated before running this script.
+# It requires `KEEP_CORE_ARTIFACTS` variable to pointing to a directory where
+# contracts artifacts after migrations are located.
+# It takes address of the first network entry in the migration artifact.
+# 
+# Sample command:
+#   KEEP_CORE_ARTIFACTS=~/go/src/github.com/keep-network/keep-core/contracts/solidity/build/contracts \
+#   ./lcl-provision-external-contracts.sh
+
+REGISTRY_CONTRACT_DATA="Registry.json"
+REGISTRY_PROPERTY="RegistryAddress"
+
+TOKEN_STAKING_CONTRACT_DATA="TokenStaking.json"
+TOKEN_STAKING_PROPERTY="TokenStakingAddress"
+
+RANDOM_BEACON_CONTRACT_DATA="KeepRandomBeaconService.json"
+RANDOM_BEACON_PROPERTY="RandomBeaconAddress"
+
+# Query to get address of the deployed contract for the first network on the list.
+JSON_QUERY="[.networks[].address][0]"
+
+DESTINATION_FILE=$(realpath $(dirname $0)/../migrations/externals.js)
+
+function fetch_registry_contract_address() {
+  local contractDataPath=$(realpath $KEEP_CORE_ARTIFACTS/$REGISTRY_CONTRACT_DATA)
+  local ADDRESS=$(cat ${contractDataPath} | jq "${JSON_QUERY}" | tr -d '"')
+  sed -i -e "/${REGISTRY_PROPERTY}/s/0x[a-fA-F0-9]\{0,40\}/${ADDRESS}/" $DESTINATION_FILE
+}
+
+function fetch_token_staking_contract_address() {
+  local contractDataPath=$(realpath $KEEP_CORE_ARTIFACTS/$TOKEN_STAKING_CONTRACT_DATA)
+  local ADDRESS=$(cat ${contractDataPath} | jq "${JSON_QUERY}" | tr -d '"')
+  sed -i -e "/${TOKEN_STAKING_PROPERTY}/s/0x[a-fA-F0-9]\{0,40\}/${ADDRESS}/" $DESTINATION_FILE
+}
+
+function fetch_random_beacon_contract_address() {
+  local contractDataPath=$(realpath $KEEP_CORE_ARTIFACTS/$RANDOM_BEACON_CONTRACT_DATA)
+  local ADDRESS=$(cat ${contractDataPath} | jq "${JSON_QUERY}" | tr -d '"')
+  sed -i -e "/${RANDOM_BEACON_PROPERTY}/s/0x[a-fA-F0-9]\{0,40\}/${ADDRESS}/" $DESTINATION_FILE
+}
+
+fetch_registry_contract_address
+fetch_token_staking_contract_address
+fetch_random_beacon_contract_address
