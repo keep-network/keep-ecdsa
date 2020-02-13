37541ac51c97f51c456d8a9f0dc9252eef3a62ec
Piotr Dyraga <piotr.dyraga@keep.network>
2020-02-13 18:22:39 +0100

    Improved BondedECDSAKeepVendorImplV1 docs

--- solidity/contracts/BondedECDSAKeepVendorImplV1.sol
+++ solidity/contracts/BondedECDSAKeepVendorImplV1.sol
@@ -3,10 +3,11 @@ pragma solidity ^0.5.4;
 import "openzeppelin-solidity/contracts/ownership/Ownable.sol";
 import "./api/IBondedECDSAKeepVendor.sol";
 
-/// @title Bonded ECDSA Keep Vendor.
-/// @notice The contract can be used to obtain a new Bonded ECDSA keep.
-/// @dev Interacts with ECDSA keep factory to obtain a new instance of the ECDSA
-/// keep. The latest version of ECDSA keep factory can be registered for a vendor.
+/// @title Bonded ECDSA Keep Vendor
+/// @notice The contract is used to obtain a new Bonded ECDSA keep.
+/// @dev Interacts with ECDSA keep factory to obtain a new instance of a bonded
+/// ECDSA keep. The latest version of ECDSA keep factory can be registered for
+/// a vendor.
 contract BondedECDSAKeepVendorImplV1 is IBondedECDSAKeepVendor, Ownable {
 
     // Address of ECDSA keep factory.
@@ -18,16 +19,13 @@ contract BondedECDSAKeepVendorImplV1 is IBondedECDSAKeepVendor, Ownable {
     /// @param _factory ECDSA keep factory address.
     function registerFactory(address payable _factory) external onlyOwner {
         require(_factory != address(0), "Incorrect factory address");
-
         keepFactory = _factory;
     }
 
     /// @notice Select the latest ECDSA keep factory.
-    /// @dev This is a stub implementation returning the latest factory.
     /// @return ECDSA keep factory address.
     function selectFactory() public view returns (address payable) {
         require(keepFactory != address(0), "Keep factory is not registered");
-
         return keepFactory;
     }
 }
