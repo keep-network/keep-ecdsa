874a8fc5156749cdf6b157455491aa7a19d88cc2
Piotr Dyraga <piotr.dyraga@keep.network>
2020-02-18 23:13:18 +0100

    Limitting relay request gas cost to 400k
    Relay request should not cost more than 360k but we include a safety
    margin for future upgrades.

--- solidity/contracts/ECDSAKeepFactory.sol
+++ solidity/contracts/ECDSAKeepFactory.sol
@@ -327,8 +327,12 @@ contract ECDSAKeepFactory is
         );
 
         // Call the random beacon to get a random group selection seed.
-        // Limiting forwarded gas to prevent malicious behavior.
-        (bool success, ) = address(randomBeacon).call.gas(300000).value(msg.value)(
+        //
+        // Limiting forwarded gas to prevent malicious behavior in case the
+        // beacon service contract gets compromised. Relay request should not
+        // consume more than 360k of gas. We set the limit to 400k to have
+        // a safety margin for future updates.
+        (bool success, ) = address(randomBeacon).call.gas(400000).value(msg.value)(
             abi.encodeWithSignature(
                 "requestRelayEntry(address,string,uint256)",
                 address(this),
