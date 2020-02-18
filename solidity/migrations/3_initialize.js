f8d21e35c4cdaf3f20c26956c8513f64bad2c9bd
Jakub Nowakowski <jakub.nowakowski@keep.network>
2020-02-18 22:10:33 +0100

    Updated migrations and init scripts to handle Bonded* prefix

--- solidity/migrations/3_initialize.js
+++ solidity/migrations/3_initialize.js
@@ -1,12 +1,12 @@
 const BondedECDSAKeepVendor = artifacts.require("BondedECDSAKeepVendor")
 const BondedECDSAKeepVendorImplV1 = artifacts.require("BondedECDSAKeepVendorImplV1")
-const ECDSAKeepFactory = artifacts.require("ECDSAKeepFactory")
+const BondedECDSAKeepFactory = artifacts.require("BondedECDSAKeepFactory")
 const Registry = artifacts.require("Registry")
 
 let { RegistryAddress } = require('./external-contracts')
 
 module.exports = async function (deployer) {
-    const ecdsaKeepFactory = await ECDSAKeepFactory.deployed()
+    await BondedECDSAKeepFactory.deployed()
 
     let registry
     if (process.env.TEST) {
@@ -19,13 +19,13 @@ module.exports = async function (deployer) {
     await vendor.initialize(registry.address)
 
     // Configure registry
-    await registry.approveOperatorContract(ECDSAKeepFactory.address)
-    console.log(`approved operator contract [${ECDSAKeepFactory.address}] in registry`)
+    await registry.approveOperatorContract(BondedECDSAKeepFactory.address)
+    console.log(`approved operator contract [${BondedECDSAKeepFactory.address}] in registry`)
 
     // Set service contract owner as operator contract upgrader by default
     const operatorContractUpgrader = await vendor.owner()
     await registry.setOperatorContractUpgrader(vendor.address, operatorContractUpgrader)
 
     // Register keep factory
-    await vendor.registerFactory(ECDSAKeepFactory.address)
+    await vendor.registerFactory(BondedECDSAKeepFactory.address)
 }
