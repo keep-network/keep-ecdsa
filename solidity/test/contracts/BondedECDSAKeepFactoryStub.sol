ca96c9f8e40d7ab71a0d5e5b7e147bbcbae45a18
NicholasDotSol <nicholas.evans@thesis.co>
2020-03-02 15:56:09 -0600

    Simple openKeep stub
    stubOpenKeep is an easy way to open a keep with no extra requirements.
    It's beneficial when mny keeps are needed
    and the initializatoin logic is not important.

--- solidity/test/contracts/BondedECDSAKeepFactoryStub.sol
+++ solidity/test/contracts/BondedECDSAKeepFactoryStub.sol
@@ -29,4 +29,24 @@ contract BondedECDSAKeepFactoryStub is BondedECDSAKeepFactory {
     function getGroupSelectionSeed() public view returns (uint256) {
         return groupSelectionSeed;
     }
+
+    /// @notice Opens a new ECDSA keep.
+    /// @param _owner Address of the keep owner.
+    /// @return Created keep address.
+    function stubOpenKeep(
+        address _owner
+    ) external payable returns (address keepAddress) {
+
+        address[] memory members;
+        keepAddress = createClone(masterBondedECDSAKeepAddress);
+        BondedECDSAKeep keep = BondedECDSAKeep(keepAddress);
+        keep.initialize(
+            _owner,
+            members,
+            0,
+            address(0),
+            address(0)
+        );
+        keeps.push(keepAddress);
+    }
 }
