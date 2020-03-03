d04f80d1110bff0031767f00e5715c03e5db196f
Piotr Dyraga <piotr.dyraga@keep.network>
2020-03-03 11:40:20 +0100

    Wait until the operator is registered in a pool
    Added a new function that waits until the operator is registered.
    Previously, we had a situation that for every new registered operator we
    were triggering the monitoring right away. This was problematic because
    all monitoring checks were instantly failing as the transaction was
    usually not yet mined. From now on, before we start monitoring, we wait
    until the operator is confirmed as registered in the pool.

--- pkg/client/client.go
+++ pkg/client/client.go
@@ -106,19 +106,18 @@ func Initialize(
 		}
 	})
 
-	// Register client as a candidate member for keep. Validates if the client
-	// is already registered. If not checks client's eligibility and retries
-	// registration until the client is eligible to complete it.
-	// Also, triggers the monitoring process to keep the operator's status
-	// up to date in the pool.
 	for _, application := range sanctionedApplications {
 		go checkStatusAndRegisterForApplication(ctx, ethereumChain, application)
 	}
 }
 
-// checkStatusAndRegisterForApplication checks if the current operator is
-// registered as a signer candidate for the given application. If not, it
-// triggers registration and status monitoring process.
+// checkStatusAndRegisterForApplication checks whether the operator is
+// registered as a member candidate for keep for the given application.
+// If not checks operators's eligibility and retries until the operator is
+// eligible. Eventually, once the operator is eligible, it is registered
+// as a keep member candidate.
+// Also, once the client is confirmed as registered, it triggers the monitoring
+// process to keep the operator's status up to date in the pool.
 func checkStatusAndRegisterForApplication(
 	ctx context.Context,
 	ethereumChain eth.Handle,
@@ -134,23 +133,25 @@ func checkStatusAndRegisterForApplication(
 		return
 	}
 
-	if !isRegistered {
-		registerAsMemberCandidate(ctx, ethereumChain, application)
-	} else {
-		logger.Debugf(
-			"client is already registered as member candidate for application: [%s]",
-			application.String(),
-		)
-	}
+	go func() {
+		if !isRegistered {
+			// if the operator is not registered, we need to register it and
+			// wait until registration is confirmed
+			registerAsMemberCandidate(ctx, ethereumChain, application)
+			waitUntilRegistered(ctx, ethereumChain, application)
+		}
 
-	// When the operator is registered monitor it's status.
-	go monitorSignerPoolStatus(ctx, ethereumChain, application)
+		// once the registration is confirmed or if the client is already
+		// registered, we can start to monitor the status
+		monitorSignerPoolStatus(ctx, ethereumChain, application)
+	}()
 }
 
-// registerAsMemberCandidate checks current operator's stake balance and if it's
-// positive registers the operator as a member candidate for the given application.
-// It retries the operation after each new mined block until the operator is
-// successfully registered.
+// registerAsMemberCandidate checks current operator's eligibility to become
+// keep member candidate for the given application and if it is positive,
+// registers the operator as a keep member candidate for the given application.
+// If operator is not eligible it executes the check for each new mined block
+// until the operator is finally eligible and can be registered.
 func registerAsMemberCandidate(
 	parentCtx context.Context,
 	ethereumChain eth.Handle,
@@ -175,6 +176,8 @@ func registerAsMemberCandidate(
 			}
 
 			if !isEligible {
+				// if the operator is not yet eligible wait for the next
+				// block and execute the check again
 				logger.Warningf(
 					"operator is not eligible for application [%s]",
 					application.String(),
@@ -182,8 +185,10 @@ func registerAsMemberCandidate(
 				continue
 			}
 
-			logger.Info(
-				"Registering member candidate for application [%s]",
+			// if the operator is eligible, register it as a keep member
+			// candidate for this application
+			logger.Infof(
+				"registering member candidate for application [%s]",
 				application.String(),
 			)
 			if err := ethereumChain.RegisterAsMemberCandidate(application); err != nil {
@@ -195,9 +200,72 @@ func registerAsMemberCandidate(
 				continue
 			}
 
+			// we cancel the context in case the registration was successful,
+			// we don't want to do it again
 			cancel()
 		case <-ctx.Done():
 			return
+		}
+	}
+}
+
+// waitUntilRegistered blocks until the operator is registered as a keep member
+// candidate for the given application. It executes the check in the predefined
+// interval, for each couple of blocks.
+func waitUntilRegistered(
+	ctx context.Context,
+	ethereumChain eth.Handle,
+	application common.Address,
+) {
+	checkIntervalBlocks := uint64(5)
+
+	for {
+		if ctx.Err() != nil {
+			return
+		}
+
+		isRegistered, err := ethereumChain.IsRegisteredForApplication(application)
+		if err != nil {
+			logger.Errorf(
+				"failed to check if member is registered for application [%s]: [%v]",
+				application.String(),
+				err,
+			)
+			continue
+		}
+
+		if isRegistered {
+			// if the operator is registered, we return
+			logger.Infof(
+				"operator is registered for application [%s]",
+				application.String(),
+			)
+			return
+		}
+
+		// if the operator is not yet registered, we set the new check
+		// tick and we will execute the status check again
+		logger.Infof(
+			"operator is not yet registered for application [%s]",
+			application.String(),
+		)
+
+		blockCounter := ethereumChain.BlockCounter()
+
+		currentBlock, err := blockCounter.CurrentBlock()
+		if err != nil {
+			logger.Errorf("failed to check the current block: [%v]", err)
+		}
+
+		nextCheckBlock := currentBlock + checkIntervalBlocks
+
+		if err = blockCounter.WaitForBlockHeight(nextCheckBlock); err != nil {
+			logger.Errorf(
+				"failed waiting for block [%v]: [%v]",
+				nextCheckBlock,
+				err,
+			)
+			continue
 		}
 	}
 }
