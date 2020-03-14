a90eea998fcf4210d5b4109e583632fe67b6a1cd
Piotr Dyraga <piotr.dyraga@keep.network>
2020-03-14 15:48:57 +0100

    Import IERC20 via openzeppelin-solidity
    Importing via `@openzeppelin` makes Go bindings code generation fails as
    the path for `@openzeppelin` is not configured in the Makefile.
    
    I could add `@openzeppelin` path to the Makefile but since all other
    contracts use `openzeppelin-solidity` import, I altered the single
    `@openzeppelin` import to `openzeppeling-solidity for consistency.

--- solidity/contracts/ECDSAKeepRewards.sol
+++ solidity/contracts/ECDSAKeepRewards.sol
@@ -1,6 +1,6 @@
 pragma solidity ^0.5.4;
 
-import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "openzeppelin-solidity/contracts/token/ERC20/IERC20.sol";
 import "openzeppelin-solidity/contracts/math/SafeMath.sol";
 
 contract ECDSAKeepRewards {
