d790d765b21ebf01f68e8e032f2d3b399c020fa9
Jakub Nowakowski <jakub.nowakowski@keep.network>
2020-03-16 15:14:36 +0100

    Updated migrations for impl initialization
    Added initialization of impl contract to vendor constructor.

--- solidity/migrations/3_initialize.js
+++ solidity/migrations/3_initialize.js
@@ -5,7 +5,7 @@ const Registry = artifacts.require("Registry")
 
 let { RegistryAddress } = require('./external-contracts')
 
-module.exports = async function (deployer) {
+module.exports = async function () {
     await BondedECDSAKeepFactory.deployed()
 
     let registry
@@ -15,18 +15,23 @@ module.exports = async function (deployer) {
         registry = await Registry.at(RegistryAddress)
     }
 
-    const vendor = await BondedECDSAKeepVendorImplV1.at(BondedECDSAKeepVendor.address)
+    const proxy = await BondedECDSAKeepVendor.deployed()
+    const vendor = await BondedECDSAKeepVendorImplV1.at(proxy.address)
 
     // Initialize vendor contract
-    await vendor.initialize(registry.address, BondedECDSAKeepFactory.address)
-    console.log(`registered factory [${BondedECDSAKeepFactory.address}] in vendor contract`)
+    if (!(await vendor.initialized())) {
+        throw Error("vendor contract not initialized")
+    }
+
+    const factoryAddress = await vendor.selectFactory()
+    console.log(`current factory address: [${factoryAddress}]`)
 
     // Configure registry
     await registry.approveOperatorContract(BondedECDSAKeepFactory.address)
     console.log(`approved operator contract [${BondedECDSAKeepFactory.address}] in registry`)
 
     // Set service contract owner as operator contract upgrader by default
-    const operatorContractUpgrader = await vendor.owner()
+    const operatorContractUpgrader = await proxy.owner()
     await registry.setOperatorContractUpgrader(vendor.address, operatorContractUpgrader)
     console.log(`set operator [${operatorContractUpgrader}] as [${vendor.address}] contract upgrader`)
 }
