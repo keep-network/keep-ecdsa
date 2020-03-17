9041e123facd5e5617037340f184b2bdc7a83ac6
Promethea Raschke <promethea.raschke@keep.network>
2020-03-17 15:05:53 +0000

    Add documentation of the reward contract and its usage

--- solidity/contracts/ECDSAKeepRewards.sol
+++ solidity/contracts/ECDSAKeepRewards.sol
@@ -4,6 +4,53 @@ import "openzeppelin-solidity/contracts/token/ERC20/IERC20.sol";
 import "openzeppelin-solidity/contracts/token/ERC20/SafeERC20.sol";
 import "openzeppelin-solidity/contracts/math/SafeMath.sol";
 
+/// @title ECDSAKeepRewards
+/// @dev A contract for distributing KEEP token rewards to ECDSA keeps.
+/// When a reward contract is created,
+/// the creator defines a reward schedule
+/// consisting of one or more reward intervals and their interval weights,
+/// the length of reward intervals,
+/// and the quota of how many keeps must be created in an interval
+/// for the full reward for that interval to be paid out.
+///
+/// The amount of KEEP to be distributed is determined by funding the contract,
+/// and additional KEEP can be added at any time.
+/// The reward contract is funded with `approveAndCall` with no extra data,
+/// but it also collects any KEEP mistakenly sent to it in any other way.
+///
+/// An interval is defined by the timestamps [startOf, endOf);
+/// a keep created at the time `startOf(i)` belongs to interval `i`
+/// and one created at `endOf(i)` belongs to `i+1`.
+///
+/// When an interval is over,
+/// it will be allocated a percentage of the remaining unallocated rewards
+/// based on its weight,
+/// and adjusted by the number of keeps created in the interval
+/// if the quota is not met.
+/// The adjustment for not meeting the keep quota is a percentage
+/// that equals the percentage of the quota that was met;
+/// if the number of keeps created is 80% of the quota
+/// then 80% of the base reward will be allocated for the interval.
+///
+/// Any unallocated rewards will stay in the unallocated rewards pool,
+/// to be allocated for future intervals.
+/// Intervals past the initially defined schedule have a weight of 100%,
+/// meaning that all remaining unallocated rewards
+/// will be allocated to the interval.
+///
+/// ECDSA keeps created by the defined `factory` can receive rewards
+/// once the interval they were created in is over,
+/// and the keep has closed happily.
+/// There is no time limit to receiving rewards,
+/// nor is there need to wait for all keeps from the interval to close.
+/// Calling `receiveReward` automatically allocates the rewards
+/// for the interval the specified keep was created in
+/// and all previous intervals.
+///
+/// If a keep is terminated,
+/// that fact can be reported to the reward contract.
+/// Reporting a terminated keep returns its allocated reward
+/// to the pool of unallocated rewards.
 contract ECDSAKeepRewards {
     using SafeMath for uint256;
     using SafeERC20 for IERC20;
