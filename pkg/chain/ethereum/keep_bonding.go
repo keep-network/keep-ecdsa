53673bdd96ecf2799834badc93e7ad91b55ba797
Jakub Nowakowski <jakub.nowakowski@keep.network>
2020-04-08 15:54:54 +0200

    Implemented status events monitoring functions for ethereum

---
+++ pkg/chain/ethereum/keep_bonding.go
@@ -0,0 +1,61 @@
+package ethereum
+
+import (
+	"fmt"
+	"math/big"
+
+	"github.com/ethereum/go-ethereum/common"
+	"github.com/keep-network/keep-common/pkg/subscription"
+	eth "github.com/keep-network/keep-ecdsa/pkg/chain"
+)
+
+// OnUnbondedValueWithdrawn installs a callback that will be called on unbonded
+// value withdraw for the given operator.
+func (ec *EthereumChain) OnUnbondedValueWithdrawn(
+	operatorAddress common.Address,
+	handler func(event *eth.UnbondedValueWithdrawnEvent),
+) (subscription.EventSubscription, error) {
+	return ec.keepBondingContract.WatchUnbondedValueWithdrawn(
+		func(
+			Operator common.Address,
+			Amount *big.Int,
+			blockNumber uint64,
+		) {
+			handler(&eth.UnbondedValueWithdrawnEvent{
+				Operator: Operator,
+			})
+		},
+		func(err error) error {
+			return fmt.Errorf("watch unbonded value withdrawn failed: [%v]", err)
+		},
+		[]common.Address{operatorAddress},
+	)
+}
+
+// OnBondCreated installs a callback that will be called on bond creation
+// for the given operator.
+func (ec *EthereumChain) OnBondCreated(
+	operatorAddress common.Address,
+	handler func(event *eth.BondCreatedEvent),
+) (subscription.EventSubscription, error) {
+	return ec.keepBondingContract.WatchBondCreated(
+		func(
+			Operator common.Address,
+			Holder common.Address,
+			SortitionPool common.Address,
+			ReferenceID *big.Int,
+			Amount *big.Int,
+			blockNumber uint64,
+		) {
+			handler(&eth.BondCreatedEvent{
+				Operator: Operator,
+			})
+		},
+		func(err error) error {
+			return fmt.Errorf("watch bond creation failed: [%v]", err)
+		},
+		[]common.Address{operatorAddress},
+		[]common.Address{},
+		[]common.Address{},
+	)
+}
---
+++ pkg/chain/ethereum/token_staking.go
@@ -0,0 +1,56 @@
+package ethereum
+
+import (
+	"fmt"
+	"math/big"
+
+	"github.com/ethereum/go-ethereum/common"
+	"github.com/keep-network/keep-common/pkg/subscription"
+	eth "github.com/keep-network/keep-ecdsa/pkg/chain"
+)
+
+// OnTokensSlashed installs a callback that will be called on token stake
+// slash for the given operator.
+func (ec *EthereumChain) OnTokensSlashed(
+	operatorAddress common.Address,
+	handler func(event *eth.TokensSlashedEvent),
+) (subscription.EventSubscription, error) {
+	return ec.tokenStakingContract.WatchTokensSlashed(
+		func(
+			Operator common.Address,
+			Amount *big.Int,
+			blockNumber uint64,
+		) {
+			handler(&eth.TokensSlashedEvent{
+				Operator: Operator,
+			})
+		},
+		func(err error) error {
+			return fmt.Errorf("watch tokens slashing failed: [%v]", err)
+		},
+		[]common.Address{operatorAddress},
+	)
+}
+
+// OnTokensSeized installs a callback that will be called on token stake
+// seizure for the given operator.
+func (ec *EthereumChain) OnTokensSeized(
+	operatorAddress common.Address,
+	handler func(event *eth.TokensSeizedEvent),
+) (subscription.EventSubscription, error) {
+	return ec.tokenStakingContract.WatchTokensSeized(
+		func(
+			Operator common.Address,
+			Amount *big.Int,
+			blockNumber uint64,
+		) {
+			handler(&eth.TokensSeizedEvent{
+				Operator: Operator,
+			})
+		},
+		func(err error) error {
+			return fmt.Errorf("watch tokens seizure failed: [%v]", err)
+		},
+		[]common.Address{operatorAddress},
+	)
+}
