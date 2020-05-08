3bb99c6eba73e97f2a0f12c74f556ab6b69054b0
Piotr Dyraga <piotr.dyraga@keep.network>
2020-05-08 09:28:28 +0200

    Applied linting to BondedECDSAKeepFactory

--- solidity/contracts/BondedECDSAKeepFactory.sol
+++ solidity/contracts/BondedECDSAKeepFactory.sol
@@ -486,15 +486,14 @@ contract BondedECDSAKeepFactory is
     /// @param _application Address of the application.
     /// @return The sum of all registered operators' weights in the pool.
     /// Reverts if sortition pool for the application does not exist.
-    function getSortitionPoolWeight(
-        address _application
-    ) public view returns (uint256) {
+    function getSortitionPoolWeight(address _application)
+        public
+        view
+        returns (uint256)
+    {
         address poolAddress = candidatesPools[_application];
 
-        require(
-            poolAddress != address(0),
-            "No pool found for the application"
-        );
+        require(poolAddress != address(0), "No pool found for the application");
 
         return BondedSortitionPool(poolAddress).totalWeight();
     }
