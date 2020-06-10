c3de57fd0c2821753f94ee69d4638a9273917d59
Jakub Nowakowski <jakub.nowakowski@keep.network>
2020-06-10 13:03:31 +0200

    Check if beneficiary is set before withdraw in keep
    Before sending the valuse to beneficiary we need to check if it's set
    for the operator. It could happen that the value was sent to the zero
    address in case of beneficiary not defined.

--- solidity/contracts/BondedECDSAKeep.sol
+++ solidity/contracts/BondedECDSAKeep.sol
@@ -485,10 +485,14 @@ contract BondedECDSAKeep is IBondedECDSAKeep {
 
         memberETHBalances[_member] = 0;
 
+        address beneficiary = tokenStaking.beneficiaryOf(_member);
+        require(
+            beneficiary != address(0),
+            "Beneficiary not defined for the operator"
+        );
+
         /* solium-disable-next-line security/no-call-value */
-        (bool success, ) = tokenStaking.beneficiaryOf(_member).call.value(
-            value
-        )("");
+        (bool success, ) = beneficiary.call.value(value)("");
 
         require(success, "Transfer failed");
     }
