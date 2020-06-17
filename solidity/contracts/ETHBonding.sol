e63faebb7a002b33dca4b9259adff1a6932df0fb
Jakub Nowakowski <jakub.nowakowski@keep.network>
2020-06-17 17:08:13 +0200

    ETH bonding contract
    The contract is used for ETH only bonding. It defined withdraw function
    that is different from the one defined in KeepBonding contract as there
    is no grantee role. Other parts of the code are common for token bonding
    and eth bonding.

---
+++ solidity/contracts/ETHBonding.sol
@@ -0,0 +1,70 @@
+/**
+▓▓▌ ▓▓ ▐▓▓ ▓▓▓▓▓▓▓▓▓▓▌▐▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓ ▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓ ▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▄
+▓▓▓▓▓▓▓▓▓▓ ▓▓▓▓▓▓▓▓▓▓▌▐▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓ ▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓ ▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓
+  ▓▓▓▓▓▓    ▓▓▓▓▓▓▓▀    ▐▓▓▓▓▓▓    ▐▓▓▓▓▓   ▓▓▓▓▓▓     ▓▓▓▓▓   ▐▓▓▓▓▓▌   ▐▓▓▓▓▓▓
+  ▓▓▓▓▓▓▄▄▓▓▓▓▓▓▓▀      ▐▓▓▓▓▓▓▄▄▄▄         ▓▓▓▓▓▓▄▄▄▄         ▐▓▓▓▓▓▌   ▐▓▓▓▓▓▓
+  ▓▓▓▓▓▓▓▓▓▓▓▓▓▀        ▐▓▓▓▓▓▓▓▓▓▓         ▓▓▓▓▓▓▓▓▓▓▌        ▐▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓
+  ▓▓▓▓▓▓▀▀▓▓▓▓▓▓▄       ▐▓▓▓▓▓▓▀▀▀▀         ▓▓▓▓▓▓▀▀▀▀         ▐▓▓▓▓▓▓▓▓▓▓▓▓▓▓▀
+  ▓▓▓▓▓▓   ▀▓▓▓▓▓▓▄     ▐▓▓▓▓▓▓     ▓▓▓▓▓   ▓▓▓▓▓▓     ▓▓▓▓▓   ▐▓▓▓▓▓▌
+▓▓▓▓▓▓▓▓▓▓ █▓▓▓▓▓▓▓▓▓ ▐▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓ ▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓  ▓▓▓▓▓▓▓▓▓▓
+▓▓▓▓▓▓▓▓▓▓ ▓▓▓▓▓▓▓▓▓▓ ▐▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓ ▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓  ▓▓▓▓▓▓▓▓▓▓
+
+                           Trust math, not hardware.
+*/
+
+pragma solidity 0.5.17;
+
+import "./AbstractBonding.sol";
+import "./ETHStaking.sol";
+
+/// @title ETH Bonding
+/// @notice Contract holding deposits from keeps' operators.
+contract ETHBonding is AbstractBonding {
+    ETHStaking ethStaking;
+
+    /// @notice Initializes Keep Bonding contract.
+    /// @param registryAddress Keep registry contract address.
+    /// @param ethStakingAddress ETH Staking contract address.
+    constructor(address registryAddress, address ethStakingAddress)
+        public
+        AbstractBonding(registryAddress)
+    {
+        ethStaking = ETHStaking(ethStakingAddress);
+    }
+
+    /// @notice Withdraws amount from operator's value available for bonding.
+    /// This function can be called only by:
+    /// - operator,
+    /// - owner of the stake.
+    ///
+    /// @param amount Value to withdraw in wei.
+    /// @param operator Address of the operator.
+    function withdraw(uint256 amount, address operator) public {
+        require(
+            msg.sender == operator ||
+                msg.sender == ethStaking.ownerOf(operator),
+            "Only operator or the owner is allowed to withdraw bond"
+        );
+
+        withdrawBond(amount, operator);
+    }
+
+    function isAuthorizedForOperator(
+        address _operator,
+        address _operatorContract
+    ) internal view returns (bool) {
+        return ethStaking.isAuthorizedForOperator(_operator, _operatorContract);
+    }
+
+    function authorizerOf(address _operator) internal view returns (address) {
+        return ethStaking.authorizerOf(_operator);
+    }
+
+    function beneficiaryOf(address _operator)
+        internal
+        view
+        returns (address payable)
+    {
+        return ethStaking.beneficiaryOf(_operator);
+    }
+}
