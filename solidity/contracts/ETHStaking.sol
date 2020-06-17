32ee0d4f4f601b58d6fa81982713754abb475401
Jakub Nowakowski <jakub.nowakowski@keep.network>
2020-06-17 17:14:48 +0200

    ETH staking contract
    The contract is used to delegate stake in ETH to the specific operator
    with authorizer and beneficiary.

---
+++ solidity/contracts/ETHStaking.sol
@@ -0,0 +1,64 @@
+/**
+▓▓▌ ▓▓ ▐▓▓ ▓▓▓▓▓▓▓▓▓▓▌▐▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓ ▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓ ▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▄
+▓▓▓▓▓▓▓▓▓▓ ▓▓▓▓▓▓▓▓▓▓▌▐▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓ ▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓ ▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓
+  ▓▓▓▓▓▓    ▓▓▓▓▓▓▓▀    ▐▓▓▓▓▓▓    ▐▓▓▓▓▓   ▓▓▓▓▓▓     ▓▓▓▓▓   ▐▓▓▓▓▓▌   ▐▓▓▓▓▓▓
+  ▓▓▓▓▓▓▄▄▓▓▓▓▓▓▓▀      ▐▓▓▓▓▓▓▄▄▄▄         ▓▓▓▓▓▓▄▄▄▄         ▐▓▓▓▓▓▌   ▐▓▓▓▓▓▓
+  ▓▓▓▓▓▓▓▓▓▓▓▓▓▀        ▐▓▓▓▓▓▓▓▓▓▓         ▓▓▓▓▓▓▓▓▓▓▌        ▐▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓
+  ▓▓▓▓▓▓▀▀▓▓▓▓▓▓▄       ▐▓▓▓▓▓▓▀▀▀▀         ▓▓▓▓▓▓▀▀▀▀         ▐▓▓▓▓▓▓▓▓▓▓▓▓▓▓▀
+  ▓▓▓▓▓▓   ▀▓▓▓▓▓▓▄     ▐▓▓▓▓▓▓     ▓▓▓▓▓   ▓▓▓▓▓▓     ▓▓▓▓▓   ▐▓▓▓▓▓▌
+▓▓▓▓▓▓▓▓▓▓ █▓▓▓▓▓▓▓▓▓ ▐▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓ ▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓  ▓▓▓▓▓▓▓▓▓▓
+▓▓▓▓▓▓▓▓▓▓ ▓▓▓▓▓▓▓▓▓▓ ▐▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓ ▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓  ▓▓▓▓▓▓▓▓▓▓
+
+                           Trust math, not hardware.
+*/
+
+pragma solidity 0.5.17;
+
+import "@keep-network/keep-core/contracts/KeepRegistry.sol";
+import "@keep-network/keep-core/contracts/Authorizations.sol";
+import "@keep-network/keep-core/contracts/StakeDelegatable.sol";
+
+import "openzeppelin-solidity/contracts/math/SafeMath.sol";
+
+/// @title ETH Staking
+/// @notice A staking contract for ETH staking. An owner of the ETH can delegate
+/// ETH as a stake to an operator. The value of ETH the owner is willing to stake
+/// should be deposited in `ETHBonding` contract for the given operator.
+contract ETHStaking is Authorizations, StakeDelegatable {
+    constructor(KeepRegistry keepRegistry)
+        public
+        Authorizations(keepRegistry)
+    {}
+
+    event Staked(
+        address owner,
+        address indexed operator,
+        address indexed beneficiary,
+        address indexed authorizer
+    );
+
+    /// @notice Registers stake details. The function is used to register
+    /// addresses of operator, beneficiary and authorizer for a stake from the
+    /// caller.
+    /// @param operator Address of the operator.
+    /// @param beneficiary Address of the beneficiary.
+    /// @param authorizer Address of the authorizer.
+    function stake(
+        address operator,
+        address payable beneficiary,
+        address authorizer
+    ) external payable {
+        // TODO: Do we need to verify the caller in any way?
+        address _from = msg.sender;
+
+        require(
+            operators[operator].owner == address(0),
+            "Operator already in use"
+        );
+
+        operators[operator] = Operator(0, _from, beneficiary, authorizer);
+        ownerOperators[_from].push(operator);
+
+        emit Staked(_from, operator, beneficiary, authorizer);
+    }
+}
