c7ad0807781fb4fe9d31ff4f6b4654310ac0efdb
Jakub Nowakowski <jakub.nowakowski@keep.network>
2020-07-10 12:01:56 +0200

    Exposed time type for configuration
    We wat to define time in multiple configurations structs. We moved the
    code to a dedicated package that can be exposed without import loop.

--- pkg/ecdsa/tss/config.go
+++ pkg/ecdsa/tss/config.go
@@ -1,24 +1,11 @@
 package tss
 
 import (
-	"time"
+	configtime "github.com/keep-network/keep-ecdsa/internal/config/time"
 )
 
 // Config contains configuration for tss protocol execution.
 type Config struct {
 	// Timeout for pre-parameters generation in tss-lib.
-	PreParamsGenerationTimeout duration
-}
-
-// We use BurntSushi/toml package to parse configuration file. Unfortunately it
-// doesn't support time.Duration out of the box. Here we introduce a workaround
-// to be able to parse values provided in more friendly way, e.g. "4m20s".
-type duration struct {
-	time.Duration
-}
-
-func (d *duration) UnmarshalText(text []byte) error {
-	var err error
-	d.Duration, err = time.ParseDuration(string(text))
-	return err
+	PreParamsGenerationTimeout configtime.Duration
 }
