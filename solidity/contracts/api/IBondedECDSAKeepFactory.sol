4d3e310b495b540e9006f30675dbbff8ab5e1d86
Piotr Dyraga <piotr.dyraga@keep.network>
2020-07-29 17:03:26 +0200

    Calculate the required bondable value per member
    Instead of expecting the application (sortition pool owner) to calculate
    the minimum unbonded value and perform all the neccessary rounding, we
    now calculate everything in BondedECDSAKeepFactory based on the provided
    bond required from a keep and the number of signers in that keep.

--- solidity/contracts/api/IBondedECDSAKeepFactory.sol
+++ solidity/contracts/api/IBondedECDSAKeepFactory.sol
@@ -52,7 +52,11 @@ interface IBondedECDSAKeepFactory {
     /// bondable value.
     /// @dev The default minimum bond value for each sortition pool created
     /// is 20 ETH.
-    /// @param _minimumBondableValue The minimum unbonded value allowing
-    /// an operator to join and stay in the sortition pool for the application.
-    function setMinimumBondableValue(uint256 _minimumBondableValue) external;
+    /// @param _minimumBondableValue The minimum unbonded value the application
+    /// requires from a single keep.
+    /// @param _groupSize Number of singers in the keep.
+    function setMinimumBondableValue(
+        uint256 _minimumBondableValue,
+        uint256 _groupSize
+    ) external;
 }
