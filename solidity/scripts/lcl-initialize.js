c722eb0bdb180a564098fc31626b56af222bc56b
Lukasz Zimnoch <lukasz.zimnoch@keep.network>
2020-08-06 13:41:45 +0200

    Fix linter errors

--- solidity/scripts/lcl-initialize.js
+++ solidity/scripts/lcl-initialize.js
@@ -86,8 +86,8 @@ module.exports = async function () {
     }
 
     // FIXME: Workaround for https://github.com/ethereum/go-ethereum/pull/21083 - instead of failing,
-    // contract call returns address 0x0. Once we are able to update to the most recent abigen 
-    // (see https://github.com/keep-network/keep-common/issues/45), the additional case for 0x0 address 
+    // contract call returns address 0x0. Once we are able to update to the most recent abigen
+    // (see https://github.com/keep-network/keep-common/issues/45), the additional case for 0x0 address
     // should be removed.
     if (
       !sortitionPoolAddress ||
