40ed242e5d6e4586cad134c9d98f17335e3e0431
Jakub Nowakowski <jakub.nowakowski@keep.network>
2020-08-12 13:37:20 +0200

    KeepFactoryBeaconConsumer -> KeepFactorySeeder

--- solidity/contracts/KeepFactorySeeder.sol
+++ solidity/contracts/KeepFactorySeeder.sol
@@ -18,7 +18,7 @@ import "@keep-network/keep-core/contracts/IRandomBeacon.sol";
 
 import "openzeppelin-solidity/contracts/math/SafeMath.sol";
 
-contract KeepFactoryBeaconConsumer is IRandomBeaconConsumer {
+contract KeepFactorySeeder is IRandomBeaconConsumer {
     using SafeMath for uint256;
 
     IRandomBeacon randomBeacon;
