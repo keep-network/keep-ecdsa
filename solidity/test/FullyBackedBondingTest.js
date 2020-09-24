20c375e8750fbee905a63b16e1096c6566bdb2f9
Jakub Nowakowski <jakub.nowakowski@keep.network>
2020-09-24 12:24:45 +0200

    Renamed EthBondingTest.js

--- solidity/test/FullyBackedBondingTest.js
+++ solidity/test/FullyBackedBondingTest.js
@@ -2,7 +2,7 @@ const {accounts, contract, web3} = require("@openzeppelin/test-environment")
 const {createSnapshot, restoreSnapshot} = require("./helpers/snapshot")
 
 const KeepRegistry = contract.fromArtifact("KeepRegistry")
-const EthBonding = contract.fromArtifact("EthBonding")
+const FullyBackedBonding = contract.fromArtifact("FullyBackedBonding")
 const TestEtherReceiver = contract.fromArtifact("TestEtherReceiver")
 
 const {expectEvent, expectRevert, time} = require("@openzeppelin/test-helpers")
@@ -14,13 +14,13 @@ chai.use(require("bn-chai")(BN))
 const expect = chai.expect
 const assert = chai.assert
 
-describe("EthBonding", function () {
+describe("FullyBackedBonding", function () {
   const initializationPeriod = new BN(60)
 
   let minimumDelegationValue
 
   let registry
-  let ethBonding
+  let bonding
   let etherReceiver
 
   let operator
@@ -41,10 +41,13 @@ describe("EthBonding", function () {
     thirdParty = accounts[7]
 
     registry = await KeepRegistry.new()
-    ethBonding = await EthBonding.new(registry.address, initializationPeriod)
+    bonding = await FullyBackedBonding.new(
+      registry.address,
+      initializationPeriod
+    )
     etherReceiver = await TestEtherReceiver.new()
 
-    minimumDelegationValue = await ethBonding.MINIMUM_DELEGATION_DEPOSIT()
+    minimumDelegationValue = await bonding.MINIMUM_DELEGATION_DEPOSIT()
 
     await registry.approveOperatorContract(bondCreator)
   })
@@ -59,37 +62,37 @@ describe("EthBonding", function () {
 
   describe("delegate", async () => {
     it("registers delegation", async () => {
-      await ethBonding.delegate(operator, beneficiary, authorizer, {
+      await bonding.delegate(operator, beneficiary, authorizer, {
         from: owner,
         value: minimumDelegationValue,
       })
 
       assert.equal(
-        await ethBonding.ownerOf(operator),
+        await bonding.ownerOf(operator),
         owner,
         "incorrect owner address"
       )
 
       assert.equal(
-        await ethBonding.beneficiaryOf(operator),
+        await bonding.beneficiaryOf(operator),
         beneficiary,
         "incorrect beneficiary address"
       )
 
       assert.equal(
-        await ethBonding.authorizerOf(operator),
+        await bonding.authorizerOf(operator),
         authorizer,
         "incorrect authorizer address"
       )
 
-      expect(await ethBonding.balanceOf(operator)).to.eq.BN(
+      expect(await bonding.balanceOf(operator)).to.eq.BN(
         0,
         "incorrect delegation balance"
       )
     })
 
     it("emits events", async () => {
-      const receipt = await ethBonding.delegate(
+      const receipt = await bonding.delegate(
         operator,
         beneficiary,
         authorizer,
@@ -114,26 +117,26 @@ describe("EthBonding", function () {
     it("deposits passed value as unbonded value", async () => {
       const value = minimumDelegationValue
 
-      await ethBonding.delegate(operator, beneficiary, authorizer, {
+      await bonding.delegate(operator, beneficiary, authorizer, {
         from: owner,
         value: value,
       })
 
-      expect(await ethBonding.unbondedValue(operator)).to.eq.BN(
+      expect(await bonding.unbondedValue(operator)).to.eq.BN(
         value,
         "invalid unbonded value"
       )
 
-      await ethBonding.authorizeOperatorContract(operator, bondCreator, {
+      await bonding.authorizeOperatorContract(operator, bondCreator, {
         from: authorizer,
       })
 
-      await ethBonding.authorizeSortitionPoolContract(operator, sortitionPool, {
+      await bonding.authorizeSortitionPoolContract(operator, sortitionPool, {
         from: authorizer,
       })
 
       expect(
-        await ethBonding.availableUnbondedValue(
+        await bonding.availableUnbondedValue(
           operator,
           bondCreator,
           sortitionPool
@@ -145,7 +148,7 @@ describe("EthBonding", function () {
       const value = minimumDelegationValue.subn(1)
 
       await expectRevert(
-        ethBonding.delegate(operator, beneficiary, authorizer, {
+        bonding.delegate(operator, beneficiary, authorizer, {
           from: owner,
           value: value,
         }),
@@ -156,25 +159,25 @@ describe("EthBonding", function () {
     it("allows multiple operators for the same owner", async () => {
       const operator2 = accounts[5]
 
-      await ethBonding.delegate(operator, beneficiary, authorizer, {
+      await bonding.delegate(operator, beneficiary, authorizer, {
         from: owner,
         value: minimumDelegationValue,
       })
 
-      await ethBonding.delegate(operator2, beneficiary, authorizer, {
+      await bonding.delegate(operator2, beneficiary, authorizer, {
         from: owner,
         value: minimumDelegationValue,
       })
     })
 
     it("reverts if operator is already in use", async () => {
-      await ethBonding.delegate(operator, beneficiary, authorizer, {
+      await bonding.delegate(operator, beneficiary, authorizer, {
         from: owner,
         value: minimumDelegationValue,
       })
 
       await expectRevert(
-        ethBonding.delegate(operator, accounts[5], accounts[5]),
+        bonding.delegate(operator, accounts[5], accounts[5]),
         "Operator already in use"
       )
     })
@@ -186,35 +189,35 @@ describe("EthBonding", function () {
       const value = new BN(123)
       const expectedFinalBalance = initialDeposit.add(value)
 
-      await ethBonding.delegate(operator, beneficiary, authorizer, {
+      await bonding.delegate(operator, beneficiary, authorizer, {
         from: owner,
         value: initialDeposit,
       })
 
-      expect(await ethBonding.unbondedValue(operator)).to.eq.BN(
+      expect(await bonding.unbondedValue(operator)).to.eq.BN(
         initialDeposit,
         "invalid initial unbonded value"
       )
 
-      await ethBonding.deposit(operator, {
+      await bonding.deposit(operator, {
         value: value,
       })
 
-      expect(await ethBonding.unbondedValue(operator)).to.eq.BN(
+      expect(await bonding.unbondedValue(operator)).to.eq.BN(
         expectedFinalBalance,
         "invalid final unbonded value"
       )
 
-      await ethBonding.authorizeOperatorContract(operator, bondCreator, {
+      await bonding.authorizeOperatorContract(operator, bondCreator, {
         from: authorizer,
       })
 
-      await ethBonding.authorizeSortitionPoolContract(operator, sortitionPool, {
+      await bonding.authorizeSortitionPoolContract(operator, sortitionPool, {
         from: authorizer,
       })
 
       expect(
-        await ethBonding.availableUnbondedValue(
+        await bonding.availableUnbondedValue(
           operator,
           bondCreator,
           sortitionPool
@@ -230,36 +233,36 @@ describe("EthBonding", function () {
     beforeEach(async () => {
       initialDeposit = minimumDelegationValue
 
-      await ethBonding.delegate(operator, beneficiary, authorizer, {
+      await bonding.delegate(operator, beneficiary, authorizer, {
         from: owner,
         value: initialDeposit,
       })
 
-      await ethBonding.authorizeOperatorContract(operator, bondCreator, {
+      await bonding.authorizeOperatorContract(operator, bondCreator, {
         from: authorizer,
       })
     })
 
     it("can be called by operator", async () => {
-      await ethBonding.withdraw(value, operator, {from: operator})
+      await bonding.withdraw(value, operator, {from: operator})
       // ok, no reverts
     })
 
     it("can be called by delegation owner", async () => {
-      await ethBonding.withdraw(value, operator, {from: owner})
+      await bonding.withdraw(value, operator, {from: owner})
       // ok, no reverts
     })
 
     it("cannot be called by authorizer", async () => {
       await expectRevert(
-        ethBonding.withdraw(value, operator, {from: authorizer}),
+        bonding.withdraw(value, operator, {from: authorizer}),
         "Only operator or the owner is allowed to withdraw bond"
       )
     })
 
     it("cannot be called by beneficiary", async () => {
       await expectRevert(
-        ethBonding.withdraw(value, operator, {from: beneficiary}),
+        bonding.withdraw(value, operator, {from: beneficiary}),
         "Only operator or the owner is allowed to withdraw bond"
       )
     })
@@ -268,7 +271,7 @@ describe("EthBonding", function () {
       const thirdParty = accounts[7]
 
       await expectRevert(
-        ethBonding.withdraw(value, operator, {from: thirdParty}),
+        bonding.withdraw(value, operator, {from: thirdParty}),
         "Only operator or the owner is allowed to withdraw bond"
       )
     })
@@ -280,14 +283,14 @@ describe("EthBonding", function () {
         .toBN(await web3.eth.getBalance(beneficiary))
         .add(value)
 
-      expect(await ethBonding.unbondedValue(operator)).to.eq.BN(
+      expect(await bonding.unbondedValue(operator)).to.eq.BN(
         initialDeposit,
         "invalid unbonded value"
       )
 
-      await ethBonding.withdraw(value, operator, {from: operator})
+      await bonding.withdraw(value, operator, {from: operator})
 
-      expect(await ethBonding.unbondedValue(operator)).to.eq.BN(
+      expect(await bonding.unbondedValue(operator)).to.eq.BN(
         expectedUnbonded,
         "invalid unbonded value"
       )
@@ -302,7 +305,7 @@ describe("EthBonding", function () {
     it("emits event", async () => {
       const value = new BN(90)
 
-      const receipt = await ethBonding.withdraw(value, operator, {
+      const receipt = await bonding.withdraw(value, operator, {
         from: operator,
       })
       expectEvent(receipt, "UnbondedValueWithdrawn", {
@@ -316,7 +319,7 @@ describe("EthBonding", function () {
       const invalidValue = initialDeposit.addn(1)
 
       await expectRevert(
-        ethBonding.withdraw(invalidValue, operator, {from: operator}),
+        bonding.withdraw(invalidValue, operator, {from: operator}),
         "Insufficient unbonded value"
       )
     })
@@ -326,13 +329,13 @@ describe("EthBonding", function () {
 
       await etherReceiver.setShouldFail(true)
 
-      await ethBonding.delegate(operator2, etherReceiver.address, authorizer, {
+      await bonding.delegate(operator2, etherReceiver.address, authorizer, {
         from: owner,
         value: initialDeposit,
       })
 
       await expectRevert(
-        ethBonding.withdraw(value, operator2, {from: operator2}),
+        bonding.withdraw(value, operator2, {from: operator2}),
         "Transfer failed"
       )
     })
@@ -340,79 +343,79 @@ describe("EthBonding", function () {
 
   describe("isInitialized", async () => {
     it("returns true when authorized and initialization period passed", async () => {
-      await ethBonding.delegate(operator, beneficiary, authorizer, {
+      await bonding.delegate(operator, beneficiary, authorizer, {
         from: owner,
         value: minimumDelegationValue,
       })
 
-      await ethBonding.authorizeOperatorContract(operator, bondCreator, {
+      await bonding.authorizeOperatorContract(operator, bondCreator, {
         from: authorizer,
       })
 
       await time.increase(initializationPeriod.addn(1))
 
-      assert.isTrue(await ethBonding.isInitialized(operator, bondCreator))
+      assert.isTrue(await bonding.isInitialized(operator, bondCreator))
     })
 
     it("returns false when authorized but initialization period not passed yet", async () => {
-      await ethBonding.delegate(operator, beneficiary, authorizer, {
+      await bonding.delegate(operator, beneficiary, authorizer, {
         from: owner,
         value: minimumDelegationValue,
       })
 
-      await ethBonding.authorizeOperatorContract(operator, bondCreator, {
+      await bonding.authorizeOperatorContract(operator, bondCreator, {
         from: authorizer,
       })
 
       await time.increase(initializationPeriod)
 
-      assert.isFalse(await ethBonding.isInitialized(operator, bondCreator))
+      assert.isFalse(await bonding.isInitialized(operator, bondCreator))
     })
 
     it("returns false when initialization period passed but not authorized", async () => {
-      await ethBonding.delegate(operator, beneficiary, authorizer, {
+      await bonding.delegate(operator, beneficiary, authorizer, {
         from: owner,
         value: minimumDelegationValue,
       })
 
       await time.increase(initializationPeriod.addn(1))
 
-      assert.isFalse(await ethBonding.isInitialized(operator, bondCreator))
+      assert.isFalse(await bonding.isInitialized(operator, bondCreator))
     })
 
     it("returns false when not authorized and initialization period not passed", async () => {
-      await ethBonding.delegate(operator, beneficiary, authorizer, {
+      await bonding.delegate(operator, beneficiary, authorizer, {
         from: owner,
         value: minimumDelegationValue,
       })
 
-      assert.isFalse(await ethBonding.isInitialized(operator, bondCreator))
+      assert.isFalse(await bonding.isInitialized(operator, bondCreator))
     })
 
     it("returns false when initialization period passed but other contract authorized", async () => {
-      await ethBonding.delegate(operator, beneficiary, authorizer, {
+      await bonding.delegate(operator, beneficiary, authorizer, {
         from: owner,
         value: minimumDelegationValue,
       })
 
       await registry.approveOperatorContract(thirdParty)
-      await ethBonding.authorizeOperatorContract(operator, thirdParty, {
+      await bonding.authorizeOperatorContract(operator, thirdParty, {
         from: authorizer,
       })
 
       await time.increase(initializationPeriod.addn(1))
 
-      assert.isFalse(await ethBonding.isInitialized(operator, bondCreator))
+      assert.isFalse(await bonding.isInitialized(operator, bondCreator))
     })
 
     describe("getDelegationInfo", async () => {
       it("returns delegation details", async () => {
-        await ethBonding.delegate(operator, beneficiary, authorizer, {
+        await bonding.delegate(operator, beneficiary, authorizer, {
           from: owner,
           value: minimumDelegationValue,
         })
 
-        const delegationInfo = await ethBonding.getDelegationInfo(operator)
+        const delegationInfo = await bonding.getDelegationInfo(operator)
         expect(delegationInfo.createdAt).to.eq.BN(await time.latest())
         expect(delegationInfo.undelegatedAt).to.eq.BN(0)
       })
