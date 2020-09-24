04c8d25087b7cf30d5f7ce7c3800741b548041f5
Jakub Nowakowski <jakub.nowakowski@keep.network>
2020-09-24 12:13:40 +0200

    Initial version of FullyBackedBondedECDSAKeep
    The contract is a Bonded ECDSA Keep that is backed only by ETH. There is
    no requirement for operators to stake any KEEP tokens.

---
+++ solidity/contracts/fully-backed/FullyBackedBondedECDSAKeep.sol
@@ -0,0 +1,69 @@
+/**
+▓▓▌ ▓▓ ▐▓▓ ▓▓▓▓▓▓▓▓▓▓▌▐▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓ ▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓ ▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▄
+▓▓▓▓▓▓▓▓▓▓ ▓▓▓▓▓▓▓▓▓▓▌▐▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓ ▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓ ▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓
+  ▓▓▓▓▓▓    ▓▓▓▓▓▓▓▀    ▐▓▓▓▓▓▓    ▐▓▓▓▓▓   ▓▓▓▓▓▓     ▓▓▓▓▓   ▐▓▓▓▓▓▌   ▐▓▓▓▓▓▓
+  ▓▓▓▓▓▓▄▄▓▓▓▓▓▓▓▀      ▐▓▓▓▓▓▓▄▄▄▄         ▓▓▓▓▓▓▄▄▄▄         ▐▓▓▓▓▓▌   ▐▓▓▓▓▓▓
+  ▓▓▓▓▓▓▓▓▓▓▓▓▓▀        ▐▓▓▓▓▓▓▓▓▓▓         ▓▓▓▓▓▓▓▓▓▓▌        ▐▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓
+  ▓▓▓▓▓▓▀▀▓▓▓▓▓▓▄       ▐▓▓▓▓▓▓▀▀▀▀         ▓▓▓▓▓▓▀▀▀▀         ▐▓▓▓▓▓▓▓▓▓▓▓▓▓▓▀
+  ▓▓▓▓▓▓   ▀▓▓▓▓▓▓▄     ▐▓▓▓▓▓▓     ▓▓▓▓▓   ▓▓▓▓▓▓     ▓▓▓▓▓   ▐▓▓▓▓▓▌
+▓▓▓▓▓▓▓▓▓▓ █▓▓▓▓▓▓▓▓▓ ▐▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓ ▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓  ▓▓▓▓▓▓▓▓▓▓
+▓▓▓▓▓▓▓▓▓▓ ▓▓▓▓▓▓▓▓▓▓ ▐▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓ ▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓  ▓▓▓▓▓▓▓▓▓▓
+
+                           Trust math, not hardware.
+*/
+
+pragma solidity 0.5.17;
+
+import "../AbstractBondedECDSAKeep.sol";
+import "./FullyBackedBonding.sol";
+import "./FullyBackedBondedECDSAKeepFactory.sol";
+
+/// @title Fully Backed Bonded ECDSA Keep
+/// @notice ECDSA keep with additional signer bond requirement that is fully backed
+/// by ETH only.
+/// @dev This contract is used as a master contract for clone factory in
+/// BondedECDSAKeepFactory as per EIP-1167. It should never be removed after
+/// initial deployment as this will break functionality for all created clones.
+contract FullyBackedBondedECDSAKeep is AbstractBondedECDSAKeep {
+    FullyBackedBonding bonding;
+    FullyBackedBondedECDSAKeepFactory keepFactory;
+
+    /// @notice Initialization function.
+    /// @dev We use clone factory to create new keep. That is why this contract
+    /// doesn't have a constructor. We provide keep parameters for each instance
+    /// function after cloning instances from the master contract.
+    /// @param _owner Address of the keep owner.
+    /// @param _members Addresses of the keep members.
+    /// @param _honestThreshold Minimum number of honest keep members.
+    /// @param _bonding Address of the Bonding contract.
+    /// @param _keepFactory Address of the BondedECDSAKeepFactory that created
+    /// this keep.
+    function initialize(
+        address _owner,
+        address[] memory _members,
+        uint256 _honestThreshold,
+        address _bonding,
+        address payable _keepFactory
+    ) public {
+        super.initialize(_owner, _members, _honestThreshold, _bonding);
+
+        bonding = FullyBackedBonding(_bonding);
+        keepFactory = FullyBackedBondedECDSAKeepFactory(_keepFactory);
+    }
+
+    function slashForSignatureFraud() internal {
+        // TODO: We don't need to do anything as keep owner is able to seize the bonds
+        // TODO: Ban members (remove from sortition pool and don't let to rejoin)
+    }
+
+    /// @notice Gets the beneficiary for the specified member address.
+    /// @param _member Member address.
+    /// @return Beneficiary address.
+    function beneficiaryOf(address _member)
+        internal
+        view
+        returns (address payable)
+    {
+        return bonding.beneficiaryOf(_member);
+    }
+}
