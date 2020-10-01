4296f4ed7d6917c8844606ec9912c83cfd9a45e8
Dmitry <dymitr.paremski@keep.network>
2020-10-01 16:03:27 +0200

    Adding functions to ecdsa backport rewards

--- solidity/contracts/ECDSABackportRewards.sol
+++ solidity/contracts/ECDSABackportRewards.sol
@@ -16,6 +16,7 @@ pragma solidity 0.5.17;
 
 import "@keep-network/keep-core/contracts/Rewards.sol";
 import "./api/IBondedECDSAKeepFactory.sol";
+import "./api/IBondedECDSAKeep.sol";
 
 contract ECDSABackportRewards is Rewards {
 
@@ -58,4 +59,60 @@ contract ECDSABackportRewards is Rewards {
         factory = IBondedECDSAKeepFactory(_factoryAddress);
     }
 
+    /// @notice Sends the reward for a keep to the group signing member beneficiaries.
+    /// @param groupIndex Index of the keep to receive a reward.
+    function receiveReward(uint256 groupIndex) public {
+        receiveReward(bytes32(groupIndex));
+    }
+
+    function _getKeepCount() internal view returns (uint256) {
+        // Between May 17 2020 - Sep 14 2020 there were 41 keeps opened.
+        return numberOfCreatedECDSAKeeps;
+    }
+
+    function _getKeepAtIndex(uint256 i) internal view returns (bytes32) {
+        return bytes32(i);
+    }
+
+    function _getCreationTime(bytes32) internal view returns (uint256) {
+        // Assign each keep to the starting timestamp of its interval.
+        return startOf(0);
+    }
+
+    function _isClosed(bytes32) internal view returns (bool) {
+        // All keeps created between May 17 2020 - Sep 14 2020 are considered closed.
+        return true;
+    }
+
+    function _isTerminated(bytes32 groupIndexBytes) internal view returns (bool) {
+        return false;
+    }
+
+    // A keep is recognized if its index is at most `last eligible keep`.
+    function _recognizedByFactory(bytes32 groupIndexBytes) internal view returns (bool) {
+        return numberOfCreatedECDSAKeeps > uint256(groupIndexBytes);
+    }
+
+    function _distributeReward(bytes32 _keep, uint256 amount) internal isAddress(_keep) {
+        token.approve(toAddress(_keep), amount);
+
+        IBondedECDSAKeep(toAddress(_keep)).distributeERC20Reward(address(token), amount);
+    }
+
+    function validAddressBytes(bytes32 keepBytes) internal pure returns (bool) {
+        return fromAddress(toAddress(keepBytes)) == keepBytes;
+    }
+
+    function toAddress(bytes32 keepBytes) internal pure returns (address) {
+        return address(bytes20(keepBytes));
+    }
+
+    function fromAddress(address keepAddress) internal pure returns (bytes32) {
+        return bytes32(bytes20(keepAddress));
+    }
+
+    modifier isAddress(bytes32 _keep) {
+        require(validAddressBytes(_keep), "Invalid keep address");
+        _;
+    }
 }
