ddf63abf235f1642b38bcc8d68ed673d8b16624f
Dmitry <dymitr.paremski@keep.network>
2020-10-06 10:47:04 +0200

    Adding a comment why all the keeps are considred closed

--- solidity/contracts/ECDSABackportRewards.sol
+++ solidity/contracts/ECDSABackportRewards.sol
@@ -32,14 +32,7 @@ contract ECDSABackportRewards is Rewards {
     // Newer: https://etherscan.io/address/0xA7d9E842EFB252389d613dA88EDa3731512e40bD
     uint256 internal constant backportECDSATermLength = 123 days; // 10678946 sec
 
-    // There were 41 Keeps created by BondedECDSAKeepFactory : 0x18758f16988E61Cd4B61E6B930694BD9fB07C22F
-    // The last Keep was opened on May-17-2020
-    // https://etherscan.io/address/0x45A3cACA2F2a78A53607618651C86111c9720AA5
-    uint256 internal constant numberOfCreatedECDSAKeeps = 41;
-
-    // We allocate ecdsa backport rewards to all 41 keeps.
-    uint256
-        internal constant minimumECDSAKeepsPerInterval = numberOfCreatedECDSAKeeps;
+    uint256 internal constant minimumECDSAKeepsPerInterval = 40;
 
     BondedECDSAKeepFactory factory;
 
@@ -58,7 +51,9 @@ contract ECDSABackportRewards is Rewards {
 
     function _getKeepCount() internal view returns (uint256) {
         // Between May 13 2020 - Sep 14 2020 there were 41 keeps opened.
-        return numberOfCreatedECDSAKeeps;
+        // The last Keep was opened on May-17-2020
+        // https://etherscan.io/address/0x45A3cACA2F2a78A53607618651C86111c9720AA5
+        return factory.getKeepCount();
     }
 
     function _getKeepAtIndex(uint256 i) internal view returns (bytes32) {
@@ -71,7 +66,11 @@ contract ECDSABackportRewards is Rewards {
     }
 
     function _isClosed(bytes32) internal view returns (bool) {
-        // All keeps created between May 13 2020 - Sep 14 2020 are considered closed.
+        // Even though we still have some of the keeps opened, all the keeps
+        // created between May 13 2020 - Sep 14 2020 are considered closed. Because
+        // of the deposits pause https://tbtc.network/news/2020-05-21-details-of-the-tbtc-deposit-pause-on-may-18-2020/
+        // closing all the keeps is not easily achievable. However, we do not want
+        // to block rewards distribution for good stakers caused by the incident on May 18th.
         return true;
     }
 
