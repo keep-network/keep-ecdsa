06ca54f3ed7c208422d28bb98cea69be9850c8de
Jakub Nowakowski <jakub.nowakowski@keep.network>
2020-11-19 12:52:02 +0100

    Introduce common Ethereum signature JSON format
    We expect a signature in a common Ethereum signature JSON format:
    {
      "address": "<address>",
      "msg": "<content>",
      "sig": "<signature>",
      "version": "2"
    }
    
    Added a possibility to output the signature to a file and read a file on
    verification.

--- cmd/signing_ethereum.go
+++ cmd/signing_ethereum.go
@@ -4,7 +4,9 @@ import (
 	"bytes"
 	"crypto/sha256"
 	"encoding/hex"
+	"encoding/json"
 	"fmt"
+	"io/ioutil"
 	"os"
 
 	"github.com/ethereum/go-ethereum/common"
@@ -22,7 +24,7 @@ var EthereumSigningCommand = cli.Command{
 	Subcommands: []cli.Command{
 		{
 			Name:        "sign",
-			Usage:       "Sign a message using operator's key",
+			Usage:       "Sign a message using the operator's key",
 			Description: ethereumSignDescription,
 			Action:      EthereumSign,
 			ArgsUsage:   "[message]",
@@ -32,6 +34,10 @@ var EthereumSigningCommand = cli.Command{
 					Usage: "Path to the ethereum key file. " +
 						"If not provided read the path from a config file.",
 				},
+				cli.StringFlag{
+					Name:  "output-file,o",
+					Usage: "Output file for the signature",
+				},
 			},
 		},
 		{
@@ -39,26 +45,62 @@ var EthereumSigningCommand = cli.Command{
 			Usage:       "Verifies a signature",
 			Description: ethereumVerifyDescription,
 			Action:      EthereumVerify,
-			ArgsUsage:   "[message] [address] [signature]",
+			ArgsUsage:   "[ethereum-signature]",
+			Flags: []cli.Flag{
+				cli.StringFlag{
+					Name:  "input-file,i",
+					Usage: "Input file with the signature",
+				},
+			},
 		},
 	},
 }
 
-const ethereumSignDescription = "Calculates a signature for a given message. " +
-	"The message is expected to be provided as a string, it is later hashed with " +
-	"SHA-256 and passed to ECDSA signing. The calculated signature is returned " +
-	"in Ethereum specific format as a hexadecimal string representation of 65-byte " +
-	"{R, S, V} parameters.\n" +
-	"It reads an Ethereum key from an encrypted file. A path to the key file can be " +
-	"configured in a config file or specified directly with a `eth-key-file` flag. " +
-	"The key file is expected to be encrypted with a password provided as " +
-	config.PasswordEnvVariable + " environment variable."
+const ethereumSignDescription = `Calculates an ethereum signature for a given message.
+The message is expected to be provided as a string, it is later hashed with SHA-256
+and passed to Ethereum ECDSA signing. Signature is calculated in Ethereum specific
+format as a hexadecimal string representation of 65-byte {R, S, V} parameters.
 
-const ethereumVerifyDescription = "Verifies if a signature was calculated for a " +
-	"message by an ethereum account identified by an address. It expects a message " +
-	"to be provided as an original message string, that is later hashed with SHA-256. " +
-	"Signature should be provided in Ethereum specific format as a hexadecimal string " +
-	"representation of 65-byte {R, S, V} parameters."
+It requires an Ethereum key to be provided in an encrypted file. A path to the key file
+can be configured in a config file or specified directly with an 'eth-key-file' flag.
+
+The key file is expected to be encrypted with a password provided as ` + config.PasswordEnvVariable + `
+environment variable.
+	
+The result is outputted in a common Ethereum signature format:
+{
+	"address": "<address>",
+	"msg": "<content>",
+	"sig": "<signature>",
+	"version": "2"
+}
+
+If 'output-file' flag is set the result will be stored in a specified file path.
+`
+
+const ethereumVerifyDescription = `Verifies if a signature was calculated for a message 
+by an ethereum account identified by an address. 
+
+It expects a signature to be provided in a common Ethereum signature format:
+{
+	"address": "<address>",
+	"msg": "<content>",
+	"sig": "<signature>",
+	"version": "2"
+}
+
+If 'input-file' flag is set the input will be read from a specified file path.
+`
+
+// EthereumSignature is a common Ethereum signature format.
+type EthereumSignature struct {
+	Address   common.Address `json:"address"`
+	Message   string         `json:"msg"`
+	Signature string         `json:"sig"`
+	Version   uint           `json:"version"`
+}
+
+const ethSignatureVersion uint = 2
 
 // EthereumSign signs a string using operator's ethereum key.
 func EthereumSign(c *cli.Context) error {
@@ -98,62 +140,83 @@ func EthereumSign(c *cli.Context) error {
 		return fmt.Errorf("failed to sign: [%v]", err)
 	}
 
-	signatureHex := hex.EncodeToString(signature)
+	ethereumSignature := &EthereumSignature{
+		Address:   ethereumKey.Address,
+		Message:   message,
+		Signature: hex.EncodeToString(signature),
+		Version:   ethSignatureVersion,
+	}
 
-	fmt.Printf(
-		"signature calculated by [%s] for message [%s]: %s\n",
-		ethereumKey.Address.Hex(), message, signatureHex,
-	)
+	marshaledSignature, err := json.Marshal(ethereumSignature)
+	if err != nil {
+		return fmt.Errorf("failed to marshal ethereum signature: [%v]", err)
+	}
 
-	return nil
+	return outputData(c, marshaledSignature)
 }
 
 // EthereumVerify verifies if a signature was calculated by a signer with the
 // given ethereum address.
 func EthereumVerify(c *cli.Context) error {
-	message := c.Args().First()
-	if len(message) == 0 {
-		return fmt.Errorf("invalid message")
+	var marshaledSignature []byte
+	if inputFilePath := c.String("input-file"); len(inputFilePath) > 0 {
+		fileContent, err := ioutil.ReadFile(inputFilePath)
+		if err != nil {
+			return fmt.Errorf("failed to read a file: [%v]", err)
+		}
+		marshaledSignature = fileContent
+	} else {
+		signatureArg := c.Args().First()
+		if len(signatureArg) == 0 {
+			return fmt.Errorf("missing argument")
+		}
+
+		marshaledSignature = []byte(signatureArg)
 	}
 
-	address := c.Args().Get(1)
-	if len(address) == 0 {
-		return fmt.Errorf("invalid address")
-	}
-
-	signature := c.Args().Get(2)
-	if len(signature) == 0 {
-		return fmt.Errorf("invalid signature")
-	}
-
-	signatureBytes, err := hex.DecodeString(signature)
+	ethereumSignature := &EthereumSignature{}
+	err := json.Unmarshal(marshaledSignature, ethereumSignature)
 	if err != nil {
-		return fmt.Errorf("could not decode signature string: [%v]", err)
+		return fmt.Errorf("failed to unmarshal ethereum signature: [%v]", err)
 	}
 
-	digest := sha256.Sum256([]byte(message))
+	if ethereumSignature.Version != ethSignatureVersion {
+		return fmt.Errorf(
+			"unsupported ethereum signature version\n"+
+				"\texpected: %d\n"+
+				"\tactual:   %d",
+			ethSignatureVersion,
+			ethereumSignature.Version,
+		)
+	}
+
+	digest := sha256.Sum256([]byte(ethereumSignature.Message))
+
+	signatureBytes, err := hex.DecodeString(ethereumSignature.Signature)
+	if err != nil {
+		return fmt.Errorf("failed to decode signature: [%v]", err)
+	}
 
 	publicKey, err := crypto.SigToPub(digest[:], signatureBytes)
 	if err != nil {
 		return fmt.Errorf("could not recover public key from signature [%v]", err)
 	}
 
-	expectedAddress := common.HexToAddress(address)
 	recoveredAddress := crypto.PubkeyToAddress(*publicKey)
 
-	if !bytes.Equal(recoveredAddress.Bytes(), expectedAddress.Bytes()) {
+	if !bytes.Equal(recoveredAddress.Bytes(), ethereumSignature.Address.Bytes()) {
 		return fmt.Errorf(
-			"invalid signer\n"+
-				"\texpected signer: %s\n"+
-				"\tactual signer:   %s",
-			expectedAddress.Hex(),
+			"signature verification failed: invalid signer\n"+
+				"\texpected: %s\n"+
+				"\tactual:   %s",
+			ethereumSignature.Address.Hex(),
 			recoveredAddress.Hex(),
 		)
 	}
 
 	fmt.Printf(
 		"signature verified correctly, message [%s] was signed by [%s]\n",
-		message,
+		ethereumSignature.Message,
 		recoveredAddress.Hex(),
 	)
 
