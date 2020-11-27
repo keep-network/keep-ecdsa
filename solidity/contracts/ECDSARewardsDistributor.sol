d361c5a76bfdaf97d13ab2e86e87cdabf6a23882
Dmitry <dymitr.paremski@keep.network>
2020-11-27 17:08:23 +0100

    Improving docs, error messages and renames

--- solidity/contracts/ECDSARewardsDistributor.sol
+++ solidity/contracts/ECDSARewardsDistributor.sol
@@ -20,6 +20,17 @@ import "openzeppelin-solidity/contracts/token/ERC20/SafeERC20.sol";
 import "@keep-network/keep-core/contracts/KeepToken.sol";
 import "@openzeppelin/contracts/cryptography/MerkleProof.sol";
 
+/// @title ECDSA Rewards distributor
+/// @notice This contract is based on the Uniswap's Merkle Distributor
+/// https://github.com/Uniswap/merkle-distributor with some modifications:
+/// - added a map of merkle root keys with the amount of KEEP (value) that will
+///   be allocated for those merkle roots
+/// - added receiveApproval() function that will be called each time to allocate
+///   new KEEP rewards for a given merkle root. Merkle root is going to be generated
+///   regulary (ex. every week) and it is also means that an interval for that
+///   merkle root has passed
+/// - changed code accordingly to process claimed rewards using a map of merkle
+///   roots
 contract ECDSARewardsDistributor is Ownable {
     using SafeERC20 for KeepToken;
     using BytesLib for bytes;
@@ -27,22 +38,24 @@ contract ECDSARewardsDistributor is Ownable {
     KeepToken public token;
 
     // This event is triggered whenever a call to #claim succeeds.
-    event Claimed(uint256 index, address account, uint256 amount);
-
+    event RewardsClaimed(uint256 index, address account, uint256 amount);
+    // This event is triggered whenever rewards are allocated.
     event RewardsAllocated(bytes32 merkleRoot, uint256 amount);
 
-    // merkleRoot -> total amount for distribution for a given week
+    // Merkle root -> total amount for distribution for a given interval.
     mapping(bytes32 => uint256) private merkleRoots;
-
-    // This is a packed array of booleans.
+    // Bytes32 key is a merkle root and the value is a packed array of booleans.
     mapping(bytes32 => mapping(uint256 => uint256)) private claimedBitMap;
 
     constructor(address _token) public {
         token = KeepToken(_token);
     }
 
-    /// Call receiveApproval to "allocate" amount of KEEP for a given merkleRoot
-    /// @param _extraData merkle root (32 bytes)
+    /// Call receiveApproval to allocate amount of KEEP for a given merkle root.
+    /// @param _from The original sender of the tokens.
+    /// @param _amount The amount of KEEP tokens to fund.
+    /// @param _token The KEEP token to fund the rewards in.
+    /// @param _extraData Merkle root (32 bytes).
     function receiveApproval(
         address _from,
         uint256 _amount,
@@ -77,20 +90,20 @@ contract ECDSARewardsDistributor is Ownable {
     }
 
     function claim(uint256 index, address account, uint256 amount, bytes32 merkleRoot, bytes32[] calldata merkleProof) external {
-        require(!isClaimed(index, merkleRoot), 'MerkleDistributor: Drop already claimed.');
+        require(!isClaimed(index, merkleRoot), 'Rewards already claimed');
 
         // Verify the merkle proof.
         bytes32 node = keccak256(abi.encodePacked(index, account, amount));
 
-        require(MerkleProof.verify(merkleProof, merkleRoot, node), 'MerkleDistributor: Invalid proof.');
+        require(MerkleProof.verify(merkleProof, merkleRoot, node), 'Invalid proof');
 
         // Mark it claimed and send the token.
         _setClaimed(index, merkleRoot);
-        require(IERC20(token).transfer(account, amount), 'MerkleDistributor: Transfer failed.');
+        require(IERC20(token).transfer(account, amount), 'Transfer failed');
 
-        // Update KEEP amount for the give merkleRoot
+        // Update KEEP amount for the given merkleRoot
         merkleRoots[merkleRoot] -= amount;
 
-        emit Claimed(index, account, amount);
+        emit RewardsClaimed(index, account, amount);
     }
 }
