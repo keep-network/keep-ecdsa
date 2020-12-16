fed593645ac64fe182c44e3d440ee1042607e017
Lukasz Zimnoch <lukasz.zimnoch@keep.network>
2020-12-16 14:20:39 +0100

    Add `block-by-date` as separate script

---
+++ staker-rewards/block-by-date.js
@@ -0,0 +1,31 @@
+import clc from "cli-color"
+import BlockByDate from "ethereum-block-by-date"
+import Context from "./lib/context.js"
+
+// Usage: ETH_HOSTNAME=<eth_host> node --experimental-json-modules \
+// block-by-date.js <timestamp>
+async function run() {
+  const ethHostname = process.env.ETH_HOSTNAME
+  const timestamp = process.argv[2]
+
+  if (!ethHostname) {
+    console.error(clc.red("Please provide ETH_HOSTNAME value"))
+    return
+  }
+
+  const { web3 } = await Context.initialize(ethHostname)
+  const blockByDate = new BlockByDate(web3)
+  return (await blockByDate.getDate(timestamp * 1000)).block
+}
+
+run()
+  .then((result) => {
+    console.log(clc.green(`Block ${result}`))
+
+    process.exit(0)
+  })
+  .catch((error) => {
+    console.trace(clc.red("Script errored out with error: "), error)
+
+    process.exit(1)
+  })
