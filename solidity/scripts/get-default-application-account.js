fd9ef7eebfa9cd8e84e5fc625961a3972c25ce26
Dmitry <dymitr.paremski@keep.network>
2021-02-11 13:15:01 +0100

    Modifying retrieval of a default account
    In case a user has less than 7 total accounts, we chose the last one for
    an application address for local development. That was a case while
    working on Celo installation scripts.

--- solidity/scripts/get-default-application-account.js
+++ solidity/scripts/get-default-application-account.js
@@ -1,5 +1,11 @@
 module.exports = async function () {
   const accounts = await web3.eth.getAccounts()
-  console.log(`${accounts[6]}`)
+  // arbitrary chosen account for default application
+  if (accounts.length < 7) {
+    // Assign the last account address as application account
+    console.log(`${accounts[accounts.length-1]}`)
+  } else {
+    console.log(`${accounts[6]}`)
+  }
   process.exit(0)
 }
