b661be3ff15692aa805df1495469926ba87047b6
Beau Shinkle <beaushinkle@gmail.com>
2021-03-04 12:18:28 -0500

    Add a PublicKeyToP2WPKHScriptCode test

--- pkg/extensions/tbtc/liquidation_recovery_test.go
+++ pkg/extensions/tbtc/liquidation_recovery_test.go
@@ -1,8 +1,23 @@
 package tbtc
 
 import (
+	cecdsa "crypto/ecdsa"
+	"crypto/elliptic"
+	"crypto/rand"
 	"testing"
+
+	"github.com/btcsuite/btcd/chaincfg"
 )
+
+func Test_PublicKeyToP2WPKHScriptCode_Works(t *testing.T) {
+	curve := elliptic.P224()
+	privateKey, _ := cecdsa.GenerateKey(curve, rand.Reader)
+	scriptCodeBytes, _ := PublicKeyToP2WPKHScriptCode(&privateKey.PublicKey, &chaincfg.TestNet3Params)
+
+	if len(scriptCodeBytes) != 25 {
+		t.Errorf("The script code must be exactly 26 bytes long. Instead, it was %v", len(scriptCodeBytes))
+	}
+}
 
 func deriveAddressTester(t *testing.T, extendedAddress string, addressIndex int, expectedAddress string) {
 	address, err := DeriveAddress(extendedAddress, addressIndex)
