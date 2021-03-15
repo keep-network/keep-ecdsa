e4686ac93a9a163c7a76ee03dd7aae6d8ecba2f0
Beau Shinkle <beaushinkle@gmail.com>
2021-03-14 20:05:50 -0400

    Updated liquidation_recovery_test to conform to testing style

--- pkg/extensions/tbtc/liquidation_recovery_test.go
+++ pkg/extensions/tbtc/liquidation_recovery_test.go
@@ -15,13 +15,13 @@ import (
 	"github.com/keep-network/keep-ecdsa/pkg/ecdsa"
 )
 
-func Test_PublicKeyToP2WPKHScriptCode_Works(t *testing.T) {
+func TestPublicKeyToP2WPKHScriptCode(t *testing.T) {
 	curve := elliptic.P224()
 	privateKey, _ := cecdsa.GenerateKey(curve, rand.Reader)
 	scriptCodeBytes, err := PublicKeyToP2WPKHScriptCode(&privateKey.PublicKey, &chaincfg.TestNet3Params)
 
 	if err != nil {
-		t.Errorf("%v", err)
+		t.Error(err)
 	}
 
 	if len(scriptCodeBytes) != 26 {
@@ -29,78 +29,72 @@ func Test_PublicKeyToP2WPKHScriptCode_Works(t *testing.T) {
 	}
 }
 
-func Test_ConstructUnsignedTransaction(t *testing.T) {
-	recoveryAddresses := []string{
+func TestConstructUnsignedTransaction(t *testing.T) {
+	recipientAddresses := []string{
 		"bcrt1q5sz7jly79m76a5e8py6kv402q07p725vm4s0zl",
 		"bcrt1qlxt5a04pefwkl90mna2sn79nu7asq3excx60h0",
 		"bcrt1qjhpgmmhaxfwj6t7zf3dvs2fhdhx02g8qn3xwsf",
 	}
-	transactionValue := int64(100000000)
-	msgTx, err := ConstructUnsignedTransaction(
+
+	previousOutputValue := int64(100000000)
+	expectedShare := int64(45)
+
+	messageTransaction, err := ConstructUnsignedTransaction(
 		"0b99dea9655f219991001e9296cfe2103dd918a21ef477a14121d1a0ba9491f1",
 		uint32(0),
-		transactionValue,
+		previousOutputValue,
 		int64(700),
-		recoveryAddresses,
+		recipientAddresses,
 		&chaincfg.TestNet3Params,
 	)
 	if err != nil {
 		t.Errorf("%v", err)
 	}
-	if len(msgTx.TxIn) != 1 {
-		t.Errorf("There should be 1 input transaction. Got %v instead.", len(msgTx.TxIn))
+	if len(messageTransaction.TxIn) != 1 {
+		t.Errorf("There should be 1 input transaction. Got %v instead.", len(messageTransaction.TxIn))
 	}
-	if len(msgTx.TxOut) != len(recoveryAddresses) {
+	if len(messageTransaction.TxOut) != len(recipientAddresses) {
 		t.Errorf(
-			"There should be an output transaction for each recovery address. Got %v but expected %v",
-			len(msgTx.TxOut),
-			len(recoveryAddresses),
+			"there should be an output transaction for each recovery address\nexpected: %v\nactual:   %v",
+			len(recipientAddresses),
+			len(messageTransaction.TxOut),
 		)
 	}
-	share := msgTx.TxOut[0].Value
-	for _, txOut := range msgTx.TxOut {
-		feelessShare := transactionValue / int64(len(recoveryAddresses))
-		if txOut.Value >= feelessShare {
+	for _, transactionOut := range messageTransaction.TxOut {
+		if transactionOut.Value != expectedShare {
 			t.Errorf(
-				"Each output transaction should not be more than a signer's feeless share of deposit value. %v >= %v",
-				txOut.Value,
-				feelessShare,
-			)
-		}
-		if txOut.Value != share {
-			t.Errorf(
-				"Each output transaction should represent an equal share. Got %v but expected %v",
-				txOut.Value,
-				share,
+				"incorrect transaction output value\nexpected: %v\nactual:   %v",
+				expectedShare,
+				transactionOut.Value,
 			)
 		}
 	}
 }
 
 func Test_BuildSignedTransactionHexString(t *testing.T) {
-	tx := wire.NewMsgTx(wire.TxVersion)
+	unsignedTransaction := wire.NewMsgTx(wire.TxVersion)
 	previousOutputTransactionHash, err := chainhash.NewHashFromStr(
 		"0b99dea9655f219991001e9296cfe2103dd918a21ef477a14121d1a0ba9491f1",
 	)
 	if err != nil {
 		t.Errorf("Something went wrong generating the previousOutputTransactionHash: %v", err)
 	}
-	tx.AddTxIn(wire.NewTxIn(
+	unsignedTransaction.AddTxIn(wire.NewTxIn(
 		wire.NewOutPoint(previousOutputTransactionHash, 0),
 		nil,
 		nil,
 	))
 
 	for _, txValue := range []int{100, 100, 100} {
-		tx.AddTxOut(wire.NewTxOut(
+		unsignedTransaction.AddTxOut(wire.NewTxOut(
 			int64(txValue),
 			nil,
 		))
 	}
 	curve := elliptic.P224()
 	privateKey, _ := cecdsa.GenerateKey(curve, rand.Reader)
-	signedHexString, err := BuildSignedTransactionHexString(
-		tx,
+	signedTransactionHex, err := BuildSignedTransactionHexString(
+		unsignedTransaction,
 		&ecdsa.Signature{
 			R:          big.NewInt(int64(3)),
 			S:          big.NewInt(int64(7)),
@@ -112,46 +106,46 @@ func Test_BuildSignedTransactionHexString(t *testing.T) {
 		t.Errorf("Something went wrong building the signed transaction string: %v", err)
 	}
 
-	decodedTx := wire.NewMsgTx(wire.TxVersion)
-	b, _ := hex.DecodeString(signedHexString)
-	decodedTx.BtcDecode(bytes.NewReader(b), 1, wire.WitnessEncoding)
+	signedTransaction := wire.NewMsgTx(wire.TxVersion)
+	signedTransactionBytes, _ := hex.DecodeString(signedTransactionHex)
+	signedTransaction.BtcDecode(bytes.NewReader(signedTransactionBytes), 1, wire.WitnessEncoding)
 
-	if len(decodedTx.TxIn) != len(tx.TxIn) {
+	if len(signedTransaction.TxIn) != len(unsignedTransaction.TxIn) {
 		t.Errorf(
-			"The original and signed transactions must have the same number of input transactions.\n"+
-				"Actual:   %v\nExpected: %v",
-			len(decodedTx.TxIn),
-			len(tx.TxIn),
+			"the original and signed transactions must have the same number of input transactions\n"+
+				"expected: %v\nactual:   %v",
+			len(unsignedTransaction.TxIn),
+			len(signedTransaction.TxIn),
 		)
 	}
-	for i, decodedTxIn := range decodedTx.TxIn {
-		originalTxIn := tx.TxIn[i]
-		if decodedTxIn.PreviousOutPoint.Hash != originalTxIn.PreviousOutPoint.Hash {
+	for i, signedTransactionIn := range signedTransaction.TxIn {
+		originalTransactionIn := unsignedTransaction.TxIn[i]
+		if signedTransactionIn.PreviousOutPoint.Hash != originalTransactionIn.PreviousOutPoint.Hash {
 			t.Errorf(
-				"TxIn hashes don't match.\nActual:   %v\nExpected: %v",
-				decodedTxIn.PreviousOutPoint.Hash,
-				originalTxIn.PreviousOutPoint.Hash,
+				"TxIn hashes don't match\nexpected: %v\nactual:   %v",
+				originalTransactionIn.PreviousOutPoint.Hash,
+				signedTransactionIn.PreviousOutPoint.Hash,
 			)
 		}
-		if decodedTxIn.Witness == nil {
+		if signedTransactionIn.Witness == nil {
 			t.Errorf("TxIn does not have a witness.")
 		}
 	}
-	if len(decodedTx.TxOut) != len(tx.TxOut) {
+	if len(signedTransaction.TxOut) != len(unsignedTransaction.TxOut) {
 		t.Errorf(
-			"The original and signed transactions must have the same number of output transactions.\n"+
-				"Actual:   %v\nExpected: %v",
-			len(decodedTx.TxOut),
-			len(tx.TxOut),
+			"the original and signed transactions must have the same number of output transactions\n"+
+				"expected: %v\nactual:   %v",
+			len(unsignedTransaction.TxOut),
+			len(signedTransaction.TxOut),
 		)
 	}
-	for i, decodedTxOut := range decodedTx.TxOut {
-		originalTxOut := tx.TxOut[i]
-		if decodedTxOut.Value != originalTxOut.Value {
+	for i, signedTransactionOut := range signedTransaction.TxOut {
+		originalTransactionOut := unsignedTransaction.TxOut[i]
+		if signedTransactionOut.Value != originalTransactionOut.Value {
 			t.Errorf(
-				"TxOut values don't match.\nActual:   %v\nExpected: %v",
-				decodedTxOut.Value,
-				originalTxOut.Value,
+				"TxOut values don't match.\nexpected: %v\nactual:   %v",
+				originalTransactionOut.Value,
+				signedTransactionOut.Value,
 			)
 		}
 	}
