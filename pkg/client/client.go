9a2cb21ee75529e284868e894200c1e0def6d2ad
Jakub Nowakowski <jakub.nowakowski@akena.co>
2021-06-04 14:22:31 +0200

    Added some loggers helpful for debuging
    These lines may be usefull for debuging problems on nodes.

--- pkg/client/client.go
+++ pkg/client/client.go
@@ -1025,6 +1025,11 @@ func monitorKeepTerminatedEvent(
 							return err
 						}
 
+						logger.Infof(
+							"starting liquidation recovery protocol for keep [%s]",
+							keep.ID(),
+						)
+
 						members, err := keep.GetMembers()
 						if err != nil {
 							logger.Errorf(
@@ -1127,6 +1132,14 @@ func monitorKeepTerminatedEvent(
 							return err
 						}
 
+						logger.Infof(
+							"building liquidation recovery transaction for keep [%s] "+
+								"with receiving addresses [%v] and maxFeePerVByte [%d]",
+							keep.ID(),
+							btcAddresses,
+							maxFeePerVByte,
+						)
+
 						recoveryTransactionHex, err := recovery.BuildBitcoinTransaction(
 							ctx,
 							networkProvider,
@@ -1147,6 +1160,12 @@ func monitorKeepTerminatedEvent(
 							return err
 						}
 
+						logger.Debugf(
+							"broadcasting liquidation recovery transaction for keep [%s]: [%s]",
+							keep.ID(),
+							recoveryTransactionHex,
+						)
+
 						broadcastError := bitcoinHandle.Broadcast(recoveryTransactionHex)
 						if broadcastError != nil {
 							logger.Errorf(
@@ -1159,6 +1178,11 @@ func monitorKeepTerminatedEvent(
 								logger.Warningf("Please broadcast Bitcoin transaction %s", recoveryTransactionHex)
 							}
 						}
+
+						logger.Debugf(
+							"unregistering keep [%s] after liquidation recovery",
+							keep.ID(),
+						)
 
 						keepsRegistry.UnregisterKeep(keep.ID())
 						keepTerminated <- event
