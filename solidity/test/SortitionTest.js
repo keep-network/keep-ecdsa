ae57e51cd9359a6a1c0c0fba1ca7e48f88c1622c
Beau Shinkle <beaushinkle@gmail.com>
2021-08-19 09:35:56 -0400

    added a script that profiles the sortition pool

--- solidity/test/SortitionTest.js
+++ solidity/test/SortitionTest.js
@@ -1,4 +1,4 @@
-const { contract, web3 } = require("@openzeppelin/test-environment")
+const {contract, web3, accounts} = require("@openzeppelin/test-environment")
 
 const TestSortitionPool = contract.fromArtifact("TestSortitionPool")
 const StackLib = contract.fromArtifact("StackLib")
@@ -10,7 +10,7 @@ const expect = chai.expect
 
 describe("blah", () => {
   let pool
-  
+
   beforeEach(async () => {
     stackLib = await StackLib.new()
     await TestSortitionPool.detectNetwork()
@@ -18,9 +18,18 @@ describe("blah", () => {
 
     pool = await TestSortitionPool.new()
   })
-  
+
   it.only("bar", async () => {
-    console.log(pool)
+    for (let i = 0; i < 1000; i++) {
+      await pool.joinPool(accounts[i])
+    }
+
+    const iterations = 100
+    for (let num = 0; num < iterations; num++) {
+      const seed = "0x" + (num).toString(16)
+      const tx = await pool.selectGroup(50, seed, 1)
+      console.log(tx.receipt.gasUsed)
+    }
     expect(42).to.equal(42)
   })
 })
